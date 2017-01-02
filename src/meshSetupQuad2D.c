dd203d7f2f828288166ffe80b8b8d01ccb4cf8fc
TW <tim.warburton@gmail.com>
2017-01-02 17:54:04 -0600

    adding flow ramping for quad based boltzmann flow solver

--- src/meshSetupQuad2D.c
+++ src/meshSetupQuad2D.c
@@ -48,16 +48,17 @@ mesh2D *meshSetupQuad2D(char *filename, int N){
 		    1720146321549.0/2090206949498.0  ,
 		    3134564353537.0/4481467310338.0  ,
 		    2277821191437.0/14882151754819.0};
-  dfloat rkc[5] = {0.0  ,
+  dfloat rkc[6] = {0.0  ,
 		   1432997174477.0/9575080441755.0 ,
 		   2526269341429.0/6820363962896.0 ,
 		   2006345519317.0/3224310063776.0 ,
-		   2802321613138.0/2924317926251.0}; 
+		   2802321613138.0/2924317926251.0,
+		   1.}; 
 
   mesh->Nrk = Nrk;
   memcpy(mesh->rka, rka, Nrk*sizeof(dfloat));
   memcpy(mesh->rkb, rkb, Nrk*sizeof(dfloat));
-  memcpy(mesh->rkc, rkc, Nrk*sizeof(dfloat));
+  memcpy(mesh->rkc, rkc, (Nrk+1)*sizeof(dfloat));
     
   return mesh;
 }
