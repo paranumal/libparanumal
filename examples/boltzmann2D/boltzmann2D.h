899dd5e9e0eb51f9a71640de1eea8a401ef93e9e
Tim Warburton <timwar@Tims-MacBook-Pro-3.local>
2017-01-03 20:14:42 -0500

    added derivative of ramp

--- examples/boltzmann2D/boltzmann2D.h
+++ examples/boltzmann2D/boltzmann2D.h
@@ -9,4 +9,5 @@ void boltzmannError2D(mesh2D *mesh, dfloat time);
 
 void boltzmannComputeVorticity2D(mesh2D *mesh, dfloat *q, iint outfld, iint Nfields);
 
-dfloat boltzmannRampFunction2D(dfloat t);
+//dfloat boltzmannRampFunction2D(dfloat t);
+void boltzmannRampFunction2D(dfloat t, dfloat *ramp, dfloat *drampdt);
