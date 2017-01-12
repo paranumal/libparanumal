9db36218477c7b7b096cae1c76cee5292a8d78c7
TW <tim.warburton@gmail.com>
2017-01-12 06:43:52 -0600

    adding hex example acoustics

---
+++ examples/acousticsHex3D/acousticsMainHex3D.c
@@ -0,0 +1,31 @@
+#include "acousticsHex3D.h"
+
+int main(int argc, char **argv){
+
+  // start up MPI
+  MPI_Init(&argc, &argv);
+
+  if(argc!=3){
+    // to run cavity test case with degree N elements
+    printf("usage: ./main meshes/cavityH005.msh N\n");
+    exit(-1);
+  }
+
+  // int specify polynomial degree 
+  int N = atoi(argv[2]);
+
+  // set up mesh stuff
+  mesh3D *mesh = meshSetupHex3D(argv[1], N);
+
+  // set up acoustics stuff
+  acousticsSetupHex3D(mesh);
+
+  // run
+  acousticsOccaRunHex3D(mesh);
+
+  // close down MPI
+  MPI_Finalize();
+
+  exit(0);
+  return 0;
+}
