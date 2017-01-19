90c227098963c323621e738ed492bedc537ef217
TW <tim.warburton@gmail.com>
2017-01-18 20:08:08 -0600

    adding gslib from https://github.com/gslib/gslib

---
+++ 3rdParty/gslib.github/src/gslib.h
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "c99.h"
+#include "name.h"
+#include "fail.h"
+#include "types.h"
+#include "comm.h"
+#include "mem.h"
+#include "gs_defs.h"
+#include "gs.h"
+
