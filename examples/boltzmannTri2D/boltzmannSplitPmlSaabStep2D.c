fc20f593d3c7a68c2929960049c6e7ff94143a14
Ali Karakus <alikarakus.me@gmail.com>
2017-02-21 22:07:11 -0500

    Added semi analytical time integration (SAAB)

--- examples/boltzmannTri2D/boltzmannSplitPmlSaabStep2D.c
+++ examples/boltzmannTri2D/boltzmannSplitPmlSaabStep2D.c
@@ -1,7 +1,7 @@
 #include "boltzmann2D.h"
 
 // complete a time step using Semi-Analytic Adams-Bahforth 
-void boltzmannSplitPmlSaabV1Step2D(mesh2D *mesh, iint tstep, iint haloBytes,
+void boltzmannSplitPmlSaabStep2D(mesh2D *mesh, iint tstep, iint haloBytes,
 				  dfloat * sendBuffer, dfloat *recvBuffer){
 
 
@@ -36,6 +36,7 @@ if(mesh->totalHaloPairs>0){
 	if(mesh->pmlNelements)
   		mesh->pmlVolumeKernel(mesh->pmlNelements,
 		    mesh->o_pmlElementIds,
+		    ramp,
 		    mesh->o_vgeo,
 		    mesh->o_sigmax,
 		    mesh->o_sigmay,
@@ -72,9 +73,12 @@ if(mesh->totalHaloPairs>0){
     if(mesh->pmlNelements)
       mesh->pmlRelaxationKernel(mesh->pmlNelements,
 			     mesh->o_pmlElementIds,
+			     ramp,
 			     mesh->o_cubInterpT,
 			     mesh->o_cubProjectT,
 			     mesh->o_q,
+			     mesh->o_pmlqx,
+			     mesh->o_pmlqy,
 			     mesh->o_rhspmlqx,
 			     mesh->o_rhspmlqy,
 			     mesh->o_rhspmlNT);
@@ -91,7 +95,7 @@ if(mesh->totalHaloPairs>0){
 #endif
 
 
-// complete halo exchange
+ // complete halo exchange
     if(mesh->totalHaloPairs>0){
       // wait for halo data to arrive
       meshHaloExchangeFinish(mesh);
@@ -101,14 +105,10 @@ if(mesh->totalHaloPairs>0){
       mesh->o_q.copyFrom(recvBuffer, haloBytes, offset);
     }
     
-
-
-
-
     mesh->device.finish();
     occa::tic("surfaceKernel");
     
-    // compute surface contribution to DG boltzmann RHS
+     if(mesh->pmlNelements)
     mesh->pmlSurfaceKernel(mesh->pmlNelements,
 			   mesh->o_pmlElementIds,
 			   mesh->o_sgeo,
@@ -143,17 +143,13 @@ if(mesh->totalHaloPairs>0){
     occa::toc("surfaceKernel");
 
 
-if(tstep==0){
+ if(tstep==0){
 
 	if (mesh->pmlNelements)   
 		      mesh->pmlUpdateFirstOrderKernel(mesh->pmlNelements,
 					mesh->o_pmlElementIds,
 				    mesh->dt,
 				    ramp,
-				    mesh->o_sigmax,
-				    mesh->o_sigmay,
-				    mesh->o_expsigmax,
-				    mesh->o_expsigmay,
 				    mesh->o_rhspmlqx,
 				    mesh->o_rhspmlqy,
 				    mesh->o_rhspmlNT,
@@ -184,13 +180,9 @@ else if(tstep==1){
 
 	if (mesh->pmlNelements)   
   			mesh->pmlUpdateSecondOrderKernel(mesh->pmlNelements,
-		        mesh->o_pmlElementIds,
+					mesh->o_pmlElementIds,
 				    mesh->dt,
 				    ramp,
-				    mesh->o_sigmax,
-				    mesh->o_sigmay,
-				    mesh->o_expsigmax,
-				    mesh->o_expsigmay,
 				    mesh->o_rhspmlqx,
 				    mesh->o_rhspmlqy,
 				    mesh->o_rhspmlNT,
@@ -225,10 +217,6 @@ else{
 		    mesh->o_pmlElementIds,
 		    mesh->dt,
 		    ramp,
-		    mesh->o_sigmax,
-		    mesh->o_sigmay,
-		    mesh->o_expsigmax,
-		    mesh->o_expsigmay,
 		    mesh->o_rhspmlqx,
 		    mesh->o_rhspmlqy,
 		    mesh->o_rhspmlNT,
--- include/mesh.h
+++ include/mesh.h
@@ -155,6 +155,7 @@ typedef struct {
   iint   errorStep; // number of steps between error calculations
   iint   Nrk;
   dfloat rka[5], rkb[5], rkc[6];
+
   //LS Imex
   dfloat LsimexB[4], LsimexC[4], LsimexABi[4], LsimexABe[4], LsimexAd[4];
   iint Nimex;
@@ -184,15 +185,13 @@ typedef struct {
   dfloat *rhspmlqx; // right hand side data array
   dfloat *respmlqx; // residual data array (for LSERK time-stepping)
   dfloat *sigmax;
-  dfloat *expsigmax;
- 
+   
 
   dfloat *pmlqy;    // y-pml data array
   dfloat *rhspmlqy; // right hand side data array
   dfloat *respmlqy; // residual data array (for LSERK time-stepping)
   dfloat *sigmay;
-  dfloat *expsigmay; // hold exp(-sigma*dt)
-  
+    
   dfloat *pmlNT;    // time integrated relaxtion term
   dfloat *rhspmlNT; //
   dfloat *respmlNT; //
