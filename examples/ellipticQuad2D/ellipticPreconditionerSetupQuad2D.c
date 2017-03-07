447b7872d383b986404531dd7137c51f8b1941b8
TW <tim.warburton@gmail.com>
2017-03-06 19:54:56 -0600

    changed order

--- examples/ellipticQuad2D/ellipticPreconditionerSetupQuad2D.c
+++ examples/ellipticQuad2D/ellipticPreconditionerSetupQuad2D.c
@@ -382,8 +382,5 @@ precon_t *ellipticPreconditionerSetupQuad2D(mesh2D *mesh, ogs_t *ogs, dfloat lam
   
   free(diagA);
     
-  // coarse grid preconditioner (only continous elements)
-  ellipticCoarsePreconditionerSetupQuad2D(mesh, precon, ogs, lambda, options);
-
   return precon;
 }
