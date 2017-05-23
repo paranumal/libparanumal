5438b0f10680253a0459a0d49a343d2282957392
Tim Warburton <tim.warburton@gmail.com>
2017-05-23 02:30:18 -0400

    fixed a couple of things. There is still odd behavior on the GPU for hex
    Former-commit-id: b4285cc2a43f1f9803b1459bd43ff740f3006967

--- utilities/parALMOND/okl/twoPhaseReduction.h
+++ utilities/parALMOND/okl/twoPhaseReduction.h
@@ -9,11 +9,11 @@
   for(int ty=0;ty<p_RDIMY;++ty;inner1){					\
     for(int tx=0;tx<p_RDIMX;++tx;inner0){				\
       s_ip[ty][tx] = r_ip;						\
-      if(tx>  1*p_RDIMX/2  -1) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/2];	\
-      if(tx>  3*p_RDIMX/4  -1) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/4];	\
-      if(tx>  7*p_RDIMX/8  -1) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/8];	\
-      if(tx> 15*p_RDIMX/16 -1) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/16];	\
-      if(tx> 31*p_RDIMX/32 -1) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/32];	\
+      if(tx>=  1*p_RDIMX/2) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/2];	\
+      if(tx>=  3*p_RDIMX/4) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/4];	\
+      if(tx>=  7*p_RDIMX/8) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/8];	\
+      if(tx>= 15*p_RDIMX/16) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/16];	\
+      if(tx>= 31*p_RDIMX/32) s_ip[ty][tx] += s_ip[ty][tx-p_RDIMX/32];	\
       if(tx==(p_RDIMX-1)) s_res[ty] = s_ip[ty][tx];			\
     }									\
   }									\
@@ -22,10 +22,10 @@
 									\
   for(int ty=0;ty<p_RDIMY;++ty;inner1){					\
     for(int tx=0;tx<p_RDIMX;++tx;inner0){				\
-      if(ty==0){							\
-      	if(tx>  1*p_RDIMY/2  -1) s_res[tx] += s_res[tx-p_RDIMY/2];	\
-      	if(tx>  3*p_RDIMY/4  -1) s_res[tx] += s_res[tx-p_RDIMY/4];	\
-      	if(tx>  7*p_RDIMY/8  -1) s_res[tx] += s_res[tx-p_RDIMY/8];	\
+      if(ty==0 && tx<p_RDIMY){						\
+      	if(tx >= 1*p_RDIMY/2) s_res[tx] += s_res[tx-p_RDIMY/2];		\
+      	if(tx >= 3*p_RDIMY/4) s_res[tx] += s_res[tx-p_RDIMY/4];		\
+      	if(tx >= 7*p_RDIMY/8) s_res[tx] += s_res[tx-p_RDIMY/8];		\
       	if(tx==(p_RDIMY-1)) {						\
           dfloat val = s_res[p_RDIMY-1];				\
           atomicAdd(g_ip, val);						\
