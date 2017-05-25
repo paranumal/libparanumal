08fc7272816bec30c03d0cc6bf9fbc22a439c025
Tim Warburton <tim.warburton@gmail.com>
2017-05-25 12:21:53 -0400

    reordered lift access

--- examples/ellipticTri2D/ellipticBuildIpdgTri2D.c
+++ examples/ellipticTri2D/ellipticBuildIpdgTri2D.c
@@ -20,7 +20,7 @@ void ellipticBuildIpdgTri2D(mesh2D *mesh, dfloat lambda, nonZero_t **A, iint *nn
   iint *rankNelements = (iint*) calloc(size, sizeof(iint));
   iint *rankStarts = (iint*) calloc(size+1, sizeof(iint));
   MPI_Allgather(&(mesh->Nelements), 1, MPI_IINT,
-    rankNelements, 1, MPI_IINT, MPI_COMM_WORLD);
+		rankNelements, 1, MPI_IINT, MPI_COMM_WORLD);
   for(iint r=0;r<size;++r){
     rankStarts[r+1] = rankStarts[r]+(rankNelements[r])*mesh->Np;
   }
@@ -46,12 +46,13 @@ void ellipticBuildIpdgTri2D(mesh2D *mesh, dfloat lambda, nonZero_t **A, iint *nn
   dfloat *MS = (dfloat *) calloc(mesh->Nfaces*mesh->Np*mesh->Nfp,sizeof(dfloat));
   for (iint f=0;f<mesh->Nfaces;f++) {
     for (iint n=0;n<mesh->Np;n++) {
-      for (iint i=0;i<mesh->Nfp;i++) {
-        MS[i+n*mesh->Nfp + f*mesh->Nfp*mesh->Np] = 0.;
-        for (iint m=0;m<mesh->Np;m++) {
-          MS[i+n*mesh->Nfp + f*mesh->Nfp*mesh->Np] 
-            += mesh->MM[m+n*mesh->Np]*mesh->LIFT[i+m*mesh->Nfp+f*mesh->Nfp*mesh->Np];
-        }
+      for (iint m=0;m<mesh->Nfp;m++) {
+	dfloat MSnm = 0;
+		  
+	for (iint i=0;i<mesh->Np;i++) 
+	  MSnm += mesh->MM[n+i*mesh->Np]*mesh->LIFT[i*mesh->Nfp*mesh->Nfaces+f*mesh->Nfp+m];
+	  
+	MS[m+n*mesh->Nfp + f*mesh->Nfp*mesh->Np]  = MSnm;
       }
     }
   }
@@ -133,9 +134,9 @@ void ellipticBuildIpdgTri2D(mesh2D *mesh, dfloat lambda, nonZero_t **A, iint *nn
           if (vidP == m) qmP[i] =1;
           
           ndotgradqmM[i] = (nx*drdx+ny*drdy)*mesh->Dr[m+vidM*mesh->Np]
-                          +(nx*dsdx+ny*dsdy)*mesh->Ds[m+vidM*mesh->Np];
+	    +(nx*dsdx+ny*dsdy)*mesh->Ds[m+vidM*mesh->Np];
           ndotgradqmP[i] = (nx*drdxP+ny*drdyP)*mesh->Dr[m+vidP*mesh->Np]
-                          +(nx*dsdxP+ny*dsdyP)*mesh->Ds[m+vidP*mesh->Np];
+	    +(nx*dsdxP+ny*dsdyP)*mesh->Ds[m+vidP*mesh->Np];
         }
 
         dfloat penalty = tau*(mesh->N+1)*(mesh->N+1)*hinv; 
@@ -144,7 +145,7 @@ void ellipticBuildIpdgTri2D(mesh2D *mesh, dfloat lambda, nonZero_t **A, iint *nn
           for (iint i=0;i<mesh->Nfp;i++) {
             BM[m+n*mesh->Np] += -0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*ndotgradqmM[i];
             BM[m+n*mesh->Np] += -0.5*sJ*(nx*drdx+ny*drdy)*DrTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]
-                                -0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]; 
+	      -0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]; 
             BM[m+n*mesh->Np] += +0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*penalty*qmM[i];
           }
 
@@ -153,14 +154,14 @@ void ellipticBuildIpdgTri2D(mesh2D *mesh, dfloat lambda, nonZero_t **A, iint *nn
             for (iint i=0;i<mesh->Nfp;i++) {
               BM[m+n*mesh->Np] += +0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*ndotgradqmM[i];
               BM[m+n*mesh->Np] += +0.5*sJ*(nx*drdx+ny*drdy)*DrTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]
-                                  +0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]; 
+		+0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmM[i]; 
               BM[m+n*mesh->Np] += -0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*penalty*qmM[i];
             }
           } else {
             for (iint i=0;i<mesh->Nfp;i++) {
               AnmP += -0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*ndotgradqmP[i];
               AnmP += +0.5*sJ*(nx*drdx+ny*drdy)*DrTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmP[i]
-                      +0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmP[i]; 
+		+0.5*sJ*(nx*dsdx+ny*dsdy)*DsTMS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*qmP[i]; 
               AnmP += -0.5*sJ*MS[i+n*mesh->Nfp+fM*mesh->Nfp*mesh->Np]*penalty*qmP[i];
             }
           }
