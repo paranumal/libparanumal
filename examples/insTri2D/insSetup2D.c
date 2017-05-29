4604e05b84f29180ca04dfa505c39b6b9abb1464
Tim Warburton <tim.warburton@gmail.com>
2017-05-29 10:49:09 -0400

    rearranging some things
    Former-commit-id: a5133fafa831d8d3d2cfe66d7b7a8264523093ef

--- examples/insTri2D/insSetup2D.c
+++ examples/insTri2D/insSetup2D.c
@@ -3,14 +3,46 @@
 // NBN: toggle use of 2nd stream
 #define USE_2_STREAMS
 
-  ins_t *insSetup2D(mesh2D *mesh, char * options){
+ins_t *insSetup2D(mesh2D *mesh, char * options){
+
+  mesh->Nfields = 1;
+  
+  // OCCA build stuff
+  char deviceConfig[BUFSIZ];
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  // use rank to choose DEVICE
+  sprintf(deviceConfig, "mode = CUDA, deviceID = %d", (rank+1)%3);
+  //sprintf(deviceConfig, "mode = OpenCL, deviceID = 1, platformID = 0");
+  // sprintf(deviceConfig, "mode = OpenCL, deviceID = 0, platformID = 0");
+  
+  // sprintf(deviceConfig, "mode = OpenMP, deviceID = %d", 1);
+  //sprintf(deviceConfig, "mode = Serial");  
+
+  // compute samples of q at interpolation nodes
+  mesh->q    = (dfloat*) calloc((mesh->totalHaloPairs+mesh->Nelements)*mesh->Np*mesh->Nfields, sizeof(dfloat));
+  mesh->rhsq = (dfloat*) calloc(mesh->Nelements*mesh->Np*mesh->Nfields,
+				sizeof(dfloat));
+  mesh->resq = (dfloat*) calloc(mesh->Nelements*mesh->Np*mesh->Nfields,
+				sizeof(dfloat));
+  
+  occa::kernelInfo kernelInfo;
+  meshOccaSetup2D(mesh, deviceConfig, kernelInfo);
+
+  dfloat lambda =1.0;
+  char *prSolverOptions = 
+  strdup("solver=PCG,FLEXIBLE method=IPDG preconditioner=FULLALMOND,UBERGRID,MATRIXFREE");
+
+  // Set-up Pressure Incriment Solver
+  solver_t *solver = ellipticSolveSetupTri2D(mesh,lambda, kernelInfo, prSolverOptions); 
+  // ins->prsolver = prsolver; 
   
   ins_t *ins = (ins_t*) calloc(1, sizeof(ins_t));
-
-   
+  
   ins->NVfields = 2; // Velocity 
   ins->NTfields = 3; // Velocity + Pressure
-  mesh->Nfields = ins->NTfields; // 
 
   ins->mesh = mesh;  
   // compute samples of q at interpolation nodes
@@ -38,26 +70,22 @@
   dfloat pr   = 0.0  ;   
   dfloat nu   = 1.0/40.0;  // kinematic viscosity,
   dfloat rho  = 1.0  ;  // Give density for getting actual pressure in nondimensional solve
-
   
   dfloat g[2]; g[0] = 0.0; g[1] = 0.0;  // No gravitation
   
-  
-  #if 0
+#if 0
   // Create Periodic Boundaries
   printf("Creating periodic connections if exist \n");
   dfloat xper = 1.0;   dfloat yper = 0.0;
   insMakePeriodic2D(mesh,xper,yper);
-  #endif
+#endif
   // Fill up required fileds
   ins->finalTime = 1. ;
   ins->nu        = nu ;
   ins->rho       = rho;
   ins->tau       = 1000.; 
   //
-   //memcpy(ins->g, g,2*sizeof(dfloat));
-
-
+  //memcpy(ins->g, g,2*sizeof(dfloat));
 
   // Define total DOF per field for INS i.e. (Nelelemts + Nelements_halo)*Np
   ins->NtotalDofs = (mesh->totalHaloPairs+mesh->Nelements)*mesh->Np ; 
@@ -77,8 +105,6 @@
 
     }
   }
-
-
 
   printf("starting parameters\n");
     // set time step
@@ -113,12 +139,10 @@
   }
   // Maximum Velocity
   umax = sqrt(umax);
-  
 
   dfloat cfl = 0.5; 
   dfloat magVel = mymax(umax,1.0); // Correction for initial zero velocity
   dfloat dt = cfl* hmin/( (mesh->N+1.)*(mesh->N+1.) * magVel) ;
-
   
   printf("hmin = %g\n", hmin);
   printf("hmax = %g\n", hmax);
@@ -135,44 +159,27 @@
   ins->errorStep = 100;
 
   printf("Nsteps = %d NerrStep= %d dt = %.8e\n", ins->NtimeSteps,ins->errorStep, ins->dt);
-
   
-
-
-  
-    // OCCA build stuff
-  char deviceConfig[BUFSIZ];
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  // use rank to choose DEVICE
- // sprintf(deviceConfig, "mode = CUDA, deviceID = %d", (rank+1)%3);
-  sprintf(deviceConfig, "mode = OpenCL, deviceID = 1, platformID = 0");
-  // sprintf(deviceConfig, "mode = OpenCL, deviceID = 0, platformID = 0");
-
-  // sprintf(deviceConfig, "mode = OpenMP, deviceID = %d", 1);
-  //sprintf(deviceConfig, "mode = Serial");  
-  
-  occa::kernelInfo kernelInfo;
-  meshOccaSetup2D(mesh, deviceConfig, kernelInfo);
-
    // specialization for Boltzmann
 
   kernelInfo.addDefine("p_maxNodesVolume", mymax(mesh->cubNp,mesh->Np));
     
   int maxNodes = mymax(mesh->Np, (mesh->Nfp*mesh->Nfaces));
   kernelInfo.addDefine("p_maxNodes", maxNodes);
-
+#if 1
   int NblockV = 256/mesh->Np; // works for CUDA
   kernelInfo.addDefine("p_NblockV", NblockV);
 
   int NblockS = 128/maxNodes; // works for CUDA
   kernelInfo.addDefine("p_NblockS", NblockS);
 
+  printf("maxNodes: %d \t NblockV: %d \t NblockS: %d  \n", maxNodes, NblockV, NblockS);
+  
+#endif
+  
   printf("Np: %d \t Ncub: %d \n", mesh->Np, mesh->cubNp);
 
-  printf("maxNodes: %d \t NblockV: %d \t NblockS: %d  \n", maxNodes, NblockV, NblockS);
+
 
   // ADD-DEFINES 
   kernelInfo.addDefine("p_Lambda2", 0.5f);
@@ -200,11 +207,12 @@
     
   ins->o_UO    = mesh->device.malloc(mesh->Np*mesh->Nelements*ins->NVfields*sizeof(dfloat), ins->UO);
   ins->o_UOO   = mesh->device.malloc(mesh->Np*mesh->Nelements*ins->NVfields*sizeof(dfloat), ins->UO);
-  
-  ins->o_totHaloBuffer = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np*(ins->NTfields)*sizeof(dfloat));
-  ins->o_velHaloBuffer = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np*(ins->NVfields)*sizeof(dfloat));
-  ins->o_prHaloBuffer  = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np *sizeof(dfloat));
-  
+
+  if(mesh->totalHaloPairs){
+    ins->o_totHaloBuffer = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np*(ins->NTfields)*sizeof(dfloat));
+    ins->o_velHaloBuffer = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np*(ins->NVfields)*sizeof(dfloat));
+    ins->o_prHaloBuffer  = mesh->device.malloc(mesh->totalHaloPairs*mesh->Np *sizeof(dfloat));
+  }
 
 
    // KERNEL DEFINITIONS
@@ -345,13 +353,7 @@
 // ===========================================================================//
   // ELLIPTIC SOLVER FOR TEST
   #if 1
-  dfloat lambda =1.0;
-  char *prSolverOptions = 
-  strdup("solver=PCG,FLEXIBLE method=IPDG preconditioner=FULLALMOND,UBERGRID,MATRIXFREE");
 
-  // Set-up Pressure Incriment Solver
-  solver_t *solver = ellipticSolveSetupTri2D(mesh,lambda, kernelInfo, prSolverOptions); 
-  // ins->prsolver = prsolver; 
 
   iint Nall = mesh->Np*(mesh->Nelements+mesh->totalHaloPairs);
   dfloat *r   = (dfloat*) calloc(Nall,   sizeof(dfloat));
