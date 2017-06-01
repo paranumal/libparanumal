3004c2b5d134f3ae9b5b2c476d20a0a34a7a492a
AliKarakus <alikarakus.me@gmail.com>
2017-05-31 21:50:17 -0400

    Ins is ready to be tested after adding bcs to elliptic solver

--- examples/insTri2D/insPoissonStep2D.c
+++ examples/insTri2D/insPoissonStep2D.c
@@ -3,7 +3,7 @@
 // complete a time step using LSERK4
 void insPoissonStep2D(ins_t *ins, iint tstep, iint haloBytes,
 				       dfloat * sendBuffer, dfloat * recvBuffer, 
-				        char   * options, char * prSolverOptions){
+				        char   * options){
 
 mesh2D *mesh = ins->mesh; 
 
@@ -12,66 +12,70 @@ solver_t *solver = ins->prsolver;
 dfloat t = tstep*ins->dt + ins->dt;
 
 
-// //Exctract Halo On Device
+//Exctract Halo On Device
 
-// 	if(mesh->totalHaloPairs>0){
+	if(mesh->totalHaloPairs>0){
 	 
-//     ins->poissonHaloExtractKernel(mesh->Nelements,
-//                            mesh->totalHaloPairs,
-//                            mesh->o_haloElementList,
-//                            ins->o_U,
-//                            ins->o_velHaloBuffer);
+    ins->poissonHaloExtractKernel(mesh->Nelements,
+                           mesh->totalHaloPairs,
+                           mesh->o_haloElementList,
+                           ins->o_Ux,
+                           ins->o_Uy,
+                           ins->o_velHaloBuffer);
 
-//     // copy extracted halo to HOST 
-//     ins->o_velHaloBuffer.copyTo(sendBuffer);            
-//     // start halo exchange
-//     meshHaloExchangeStart(mesh,
-//                           mesh->Np*ins->NVfields*sizeof(dfloat), 
-//                           sendBuffer,
-//                           recvBuffer);
-//   	}
-
-
-
-//    // computes div u^(n+1) volume term
-//    ins->poissonRhsVolumeKernel(mesh->Nelements,
-//                                  mesh->o_vgeo,
-//                                  mesh->o_DrT,
-//                                  mesh->o_DsT,
-//                                  mesh->o_MM,
-//                                  ins->o_U,  
-//                                  ins->o_rhsPr);
+    // copy extracted halo to HOST 
+    ins->o_velHaloBuffer.copyTo(sendBuffer);            
+    // start halo exchange
+    meshHaloExchangeStart(mesh,
+                          mesh->Np*ins->NVfields*sizeof(dfloat), 
+                          sendBuffer,
+                          recvBuffer);
+  	}
 
 
-//     // COMPLETE HALO EXCHANGE
-//   if(mesh->totalHaloPairs>0){
-//   // wait for halo data to arrive
-//     meshHaloExchangeFinish(mesh);
 
-//     mesh->o_haloBuffer.copyFrom(recvBuffer); 
-
-//     ins->poissonHaloScatterKernel(mesh->Nelements,
-//                                   mesh->totalHaloPairs,
-//                                   mesh->o_haloElementList,
-//                                   ins->o_U,
-//                                   ins->o_velHaloBuffer);
-//   }
+   // computes div u^(n+1) volume term
+   ins->poissonRhsVolumeKernel(mesh->Nelements,
+                                 mesh->o_vgeo,
+                                 mesh->o_DrT,
+                                 mesh->o_DsT,
+                                 mesh->o_MM,
+                                 ins->o_Ux, 
+                                 ins->o_Uy, 
+                                 ins->o_rhsPr);
 
 
-//    //computes div u^(n+1) surface term
-//   ins->poissonRhsSurfaceKernel(mesh->Nelements,
-//   	                          ins->dt,	
-//                               ins->g0,
-//                               mesh->o_sgeo,
-//                               mesh->o_FMMT,
-//                               mesh->o_vmapM,
-//                               mesh->o_vmapP,
-//                               mesh->o_EToB,
-//                               t,
-//                               mesh->o_x,
-//                               mesh->o_y,
-//                               ins->o_U,
-//                               ins->o_rhsPr);
+    // COMPLETE HALO EXCHANGE
+  if(mesh->totalHaloPairs>0){
+  // wait for halo data to arrive
+    meshHaloExchangeFinish(mesh);
+
+    mesh->o_haloBuffer.copyFrom(recvBuffer); 
+
+    ins->poissonHaloScatterKernel(mesh->Nelements,
+                                  mesh->totalHaloPairs,
+                                  mesh->o_haloElementList,
+                                  ins->o_Ux,
+                                  ins->o_Uy,
+                                  ins->o_velHaloBuffer);
+  }
+
+
+   //computes div u^(n+1) surface term
+  ins->poissonRhsSurfaceKernel(mesh->Nelements,
+  	                          ins->dt,	
+                              ins->g0,
+                              mesh->o_sgeo,
+                              mesh->o_FMMT,
+                              mesh->o_vmapM,
+                              mesh->o_vmapP,
+                              mesh->o_EToB,
+                              t,
+                              mesh->o_x,
+                              mesh->o_y,
+                              ins->o_Ux,
+                              ins->o_Uy,
+                              ins->o_rhsPr);
 
 
 
@@ -97,7 +101,7 @@ dfloat t = tstep*ins->dt + ins->dt;
 
 
 
-//ellipticSolveTri2D(solver, 0.0, ins->o_rhsPr, ins->o_PrI,  prSolverOptions);
+ellipticSolveTri2D(solver, 0.0, ins->o_rhsPr, ins->o_PrI,  ins->prsolverOptions);
 
    
 }
