1442bad6e815b95da8033bbf49e7daf8d067dfec
AliKarakus <alikarakus.me@gmail.com>
2017-06-01 15:51:33 -0400

    Addinf elliptic bcs

--- examples/insTri2D/insRun2D.c
+++ examples/insTri2D/insRun2D.c
@@ -89,63 +89,55 @@ void insRun2D(ins_t *ins, char *options){
   ins->b2 = 0.f, ins->b2 = 0.f;
   ins->g0 = 1.f; 
   
-
+  ins->lamda = ins->g0 / (ins->dt * ins->nu); // Update Lamda for first order
   // One First Order Step 
   insHelmholtzStep2D(ins, 0, helmholtzHaloBytes, helmholtzSendBuffer, helmholtzRecvBuffer, options);
-  insPoissonStep2D(ins,   0, poissonHaloBytes  , poissonSendBuffer  , poissonRecvBuffer  , options);
+  //insPoissonStep2D(ins,   0, poissonHaloBytes  , poissonSendBuffer  , poissonRecvBuffer  , options);
   //insUpdateStep2D(ins, 0, updateHaloBytes, updateSendBuffer, updateRecvBuffer, options);
   //
-  printf("Finished first order step\n");
+ //  printf("Finished first order step\n");
   
-  // // Switch to second order
-  // ins->a0 =  2.0,  ins->b0  = 2.0f;
-  // ins->a1 = -0.5f, ins->b1 = -1.0f;
-  // ins->b2 =  0.f,  ins->b2  = 0.f;
-  // ins->g0 =  1.5f; 
+ //  // Switch to second order
+ //  ins->a0 =  2.0,  ins->b0  = 2.0f;
+ //  ins->a1 = -0.5f, ins->b1 = -1.0f;
+ //  ins->b2 =  0.f,  ins->b2  = 0.f;
+ //  ins->g0 =  1.5f; 
 
-  // // Set-up First Order Solver
-  // ins->lamda = ins->g0 / (ins->dt * ins->nu);
-  // velsolver = ellipticSolveSetupTri2D(mesh, ins->lamda, kernelInfo, velSolverOptions); 
-  // ins->velsolver = velsolver; 
+ //  ins->lamda = ins->g0 / (ins->dt * ins->nu);
+ // // One First Order Step 
+ //  insHelmholtzStep2D(ins, 1, helmholtzHaloBytes, helmholtzSendBuffer, helmholtzRecvBuffer, options);
+ //  insPoissonStep2D(ins,   1, poissonHaloBytes  , poissonSendBuffer  , poissonRecvBuffer  , options);
+ //  insUpdateStep2D(ins, 1, updateHaloBytes, updateSendBuffer, updateRecvBuffer, options);
+ //  //
+ //  printf("Finished second order step\n");
 
-//   insHelmholtzStep2D(ins, 1, helmholtzHaloBytes, helmholtzSendBuffer, helmholtzRecvBuffer, options, velSolverOptions);
-//   insPoissonStep2D(ins, 1, poissonHaloBytes, poissonSendBuffer, poissonRecvBuffer, options,prSolverOptions);
-//   insUpdateStep2D(ins, 1, updateHaloBytes, updateSendBuffer, updateRecvBuffer, options);
-//   printf("Finished second order step\n");
 
-// insReport2D(ins, ins->NtimeSteps,options);
+
+ //  // Switch to third order
+ //  ins->a0 =  3.f,       ins->b0  =  3.0f;
+ //  ins->a1 = -1.5f,      ins->b1  = -3.0f;
+ //  ins->b2 =  1.f/3.f,   ins->b2  =  1.0f;
+ //  ins->g0 =  11.f/6.f; 
+
+ //  // // Set-up First Order Solver
+ //  ins->lamda = ins->g0 / (ins->dt * ins->nu);
   
-
-   // Switch to third order
-  // ins->a0 =  3.f,       ins->b0  =  3.0f;
-  // ins->a1 = -1.5f,      ins->b1  = -3.0f;
-  // ins->b2 =  1.f/3.f,   ins->b2  =  1.0f;
-  // ins->g0 =  11.f/6.f; 
-
-  // // Set-up First Order Solver
-  // ins->lamda = ins->g0 / (ins->dt * ins->nu);
-  // velsolver = ellipticSolveSetupTri2D(mesh, ins->lamda, kernelInfo, velSolverOptions); 
-  // ins->velsolver = velsolver; 
-  // printf("Finished third order solver-setup\n");
-
-
-  // for(iint tstep=2;tstep<ins->NtimeSteps;++tstep){
-  //      //
-  //     insHelmholtzStep2D(ins, tstep, helmholtzHaloBytes, helmholtzSendBuffer, helmholtzRecvBuffer, options);
-  //     insPoissonStep2D(ins, tstep, poissonHaloBytes, poissonSendBuffer, poissonRecvBuffer, options);
-  //     insUpdateStep2D(ins, tstep, updateHaloBytes, updateSendBuffer, updateRecvBuffer, options);
-
-  //    if(strstr(options, "REPORT")){
-  //     if((tstep%ins->errorStep)==0){
-  //       insReport2D(ins, tstep,options);
-  //      }
-  //    }
-  // }
+ //    for(iint tstep=2;tstep<ins->NtimeSteps;++tstep){
+ //      //
+ //      insHelmholtzStep2D(ins, tstep, helmholtzHaloBytes, helmholtzSendBuffer, helmholtzRecvBuffer, options);
+ //      insPoissonStep2D(ins,   tstep, poissonHaloBytes  , poissonSendBuffer  , poissonRecvBuffer  , options);
+ //      insUpdateStep2D(ins, tstep, updateHaloBytes, updateSendBuffer, updateRecvBuffer, options);
+ //     if(strstr(options, "REPORT")){
+ //      if((tstep%ins->errorStep)==0){
+ //        insReport2D(ins, tstep,options);
+ //       }
+ //     }
+ //  }
   
-  // // For Final Time
-  // insReport2D(ins, ins->NtimeSteps,options);
+ //  // // For Final Time
+ //  insReport2D(ins, ins->NtimeSteps,options);
 
-  occa::printTimer();
+ //  occa::printTimer();
 
   // // Deallocate Halo MPI storage
   free(helmholtzSendBuffer);
@@ -154,8 +146,8 @@ void insRun2D(ins_t *ins, char *options){
   free(poissonSendBuffer);
   free(poissonRecvBuffer);
   // //
-  // free(updateSendBuffer);
-  // free(updateRecvBuffer);
+  free(updateSendBuffer);
+  free(updateRecvBuffer);
   //
 
 }
