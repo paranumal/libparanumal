5b9f4614d9f158e82d5c916e465c19c311c026be
AliKarakus <alikarakus.me@gmail.com>
2017-06-01 15:51:33 -0400

    Addinf elliptic bcs
    Former-commit-id: d82bc9c0c69d586571b1a5955de3be00c4dd4377

--- examples/insTri2D/insHelmholtzStep2D.c
+++ examples/insTri2D/insHelmholtzStep2D.c
@@ -103,33 +103,75 @@ void insHelmholtzStep2D(ins_t *ins, iint tstep,  iint haloBytes,
                               );
 
 
-  ins->helmholtzRhsIpdgBCKernel(mesh->Nelements,
-                                mesh->o_sgeo,
-                                mesh->o_vgeo,
-                                mesh->o_DrT,
-                                mesh->o_DsT,
-                                mesh->o_FMMT,
-                                mesh->o_vmapM,
-                                mesh->o_vmapP,
-                                mesh->o_EToB,
-                                t,
-                                ins->tau,
-                                mesh->o_x,
-                                mesh->o_y,
-                                ins->o_Ux,
-                                ins->o_Uy,
-                                ins->o_rhsUx,
-                                ins->o_rhsUy
-                                );
+  // ins->helmholtzRhsIpdgBCKernel(mesh->Nelements,
+  //                               mesh->o_sgeo,
+  //                               mesh->o_vgeo,
+  //                               mesh->o_DrT,
+  //                               mesh->o_DsT,
+  //                               mesh->o_FMMT,
+  //                               mesh->o_vmapM,
+  //                               mesh->o_vmapP,
+  //                               mesh->o_EToB,
+  //                               t,
+  //                               ins->tau,
+  //                               mesh->o_x,
+  //                               mesh->o_y,
+  //                               ins->o_Ux,
+  //                               ins->o_Uy,
+  //                               ins->o_rhsUx,
+  //                               ins->o_rhsUy
+  //                               );
 
 
 
 
 // USE STREAMING LATER!!!!!!!
   // SOLVE HELMHOLTZ EQUATION for ins->o_U
+#if 1
+ ins->o_Ux.copyTo(ins->Ux);
+ ins->o_rhsUx.copyTo(ins->rhsUx);
+
+ dfloat maxU = 0, minU = 1e9;
+ dfloat maxR = 0, minR = 1e9;
+
+ for(iint e=0;e<mesh->Nelements;++e){
+      for(int n=0;n<mesh->Np;++n){
+        iint id = n+e*mesh->Np;
+        maxU  = mymax(maxU, fabs(ins->Ux[id]));
+        minU  = mymin(minU, fabs(ins->Ux[id]));
+        //
+        maxR = mymax(maxR, fabs(ins->rhsUx[id]));
+        minR = mymin(minR, fabs(ins->rhsUx[id]));
+      }
+    }
+
+  printf("minU: %g maxU: %g minR: %g maxR: %g \n", minU, maxU, minR, maxR);   
+#endif  
+
 printf("Solving for Ux \n");
  ellipticSolveTri2D( solver, ins->lamda, ins->o_rhsUx, ins->o_Ux, ins->velsolverOptions);
 
+#if 1
+maxU = 0, minU = 1e9;
+maxR = 0, minR = 1e9;
+
+ins->o_Uy.copyTo(ins->Uy);
+ins->o_rhsUy.copyTo(ins->rhsUy);
+
+ for(iint e=0;e<mesh->Nelements;++e){
+      for(int n=0;n<mesh->Np;++n){
+        iint id = n+e*mesh->Np;
+        maxU  = mymax(maxU, fabs(ins->Uy[id]));
+        minU  = mymin(minU, fabs(ins->Uy[id]));
+        //
+        maxR = mymax(maxR, fabs(ins->rhsUy[id]));
+        minR = mymin(minR, fabs(ins->rhsUy[id]));
+      }
+    }
+
+  printf("minU: %g maxU: %g minR: %g maxR: %g \n", minU, maxU, minR, maxR);   
+
+#endif
 printf("Solving for Uy \n");
   ellipticSolveTri2D(solver, ins->lamda, ins->o_rhsUy, ins->o_Uy, ins->velsolverOptions);
 
