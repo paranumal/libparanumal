320c057e570f7d9f91c543ba4edc0bb2ba8a29a0
Tim Warburton <tim.warburton@gmail.com>
2017-06-11 11:44:00 -0400

    fixed plot vtu for new storage arrangement

--- examples/insTri2D/insAdvectionStep2D.c
+++ examples/insTri2D/insAdvectionStep2D.c
@@ -13,23 +13,23 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 
   //Exctract Halo On Device
   if(mesh->totalHaloPairs>0){
-     ins->totalHaloExtractKernel(mesh->Nelements,
-                                 mesh->totalHaloPairs,
-                                 mesh->o_haloElementList,
-                                 offset,
-                                 ins->o_U,
-                                 ins->o_V,
-                                 ins->o_P,
-                                 ins->o_tHaloBuffer);
+    ins->totalHaloExtractKernel(mesh->Nelements,
+				mesh->totalHaloPairs,
+				mesh->o_haloElementList,
+				offset,
+				ins->o_U,
+				ins->o_V,
+				ins->o_P,
+				ins->o_tHaloBuffer);
 
-     // copy extracted halo to HOST 
-     ins->o_tHaloBuffer.copyTo(sendBuffer);            
+    // copy extracted halo to HOST 
+    ins->o_tHaloBuffer.copyTo(sendBuffer);            
     
-     // start halo exchange
-     meshHaloExchangeStart(mesh,
-                           mesh->Np*(ins->NTfields)*sizeof(dfloat), 
-                           sendBuffer,
-                           recvBuffer);
+    // start halo exchange
+    meshHaloExchangeStart(mesh,
+			  mesh->Np*(ins->NTfields)*sizeof(dfloat), 
+			  sendBuffer,
+			  recvBuffer);
   }
 
   // Compute Volume Contribution
@@ -39,9 +39,9 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 				       mesh->o_cubDrWT,
 				       mesh->o_cubDsWT,
 				       mesh->o_cubInterpT,
-               offset,
-               ins->o_U,
-               ins->o_V,
+				       offset,
+				       ins->o_U,
+				       ins->o_V,
 				       ins->o_NU,
 				       ins->o_NV);
   } else {
@@ -49,9 +49,9 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 			       mesh->o_vgeo,
 			       mesh->o_DrT,
 			       mesh->o_DsT,
-             offset,
-             ins->o_U,
-             ins->o_V,
+			       offset,
+			       ins->o_U,
+			       ins->o_V,
 			       ins->o_NU,
 			       ins->o_NV);
   }
@@ -73,13 +73,13 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 
     ins->o_tHaloBuffer.copyFrom(recvBuffer); 
     ins->totalHaloScatterKernel(mesh->Nelements,
-                                    mesh->totalHaloPairs,
-                                    mesh->o_haloElementList,
-                                    offset,
-                                    ins->o_U,
-                                    ins->o_V,
-                                    ins->o_P,
-                                    ins->o_tHaloBuffer);
+				mesh->totalHaloPairs,
+				mesh->o_haloElementList,
+				offset,
+				ins->o_U,
+				ins->o_V,
+				ins->o_P,
+				ins->o_tHaloBuffer);
   }
 
 
@@ -94,9 +94,9 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 					t,
 					mesh->o_intx,
 					mesh->o_inty,
-          offset,
-          ins->o_U,
-          ins->o_V,
+					offset,
+					ins->o_U,
+					ins->o_V,
 					ins->o_NU,
 					ins->o_NV);
   } else {
@@ -109,30 +109,30 @@ void insAdvectionStep2D(ins_t *ins, iint tstep,  iint haloBytes,
 				t,
 				mesh->o_x,
 				mesh->o_y,
-        offset,
-        ins->o_U,
-        ins->o_V,
+				offset,
+				ins->o_U,
+				ins->o_V,
 				ins->o_NU,
 				ins->o_NV);
   }
 
   // Compute Surface Conribution
   ins->gradientSurfaceKernel(mesh->Nelements,
-                            mesh->o_sgeo,
-                            mesh->o_LIFTT,
-                            mesh->o_vmapM,
-                            mesh->o_vmapP,
-                            mesh->o_EToB,
-                            mesh->o_x,
-                            mesh->o_y,
-                            t,
-                            ins->dt, 
-                            ins->a0,
-                            ins->a1,
-                            ins->a2,
-                            offset,
-                            0, // Normal pressure BCs
-                            ins->o_P,
-                            ins->o_Px,
-                            ins->o_Py);
+			     mesh->o_sgeo,
+			     mesh->o_LIFTT,
+			     mesh->o_vmapM,
+			     mesh->o_vmapP,
+			     mesh->o_EToB,
+			     mesh->o_x,
+			     mesh->o_y,
+			     t,
+			     ins->dt, 
+			     ins->a0,
+			     ins->a1,
+			     ins->a2,
+			     offset,
+			     0, // Normal pressure BCs
+			     ins->o_P,
+			     ins->o_Px,
+			     ins->o_Py);
 }
