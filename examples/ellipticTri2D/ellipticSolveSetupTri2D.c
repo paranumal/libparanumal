040f4a6d2100cedda48b381e8a65f7eebdc859e3
Tim Warburton <tim.warburton@gmail.com>
2017-06-28 15:39:17 -0400

    bug fix

--- examples/ellipticTri2D/ellipticSolveSetupTri2D.c
+++ examples/ellipticTri2D/ellipticSolveSetupTri2D.c
@@ -96,7 +96,7 @@ solver_t *ellipticSolveSetupTri2D(mesh_t *mesh, dfloat tau, dfloat lambda, iint*
   int NblockV = 256/mesh->Np; // get close to 256 threads
   kernelInfo.addDefine("p_NblockV", NblockV);
 
-  int NblockP = 256/(4*mesh->Np); // get close to 256 threads
+  int NblockP = 512/(4*mesh->Np); // get close to 256 threads
   kernelInfo.addDefine("p_NblockP", NblockP);
 
   mesh->haloExtractKernel =
