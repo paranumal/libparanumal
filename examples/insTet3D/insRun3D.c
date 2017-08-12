50faf65413e2d7435e40c6eb38b4a2182f533946
AliKarakus <alikarakus.me@gmail.com>
2017-08-12 00:41:12 -0400

    Testing wall-bounded square cylinder 3D
    Former-commit-id: 81145afe539a95de4efce1372828c539e4ad9a51

--- examples/insTet3D/insRun3D.c
+++ examples/insTet3D/insRun3D.c
@@ -4,7 +4,7 @@ void insRun3D(ins_t *ins, char *options){
 
   mesh3D *mesh = ins->mesh;
   // Write Initial Data
-  insReport3D(ins, 0, options);
+  //insReport3D(ins, 0, options);
   
   //insErrorNorms3D(ins, 0, options);
   // Allocate MPI buffer for velocity step solver!! May Change Later!!!!!!
@@ -28,7 +28,7 @@ void insRun3D(ins_t *ins, char *options){
   occa::initTimer(mesh->device);
   //ins->NtimeSteps = 10; // !!!!!!!!!!!!!
   for(iint tstep=0;tstep<ins->NtimeSteps;++tstep){
-  #if 0
+  #if 1
     // ok it seems 
     if(tstep<100){
       // no advection, first order in time
@@ -51,8 +51,7 @@ void insRun3D(ins_t *ins, char *options){
       ins->b2 =  0.f,  ins->a2 =  0.f,  ins->c2 = 0.0f;
       ins->g0 =  1.5f;
     }
-    else 
-      //if(tstep<400)
+    else if(tstep<400)
     {
       // advection, second order in time, first order increment
       ins->b0 =  2.f,  ins->a0 =  2.0f, ins->c0 = 1.0f;  // 2
@@ -60,12 +59,12 @@ void insRun3D(ins_t *ins, char *options){
       ins->b2 =  0.f,  ins->a2 =  0.f,  ins->c2 = 0.0f;
       ins->g0 =  1.5f;
     }
-    // else{
-    //   ins->b0 =  3.f,       ins->a0  =  3.0f, ins->c0 = 1.0f;
-    //   ins->b1 = -1.5f,      ins->a1  = -3.0f, ins->c1 = 0.0f;
-    //   ins->b2 =  1.f/3.f,   ins->a2  =  1.0f, ins->c2 =  0.0f;
-    //   ins->g0 =  11.f/6.f;
-    // }
+    else{
+      ins->b0 =  3.f,       ins->a0  =  3.0f, ins->c0 = 1.0f;
+      ins->b1 = -1.5f,      ins->a1  = -3.0f, ins->c1 = 0.0f;
+      ins->b2 =  1.f/3.f,   ins->a2  =  1.0f, ins->c2 =  0.0f;
+      ins->g0 =  11.f/6.f;
+    }
   #else
    if(tstep<1){
        //advection, first order in time, increment
@@ -91,15 +90,15 @@ void insRun3D(ins_t *ins, char *options){
     insPoissonStep3D(  ins, tstep, vHaloBytes,vSendBuffer,vRecvBuffer, options);
     insUpdateStep3D(   ins, tstep, pHaloBytes,pSendBuffer,pRecvBuffer, options);
     //
-    printf("tstep = %d\n", tstep);
-    if(strstr(options, "REPORT")){
-      if(((tstep+1)%(ins->errorStep))==0){
-        //insReport3D(ins, tstep+1,options);
-        //insErrorNorms3D(ins, (tstep+1)*ins->dt, options);
-      }
-    }
+    // printf("tstep = %d\n", tstep);
+    // if(strstr(options, "REPORT")){
+    //   if(((tstep+1)%(ins->errorStep))==0){
+    //     insReport3D(ins, tstep+1,options);
+    //     insErrorNorms3D(ins, (tstep+1)*ins->dt, options);
+    //   }
+    // }
     
-#if 1 // For time accuracy test fed history with exact solution
+#if 0 // For time accuracy test fed history with exact solution
     if(tstep<1){
       iint offset = (mesh->Nelements+mesh->totalHaloPairs);
      // Overwrite Velocity
@@ -155,8 +154,8 @@ void insRun3D(ins_t *ins, char *options){
 #if 1
 // For Final Time
 insReport3D(ins, ins->NtimeSteps+1,options);
-dfloat finaltime = (ins->NtimeSteps+1)*ins->dt;
-insErrorNorms3D(ins, finaltime, options);
+//dfloat finaltime = (ins->NtimeSteps+1)*ins->dt;
+//insErrorNorms3D(ins, finaltime, options);
 #endif
 
 //Deallocate Halo MPI storage
