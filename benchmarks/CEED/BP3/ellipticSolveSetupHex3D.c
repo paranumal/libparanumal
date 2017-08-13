ac14fbcfdd0decd87e7eb9a70d9ae5b0ff830bb3
kswirydo <kasia.swirydowicz@gmail.com>
2017-08-13 17:06:04 -0400

    1d: sliced kernel for k slices processed together

--- benchmarks/CEED/BP3/ellipticSolveSetupHex3D.c
+++ benchmarks/CEED/BP3/ellipticSolveSetupHex3D.c
@@ -291,8 +291,8 @@ printf("p_blockSize = %d \n", blockSize);
 
 			printf("building e9 kernel \n");
 			solver->partialAxKernel =
-				saferBuildKernelFromSource(mesh->device, DHOLMES "/okl/ellipticAxHex3D.okl",
-				                           "ellipticAxHex3D_e1c",
+				saferBuildKernelFromSource(mesh->device, DHOLMES "/okl/ellipticAxHex3DKS.okl",
+				                           "ellipticAxHex3D_e1d",
 				                           kernelInfo);
 
 
