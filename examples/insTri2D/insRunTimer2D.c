5e5373e6717e47b3d49a02ff165ebbed02e47f7d
tcew <tcew@nrlogin1.cluster>
2017-10-26 19:04:41 -0400

    added dfloat2 initial timings for insSubCycycleVolume kernel
    Former-commit-id: 380f87a4b6896af01b132e798de39b973dfe3c32

--- examples/insTri2D/insRunTimer2D.c
+++ examples/insTri2D/insRunTimer2D.c
@@ -94,7 +94,7 @@ void insRunTimer2D(mesh2D *mesh, char *options, char *boundaryHeaderFileName){
   occa::kernel TestKernel; 
 
   #if KERNEL_TEST==1
-  int NKernels = 5;
+  int NKernels = 6;
 
   occa::kernel *testKernels = new occa::kernel[NKernels];
   char kernelNames[NKernels][BUFSIZ];
