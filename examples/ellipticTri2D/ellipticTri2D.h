0ef7e2cefaaf7955e863f8a5d5f3a595bab6c91e
kswirydo <kasia.swirydowicz@gmail.com>
2017-12-31 01:48:07 -0500

    v5 kernel with sparse storage row-wise for ellipticAxTri2D

--- examples/ellipticTri2D/ellipticTri2D.h
+++ examples/ellipticTri2D/ellipticTri2D.h
@@ -108,7 +108,7 @@ void diagnostic(int N, occa::memory &o_x, const char *message);
 
 void ellipticPreconditioner2D(solver_t *solver, dfloat lambda, occa::memory &o_r,occa::memory &o_z,const char *options);
 
-int ellipticSolveTri2D(solver_t *solver, dfloat lambda, dfloat tol, occa::memory &o_r, occa::memory &o_x, const char *options);
+int ellipticSolveTri2D(solver_t *solver, dfloat lambda, dfloat tol, occa::memory &o_r, occa::memory &o_x, const char *options, iint NblockV, iint NnodesV);
 
 solver_t *ellipticSolveSetupTri2D(mesh_t *mesh, dfloat tau, dfloat lambda, iint *BCType, occa::kernelInfo &kernelInfo, const char *options, const char *parAlmondOptions, iint NblockV, iint NnodesV);
 
