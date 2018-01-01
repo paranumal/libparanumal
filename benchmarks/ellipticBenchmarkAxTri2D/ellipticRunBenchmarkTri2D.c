b760dacd19ded3861d7f755b7c89e5a8348e573d
tcew <tcew@nrlogin1.cluster>
2018-01-01 09:25:31 -0500

    starting debug

--- benchmarks/ellipticBenchmarkAxTri2D/ellipticRunBenchmarkTri2D.c
+++ benchmarks/ellipticBenchmarkAxTri2D/ellipticRunBenchmarkTri2D.c
@@ -46,32 +46,33 @@ void ellipticRunBenchmark2D(solver_t *solver, char *options, occa::kernelInfo ke
 
     double  copyElapsed = mesh->device.timeBetween(startCopy, endCopy);
 
-
-
-double timeAx = 0.0f;
-double kernelElapsed =0.0f;;
+    // time Ax
+    double timeAx = 0.0f;
+    double kernelElapsed =0.0f;;
     occa::streamTag start, end;
     for(int it=0;it<Ntrials;++it){
       mesh->device.finish();
-start = mesh->device.tagStream();
+      start = mesh->device.tagStream();
 
       testKernel(mesh->Nelements, 
-          solver->o_localGatherElementList,
-          mesh->o_ggeo, 
-          mesh->o_SrrT, 
-          mesh->o_SrsT, 
-          mesh->o_SsrT, 
-          mesh->o_SssT,
-          mesh->o_MM, 
-          lambda,
-          solver->o_p,
-          solver->o_grad);
-    end = mesh->device.tagStream();
-    timeAx = mesh->device.timeBetween(start,end);
-kernelElapsed +=timeAx;    
-}
+		 solver->o_localGatherElementList,
+		 mesh->o_ggeo, 
+		 mesh->o_SrrT, 
+		 mesh->o_SrsT, 
+		 //		 mesh->o_SsrT, 
+		 mesh->o_IndTchar,
+		 mesh->o_SssT,
+		 mesh->o_MM, 
+		 lambda,
+		 solver->o_p,
+		 solver->o_grad);
+
+      end = mesh->device.tagStream();
+      timeAx = mesh->device.timeBetween(start,end);
+      kernelElapsed +=timeAx;    
+    }
     mesh->device.finish();
-kernelElapsed = kernelElapsed/(dfloat)Ntrials;
+    kernelElapsed = kernelElapsed/(dfloat)Ntrials;
 
     //start
     //
@@ -94,7 +95,8 @@ kernelElapsed = kernelElapsed/(dfloat)Ntrials;
     if (rank==0){
       printf("Ntrials = %d globla copy el %f local copy el %f size(dfloat) %d \n",Ntrials, globalCopyElapsed, copyElapsed, sizeof(dfloat));
 
-      iint flops = mesh->Np*(mesh->Np*6+5);
+      //      iint flops = mesh->Np*(mesh->Np*6+5);
+      iint flops = mesh->Np*(mesh->maxNnzPerRow*6+5);
 
       double roofline = ((dfloat)(mesh->Nelements*flops*Ntrials))/(1e9*globalCopyElapsed);
       printf("Nelements = %d flops = %d Ntials = %d copy elapsed scaled = %f \n",mesh->Nelements, flops, Ntrials,  1e9*globalCopyElapsed);
@@ -102,7 +104,8 @@ kernelElapsed = kernelElapsed/(dfloat)Ntrials;
       double copyBandwidth   = mesh->Nelements*((Nbytes*Ntrials*2.)/(1e9*globalCopyElapsed));
       double kernelBandwidth = mesh->Nelements*((Nbytes*2.)/(1e9*kernelElapsed));
       double kernelGFLOPS = mesh->Nelements*flops/(1e9*kernelElapsed);
-      printf("copy BW %16.17g achieved BW %16.17g\n", copyBandwidth, kernelBandwidth);
+
+      printF("Copy Bw %16.17g achieved BW %16.17g\n", copyBandwidth, kernelBandwidth);
       printf("ROOFLINE %16.17g \n", roofline);
       printf("GFLOPS %16.17f \n", kernelGFLOPS);
       printf("time per kernel %f \n",kernelElapsed);
