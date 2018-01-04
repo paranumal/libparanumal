a5760cf47516cb5a026f5d562ed8cf81c923651b
Noel Chalmers <noel.chalmers@gmail.com>
2018-01-04 10:20:48 -0500

    Some code cleanup and restored the partialAx kernel in the ellipticAx benchmarks

--- benchmarks/ellipticBenchmarkAxTri2D/ellipticSetupTri2D.c
+++ benchmarks/ellipticBenchmarkAxTri2D/ellipticSetupTri2D.c
@@ -158,10 +158,10 @@ solver_t *ellipticSetupTri2D(mesh_t *mesh, dfloat tau, dfloat lambda, iint*BCTyp
   kernelInfo.addDefine("p_NblockS", Nblocks);
   kernelInfo.addDefine("p_NblockP", Nblocks);
   kernelInfo.addDefine("p_NblockG", Nblocks);
- iint *globalGatherElementList    = (iint*) calloc(mesh->Nelements, sizeof(iint));
+  iint *globalGatherElementList    = (iint*) calloc(mesh->Nelements, sizeof(iint));
   iint *localGatherElementList = (iint*) calloc(mesh->Nelements, sizeof(iint));
-int globalCount = 0;
-int localCount =0;
+  int globalCount = 0;
+  int localCount =0;
 
   for(iint e=0;e<mesh->Nelements;++e){
       globalGatherElementList[globalCount++] = e;
@@ -176,9 +176,6 @@ int localCount =0;
 
     solver->o_localGatherElementList =
       mesh->device.malloc(localCount*sizeof(iint), localGatherElementList);
-
-
-
 
   return solver;
 }
