47826d206f682957ecb464f9a31e58ef9e0c0fa6
Tim Warburton <tim.warburton@gmail.com>
2018-01-12 17:17:16 -0500

    tweaked initial conditions

--- examples/boltzmannQuad3D/boltzmannSetupQuad3D.c
+++ examples/boltzmannQuad3D/boltzmannSetupQuad3D.c
@@ -1,5 +1,30 @@
 #include "boltzmannQuad3D.h"
 
+void brownMinion(dfloat bmRho, dfloat bmDelta, dfloat sphereRadius,
+		 dfloat x, dfloat y, dfloat z,
+		 dfloat *u, dfloat *v, dfloat *w){
+
+  dfloat Utangential = 0.25*(1+tanh(bmRho*(-z+0.5)))*(1+tanh(bmRho*(0.5+z)));
+  
+  dfloat uout, vout;
+  
+  if(x*x+y*y>1e-4) {
+    uout =  -y*Utangential/(x*x+y*y);
+    vout =   x*Utangential/(x*x+y*y);
+  }
+  else{
+    uout = 0;
+    vout = 0;
+  }
+  
+  dfloat wout = bmDelta*sin(2*atan2(y,x))*(1-z*z);
+  
+  dfloat udotx = uout*x+vout*y+wout*z;
+  *u = uout - udotx*x/(sphereRadius*sphereRadius);
+  *v = vout - udotx*y/(sphereRadius*sphereRadius);
+  *w = wout - udotx*z/(sphereRadius*sphereRadius);
+
+}
 solver_t *boltzmannSetupQuad3D(mesh_t *mesh){
 
   solver_t *solver = (solver_t*) calloc(1, sizeof(solver_t));
@@ -19,31 +44,73 @@ solver_t *boltzmannSetupQuad3D(mesh_t *mesh){
   // set temperature, gas constant, wave speeds
   mesh->RT = 9.;
   mesh->sqrtRT = sqrt(mesh->RT);  
+  dfloat nu = 2.e-4;  // was 6.e-3
   
   // initial conditions
-  // uniform flow
-  dfloat rho = 1, u = 0, v = 0, w = 0; 
-  dfloat sigma11 = 0, sigma12 = 0, sigma13 = 0, sigma22 = 0, sigma23 = 0, sigma33 = 0;
-
-  //  dfloat ramp = 0.5*(1.f+tanh(10.f*(0-.5f)));
-  dfloat q1bar = rho;
-  dfloat q2bar = rho*u/mesh->sqrtRT;
-  dfloat q3bar = rho*v/mesh->sqrtRT;
-  dfloat q4bar = rho*w/mesh->sqrtRT;
-  dfloat q5bar = (rho*u*u - sigma11)/(sqrt(2.)*mesh->RT);
-  dfloat q6bar = (rho*v*v - sigma22)/(sqrt(2.)*mesh->RT);
-  dfloat q7bar = (rho*w*w - sigma33)/(sqrt(2.)*mesh->RT);
-  dfloat q8bar  = (rho*u*v - sigma12)/mesh->RT;
-  dfloat q9bar =  (rho*u*w - sigma13)/mesh->RT;
-  dfloat q10bar = (rho*v*w - sigma23)/mesh->RT;
-
+  dfloat sR = mesh->sphereRadius;
+  
   iint cnt = 0;
   for(iint e=0;e<mesh->Nelements;++e){
     for(iint n=0;n<mesh->Np;++n){
-      dfloat t = 0;
+
       dfloat x = mesh->x[n + mesh->Np*e];
       dfloat y = mesh->y[n + mesh->Np*e];
       dfloat z = mesh->z[n + mesh->Np*e];
+
+      // Brown Minion shear layer roll up
+      dfloat bmRho = 40;
+      dfloat bmDelta  = 0.05;
+
+      dfloat rho = 1;
+
+      dfloat umod, vmod, wmod;
+      
+      brownMinion(bmRho, bmDelta, sR, x, y, z, &umod, &vmod, &wmod);
+
+      dfloat delta = 1e-5;
+
+      dfloat uP, uM, vP, vM, wP, wM;
+      
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x+delta, y, z, &uP, &vP, &wP);
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x-delta, y, z, &uM, &vM, &wM);
+
+      dfloat dudx = (uP-uM)/(2*delta);
+      dfloat dvdx = (vP-vM)/(2*delta);
+      dfloat dwdx = (wP-wM)/(2*delta);
+
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x, y+delta, z, &uP, &vP, &wP);
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x, y-delta, z, &uM, &vM, &wM);
+      
+      dfloat dudy = (uP-uM)/(2*delta);
+      dfloat dvdy = (vP-vM)/(2*delta);
+      dfloat dwdy = (wP-wM)/(2*delta);
+
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x, y, z+delta, &uP, &vP, &wP);
+      brownMinion(bmRho, bmDelta, mesh->sphereRadius, x, y, z-delta, &uM, &vM, &wM);
+      
+      dfloat dudz = (uP-uM)/(2*delta);
+      dfloat dvdz = (vP-vM)/(2*delta);
+      dfloat dwdz = (wP-wM)/(2*delta);
+      
+      dfloat sigma11 = nu*(dudx+dudx);
+      dfloat sigma12 = nu*(dvdx+dudy);
+      dfloat sigma13 = nu*(dwdx+dudz);
+      dfloat sigma22 = nu*(dvdy+dvdy);
+      dfloat sigma23 = nu*(dwdy+dvdz);
+      dfloat sigma33 = nu*(dwdz+dwdz);
+			     
+      dfloat q1bar = rho;
+      dfloat q2bar = rho*umod/mesh->sqrtRT;
+      dfloat q3bar = rho*vmod/mesh->sqrtRT;
+      dfloat q4bar = rho*wmod/mesh->sqrtRT;
+      dfloat q5bar = (rho*umod*umod - sigma11)/(sqrt(2.)*mesh->RT);
+      dfloat q6bar = (rho*vmod*vmod - sigma22)/(sqrt(2.)*mesh->RT);
+      dfloat q7bar = (rho*wmod*wmod - sigma33)/(sqrt(2.)*mesh->RT);
+      dfloat q8bar  = (rho*umod*vmod - sigma12)/mesh->RT;
+      dfloat q9bar =  (rho*umod*wmod - sigma13)/mesh->RT;
+      dfloat q10bar = (rho*vmod*wmod - sigma23)/mesh->RT;
+      
+      dfloat t = 0;
 
       int base = n + e*mesh->Np*mesh->Nfields;
 
@@ -52,39 +119,12 @@ solver_t *boltzmannSetupQuad3D(mesh_t *mesh){
       q1bar = 1 + .1*exp(-20*((x-1)*(x-1)+y*y+z*z));
 #endif
 
-      // Brown Minion shear layer roll up
-      q1bar = 1;
-      dfloat delta = 40;
-      dfloat Utangential = 0.25*(1+tanh(delta*(-z+0.5)))*(1+tanh(delta*(0.5+z)));
-
-      if(x*x+y*y>1e-4) {
-	q2bar =  (q1bar/mesh->sqrtRT)*(-y*Utangential)/(x*x+y*y);
-	q3bar =  (q1bar/mesh->sqrtRT)*( x*Utangential)/(x*x+y*y);
-      }
-      else{
-	q2bar = 0;
-	q3bar = 0;
-      }
-
-      dfloat epsilon  = 0.05;
-      if(x*x+y*y>1e-5)
-	q4bar = epsilon*y/sqrt(x*x+y*y);
-      else
-	q4bar = 0;
-
-      //      printf("x=%g, y=%g, z=%g, q2,3,4 = %g,%g,%g\n",
-      //	     x, y, z, q2bar, q3bar, q4bar);
-      
-      dfloat qdotx = q2bar*x + q3bar*y + q4bar*z;
-      dfloat q2mod = q2bar - qdotx*x/(mesh->sphereRadius*mesh->sphereRadius);
-      dfloat q3mod = q3bar - qdotx*y/(mesh->sphereRadius*mesh->sphereRadius);
-      dfloat q4mod = q4bar - qdotx*z/(mesh->sphereRadius*mesh->sphereRadius);
 
       mesh->q[base+0*mesh->Np] = q1bar; // uniform density, zero flow
 
-      mesh->q[base+1*mesh->Np] = q2mod;
-      mesh->q[base+2*mesh->Np] = q3mod;
-      mesh->q[base+3*mesh->Np] = q4mod;
+      mesh->q[base+1*mesh->Np] = q2bar;
+      mesh->q[base+2*mesh->Np] = q3bar;
+      mesh->q[base+3*mesh->Np] = q4bar;
 
       mesh->q[base+4*mesh->Np] = q5bar;
       mesh->q[base+5*mesh->Np] = q6bar;
@@ -102,7 +142,7 @@ solver_t *boltzmannSetupQuad3D(mesh_t *mesh){
   //  dfloat nu = 1.e-3/.5;
   //  dfloat nu = 5.e-4;
   //    dfloat nu = 1.e-2; TW works for start up fence
-  dfloat nu = 2.e-4;  // was 6.e-3
+
   mesh->tauInv = mesh->RT/nu; // TW
   
   // set time step
