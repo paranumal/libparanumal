9e4e4666d27696d2b5e79942135b07f5d7f6e467
tcew <tim.warburton@gmail.com>
2018-01-13 09:22:19 -0500

    boltzmann triangle 3D example. Need to switch to skew symmetric version

---
+++ examples/boltzmannTri3D/boltzmannTri3D.h
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+#include "mesh3D.h"
+
+typedef struct {
+  
+  mesh_t   *mesh;
+
+  char *options;
+  
+  // INS SOLVER OCCA VARIABLES
+  dfloat rho, nu;
+
+  dfloat dt;          // time step
+  dfloat lambda;      // helmhotz solver -lap(u) + lamda u
+  dfloat finalTime;   // final time to run acoustics to
+  iint   NtimeSteps;  // number of time steps 
+  iint   Nstages;     // Number of history states to store
+  iint   index;       // Index of current state
+  iint   errorStep; 
+
+  dfloat a0, a1, a2, b0, b1, b2, c0, c1, c2, g0, tau; 
+
+#if 0
+  dfloat *resQ;
+  dfloat *rhsQ;
+  dfloat *Q;
+#endif
+  
+  iint Nsubsteps;  
+
+  //  occa::memory o_Q, o_resQ, o_rhsQ;
+  
+  occa::kernel volumeKernel;
+  occa::kernel surfaceKernel;
+  occa::kernel rkUpdateKernel;
+
+  occa::memory o_haloBuffer;
+
+  occa::kernel haloExtractKernel;
+  occa::kernel haloScatterKernel;
+
+}solver_t;
+
+
+solver_t *boltzmannSetupTri3D(mesh_t *mesh);
+
+void boltzmannRunTri3D(solver_t *solver);
+
+void boltzmannPlotVTUTri3D(mesh_t *mesh, char *fileNameBase, iint fld);
+void boltzmannPlotVTUTri3DV2(mesh_t *mesh, char *fileNameBase, iint tstep);
+
+void boltzmannOccaSetupTri3D(mesh_t *mesh, char *deviceConfig, occa::kernelInfo &kernelInfo);
+
