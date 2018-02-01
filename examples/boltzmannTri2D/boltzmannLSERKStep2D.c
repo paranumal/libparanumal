21b1d0496d6913740c8162a1b6bc8f3269648d73
AliKarakus <alikarakus.me@gmail.com>
2018-02-01 10:13:07 -0500

    No cubature for sigma terms for testing

--- examples/boltzmannTri2D/boltzmannLSERKStep2D.c
+++ examples/boltzmannTri2D/boltzmannLSERKStep2D.c
@@ -113,31 +113,31 @@ void boltzmannLSERKStep2D(mesh2D *mesh, iint tstep, iint haloBytes,
 			mesh->device.finish();
            occa::tic("PML_relaxationKernel");
 
-		  mesh->pmlRelaxationKernel(mesh->pmlNelements,
+		  // mesh->pmlRelaxationKernel(mesh->pmlNelements,
+    //                             mesh->o_pmlElementIds,
+    //                             mesh->o_pmlIds,
+    //                             mesh->Nrhs,
+    //                             mesh->shiftIndex,
+    //                             mesh->o_cubInterpT,
+    //                             mesh->o_cubProjectT,
+    //                             mesh->o_pmlSigmaX,
+    //                             mesh->o_pmlSigmaY,
+    //                             mesh->o_q,
+    //                             mesh->o_pmlqx,
+    //                             mesh->o_pmlqy,
+    //                             mesh->o_rhsq,
+    //                             mesh->o_pmlrhsqx,
+    //                             mesh->o_pmlrhsqy);
+
+        mesh->pmlRelaxationKernel(mesh->pmlNelements,
                                 mesh->o_pmlElementIds,
                                 mesh->o_pmlIds,
                                 mesh->Nrhs,
                                 mesh->shiftIndex,
                                 mesh->o_cubInterpT,
                                 mesh->o_cubProjectT,
-                                mesh->o_pmlSigmaX,
-                                mesh->o_pmlSigmaY,
                                 mesh->o_q,
-                                mesh->o_pmlqx,
-                                mesh->o_pmlqy,
-                                mesh->o_rhsq,
-                                mesh->o_pmlrhsqx,
-                                mesh->o_pmlrhsqy);
-
-        // mesh->pmlRelaxationKernel(mesh->pmlNelements,
-        //                         mesh->o_pmlElementIds,
-        //                         mesh->o_pmlIds,
-        //                         mesh->Nrhs,
-        //                         mesh->shiftIndex,
-        //                         mesh->o_cubInterpT,
-        //                         mesh->o_cubProjectT,
-        //                         mesh->o_q,
-        //                         mesh->o_rhsq);
+                                mesh->o_rhsq);
 
 
 
