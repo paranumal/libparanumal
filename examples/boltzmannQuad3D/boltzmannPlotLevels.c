be4c69ffc30b1a99b45efd42590ef16b4b2780a9
Stephen Timmel <stimmel@princeton.edu>
2018-02-14 19:11:28 -0500

    Implemented debug version of filter timestamps
    Former-commit-id: 1aa8fb2e26862d08213e2434017d8d3534e7543b

--- examples/boltzmannQuad3D/boltzmannPlotLevels.c
+++ examples/boltzmannQuad3D/boltzmannPlotLevels.c
@@ -61,7 +61,7 @@ void boltzmannPlotLevels(mesh_t *mesh, char *fileNameBase, int tstep,dfloat *q){
 
   fprintf(fp, "       </DataArray>\n");
 
-  fprintf(fp, "        <DataArray type=\"Float32\" Name=\"q_value\" Format=\"ascii\">\n");
+  /*fprintf(fp, "        <DataArray type=\"Float32\" Name=\"q_value\" Format=\"ascii\">\n");
   for(iint e=0;e<mesh->Nelements;++e){
     for(iint n=0;n<mesh->plotNp;++n){
 
@@ -74,7 +74,7 @@ void boltzmannPlotLevels(mesh_t *mesh, char *fileNameBase, int tstep,dfloat *q){
       fprintf(fp, "%g\n", plotpn);
     }
   }
-  fprintf(fp, "       </DataArray>\n");
+  fprintf(fp, "       </DataArray>\n");*/
 
   fprintf(fp, "        <DataArray type=\"Float32\" Name=\"cube_faces\" Format=\"ascii\">\n");
   for(iint e=0;e<mesh->Nelements;++e){
