171ca2e05293e2f9b03b728dc502271745de0981
Noel Chalmers <noel.chalmers@gmail.com>
2018-02-15 10:37:18 -0500

    Fixing an issue where advection was turned off
    Former-commit-id: 4361d812a89f45132fd2d9809d667cceffc30f09

--- examples/insTet3D/insRun3D.c
+++ examples/insTet3D/insRun3D.c
@@ -42,28 +42,28 @@ void insRun3D(ins_t *ins, char *options){
       ins->lambda = ins->g0 / (ins->dt * ins->nu);
       ins->idt = 1.0/ins->dt; 
       ins->ig0 = 1.0/ins->g0; 
-    // } else if(tstep<2) {
-    //   //advection, second order in time, increment
-    //   ins->b0 =  2.f,  ins->a0 =  2.0f, ins->c0 = 1.0f;  // 2
-    //   ins->b1 = -0.5f, ins->a1 = -1.0f, ins->c1 = 0.0f; // -1
-    //   ins->b2 =  0.f,  ins->a2 =  0.f,  ins->c2 = 0.0f;
-    //   ins->g0 =  1.5f;
-    //   ins->ExplicitOrder=2;
+    } else if(tstep<2) {
+      //advection, second order in time, increment
+      ins->b0 =  2.f,  ins->a0 =  2.0f, ins->c0 = 1.0f;  // 2
+      ins->b1 = -0.5f, ins->a1 = -1.0f, ins->c1 = 0.0f; // -1
+      ins->b2 =  0.f,  ins->a2 =  0.f,  ins->c2 = 0.0f;
+      ins->g0 =  1.5f;
+      ins->ExplicitOrder=2;
 
-    //   ins->lambda = ins->g0 / (ins->dt * ins->nu);
-    //   ins->idt = 1.0/ins->dt; 
-    //   ins->ig0 = 1.0/ins->g0; 
-    // } else {
-    //   //advection, third order in time, increment
-    //   ins->b0 =  3.f,       ins->a0  =  3.0f, ins->c0 = 1.0f;
-    //   ins->b1 = -1.5f,      ins->a1  = -3.0f, ins->c1 = 0.0f;
-    //   ins->b2 =  1.f/3.f,   ins->a2  =  1.0f, ins->c2 =  0.0f;
-    //   ins->g0 =  11.f/6.f;
-    //   ins->ExplicitOrder=3;
+      ins->lambda = ins->g0 / (ins->dt * ins->nu);
+      ins->idt = 1.0/ins->dt; 
+      ins->ig0 = 1.0/ins->g0; 
+    } else {
+      //advection, third order in time, increment
+      ins->b0 =  3.f,       ins->a0  =  3.0f, ins->c0 = 1.0f;
+      ins->b1 = -1.5f,      ins->a1  = -3.0f, ins->c1 = 0.0f;
+      ins->b2 =  1.f/3.f,   ins->a2  =  1.0f, ins->c2 =  0.0f;
+      ins->g0 =  11.f/6.f;
+      ins->ExplicitOrder=3;
 
-    //   ins->lambda = ins->g0 / (ins->dt * ins->nu);
-    //   ins->idt = 1.0/ins->dt; 
-    //   ins->ig0 = 1.0/ins->g0; 
+      ins->lambda = ins->g0 / (ins->dt * ins->nu);
+      ins->idt = 1.0/ins->dt; 
+      ins->ig0 = 1.0/ins->g0; 
     }
 
     if(strstr(options,"SUBCYCLING")) {
