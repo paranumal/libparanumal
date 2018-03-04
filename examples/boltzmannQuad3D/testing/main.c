367f424aa7284f061bafd7fee0ba541fd138001a
Stephen Timmel <stimmel@princeton.edu>
2018-03-03 19:29:31 -0500

    Preparing to add relaxation kernel
    Former-commit-id: 6a0c9c0b9771f762ca0ff76c124062a51dbe1c9e

--- examples/boltzmannQuad3D/testing/main.c
+++ examples/boltzmannQuad3D/testing/main.c
@@ -8,10 +8,10 @@ int main() {
   char *ref_string = (char *) calloc(300,sizeof(char));
   char *out_string = (char *) calloc(300,sizeof(char));
   
-  for(int i = 1; i < 63; ++i) {
+  for(int i = 0; i < 63; ++i) {
     
     sprintf(test_string,"/scratch/stimmel/filter/norms_0000_%04d.vtu",i);
-    sprintf(ref_string,"/scratch/stimmel/mrab/norms_0000_%04d.vtu",4*i);
+    sprintf(ref_string,"/scratch/stimmel/mrab/norms_0000_%04d.vtu",4*(i+1) - 1);
     
     parse_vtu(ref_string,ref);
     parse_vtu(test_string,test);
