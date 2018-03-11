39d8525eb794807518abed119bf398d74fb0d43c
tcew <tim.warburton@gmail.com>
2018-03-11 17:47:37 -0400

    more files
    Former-commit-id: 1b96cd59098deac5fba43d1e3235584c7c82a977

---
+++ benchmarks/ellipticBenchmarkAxTet3D/ellipticBenchmarkTet3D.h
@@ -0,0 +1,99 @@
+#ifndef ELLIPTICTRI3D_H
+#define ELLIPTICTRI3D_H 1
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+#include "mesh3D.h"
+#include "parAlmond.h"
+
+// block size for reduction (hard coded)
+#define blockSize 256
+
+typedef struct {
+
+  mesh_t *mesh;
+
+  ogs_t *ogs;
+
+  ogs_t *ogsDg;
+
+  // C0 halo gather-scatter info
+  ogs_t *halo;
+
+  // C0 nonhalo gather-scatter info
+  ogs_t *nonHalo;
+
+  char *type;
+
+  int Nblock;
+
+  dfloat tau;
+  dfloat lambda;
+
+  bool allNeumann;
+  dfloat allNeumannPenalty;
+  dfloat allNeumannScale;
+
+  // HOST shadow copies
+  dfloat *Ax, *p, *r, *z, *Ap, *tmp, *grad;
+
+  int *EToB;
+  dfloat *sendBuffer, *recvBuffer;
+  dfloat *gradSendBuffer, *gradRecvBuffer;
+
+  occa::stream defaultStream;
+  occa::stream dataStream;
+
+  occa::memory o_p; // search direction
+  occa::memory o_z; // preconditioner solution
+  occa::memory o_res;
+  occa::memory o_Sres;
+  occa::memory o_Ax; // A*initial guess
+  occa::memory o_Ap; // A*search direction
+  occa::memory o_tmp; // temporary
+  occa::memory o_grad; // temporary gradient storage (part of A*)
+  occa::memory o_rtmp;
+  occa::memory o_invDegree;
+  occa::memory o_EToB;
+
+  // list of elements that are needed for global gather-scatter
+  int NglobalGatherElements;
+  occa::memory o_globalGatherElementList;
+
+  // list of elements that are not needed for global gather-scatter
+  int NlocalGatherElements;
+  occa::memory o_localGatherElementList;
+
+  occa::kernel AxKernel;
+  occa::kernel partialAxKernel;
+  occa::kernel rhsBCKernel;
+  occa::kernel innerProductKernel;
+  occa::kernel weightedInnerProduct1Kernel;
+  occa::kernel weightedInnerProduct2Kernel;
+  occa::kernel scaledAddKernel;
+  occa::kernel dotMultiplyKernel;
+  occa::kernel dotDivideKernel;
+
+  occa::kernel gradientKernel;
+  occa::kernel ipdgKernel;
+  occa::kernel partialGradientKernel;
+  occa::kernel partialIpdgKernel;
+  occa::kernel rhsBCIpdgKernel;
+
+  occa::kernel BRGradientVolumeKernel;
+  occa::kernel BRGradientSurfaceKernel;
+  occa::kernel BRDivergenceVolumeKernel;
+  occa::kernel BRDivergenceSurfaceKernel;
+
+}solver_t;
+
+solver_t *ellipticSetupTri3D(mesh_t *mesh, dfloat tau, dfloat lambda, int *BCType, occa::kernelInfo &kernelInfo, const char *options, const char *parAlmondOptions, int Nblocks, int Nnodes);
+
+void ellipticRunBenchmark3D(solver_t *solver, char *options, occa::kernelInfo kernelInfo, char *kernelFileName, int Nblocks, int Nnodes);
+
+void loadElementStiffnessMatricesTri3D(mesh_t *mesh, const char *options, int N);
+
+#endif
