2fca5e9c96d3200ef673126bb8d7dd4d6cb4794f
Stephen Timmel <stimmel@princeton.edu>
2018-03-26 00:19:25 -0400

    overhauled mesh generation and created additional sizes
    Former-commit-id: 20a159ac5e6ac5a62b25c8e68ebeda99d13eb973

--- examples/advectionQuad3D/convergence.sh
+++ examples/advectionQuad3D/convergence.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /scratch/stimmel/convergence
-for N in `seq 3 7`;
+for N in `seq 2 7`;
 do  
-    for meshnum in 4 5 6;
+    for meshnum in 18 29 60;
     do
 	echo mesh=$meshnum N=$N;
 	~/holmes/examples/advectionQuad3D/advectionMainQuad3D ~/holmes/meshes/cubed_grid_${meshnum}.msh $N | grep norm ;
