de883108d446a216dd70a9a3eed729786bf94d77
Tim Warburton <tim.warburton@gmail.com>
2018-03-28 10:38:33 -0400

    tidying up benchmark
    Former-commit-id: 53348e49d8c515cc640b1f0a1887522e46cfb685

--- benchmarks/ellipticBenchmarkAxTet3D/kernelTestDriver.cpp
+++ benchmarks/ellipticBenchmarkAxTet3D/kernelTestDriver.cpp
@@ -12,7 +12,7 @@
 // - 3 number of elements per group
 // - 0:8 run kernels 0 to 8
 
-#if 0
+#if 1
 #define datafloat double
 #define datafloatString "double"
 #else
@@ -56,6 +56,7 @@ void testIpdgAxTet3D(int argc, char **argv){
   printf("==============================================================\n");
   printf("===================== BASIC INFO =============================\n");
   printf("==============================================================\n");
+  printf("Test               : IPDG\n");
   printf("Number of elements : %d\n", E);
   printf("Polynomial degree  : %d\n", p_N);
   printf("Nodes per element  : %d\n", p_Np);
@@ -72,8 +73,6 @@ void testIpdgAxTet3D(int argc, char **argv){
   // number of geometric factors
   int Nvgeo =  10;
   int Nsgeo = 6;
-
-  int pad;
 
   int Niter = 10, it;
   double gflops;
@@ -151,9 +150,7 @@ void testIpdgAxTet3D(int argc, char **argv){
     Tet3Dkernel[i] = device.buildKernelFromSource("ellipticAxIpdgTet3D.okl", buf, kernelInfo);
   }
   
-  // initialize with random numbers on Host and Device
-  srand48(12345);
-  
+ 
   randCalloc(device, E*BSIZE*11, &vgeo, o_vgeo);
   randCalloc(device, E*BSIZE*p_Nfaces*p_Nfp*7, &sgeo, o_sgeo);
   randCalloc(device, BSIZE*BSIZE, &DrT, o_DrT);
@@ -335,7 +332,6 @@ void testIpdgAxTet3D(int argc, char **argv){
 }
 
 void testLocalAxTet3D(int argc, char **argv){
-
     
   int NKernels = 9;
 
@@ -348,15 +344,12 @@ void testLocalAxTet3D(int argc, char **argv){
   int kMin = (argc>=7) ? atoi(argv[6]):0;
   int kMax = (argc>=8) ? atoi(argv[7]):8;
 
-
   int p_Np = ((p_N+1)*(p_N+2)*(p_N+3))/6;
-  int p_Nfp = ((p_N+1)*(p_N+2))/2;
-  int p_Nfaces = 4;
-  int p_NfacesNfp = p_Nfaces*p_Nfp;
 
   printf("==============================================================\n");
   printf("===================== BASIC INFO =============================\n");
   printf("==============================================================\n");
+  printf("Test               : local Ax\n");
   printf("Number of elements : %d\n", E);
   printf("Polynomial degree  : %d\n", p_N);
   printf("Nodes per element  : %d\n", p_Np);
@@ -367,14 +360,11 @@ void testLocalAxTet3D(int argc, char **argv){
   printf("==============================================================\n");
   printf("==============================================================\n");
   printf("\n\n");
-  int BSIZE  = p_Np;
 
+  int BSIZE  = p_Np;
 
   // number of geometric factors
   int Nvgeo =  10;
-  int Nsgeo = 6;
-
-  int pad;
 
   int Niter = 10, it;
   double gflops;
@@ -385,6 +375,8 @@ void testLocalAxTet3D(int argc, char **argv){
   double results3D[15];
   double roofline[15];
   double timeData[15];
+
+  // occa stuff
   occa::device device;
   occa::kernel Tet3Dkernel[15];
   occa::kernel correctRes;
@@ -404,12 +396,8 @@ void testLocalAxTet3D(int argc, char **argv){
   kernelInfo.addDefine("dfloat", datafloatString);
   kernelInfo.addDefine("p_N", p_N);
   kernelInfo.addDefine("p_Np", p_Np);
-  kernelInfo.addDefine("p_Nfp", p_Nfp);
-  kernelInfo.addDefine("p_Nfaces", p_Nfaces);
-  kernelInfo.addDefine("p_NfacesNfp", p_Nfaces*p_Nfp);
   kernelInfo.addDefine("BSIZE", (int)BSIZE);
   kernelInfo.addDefine("p_Nvgeo", 11);
-  kernelInfo.addDefine("p_Nsgeo", 7);
   kernelInfo.addDefine("p_Nggeo", 7);
   kernelInfo.addDefine("p_Ne", p_Ne);
   kernelInfo.addDefine("p_Nb", p_Nb);
@@ -418,7 +406,6 @@ void testLocalAxTet3D(int argc, char **argv){
   int  *elementList;
   occa::memory o_elementList, o_ggeo, o_SrrT, o_SrsT, o_SrtT, o_SsrT,o_SssT,o_SstT, o_StrT, o_StsT, o_SttT, o_MM, o_q, o_Aq; 
   
-  srand(12345);
 
   kernelInfo.addDefine("p_G00ID", 0);
   kernelInfo.addDefine("p_G01ID", 1);
@@ -541,7 +528,6 @@ void testLocalAxTet3D(int argc, char **argv){
 
   }//for
 
-
   //printf("\n\nBWfromCopy%d = [", E);
   for (int k=kMin; k<=kMax; k++){
     printf("==== this is kernel %d \n", k);
@@ -576,7 +562,7 @@ void testLocalAxTet3D(int argc, char **argv){
     double copyBandwidth = ((Nbytes*Niter*2.)/(1.e9*copyElapsed));
     printf("copytime = %16.17f \n", copyElapsed);
 
-    printf("pNp = %d p_Nfp = %d Nbytes = %d \n", p_Np, p_Nfp, Nbytes);    
+    printf("pNp = %d Nbytes = %d \n", p_Np, Nbytes);    
     printf("copy BW = %f gflops = %f bytes = %d \n", copyBandwidth, gflops, Nbytes);
 
     roofline[k] = (copyBandwidth*gflops*E)/(2.*Nbytes);
@@ -625,24 +611,309 @@ void testLocalAxTet3D(int argc, char **argv){
   }
 
   printf("];\n\n");
-
-
-
-  
 }
 
+
+void testLocalAxCurvedTet3D(int argc, char **argv){
+    
+  int NKernels = 9;
+
+  // default to 512 elements if no arg is given
+  int E = (argc>=2) ? atoi(argv[1]):512;
+  int p_N = (argc>=3) ? atoi(argv[2]):5;
+  int p_Ne = (argc>=5) ? atoi(argv[4]):1;
+  int p_Nb = (argc>=6) ? atoi(argv[5]):1;
+
+  int kMin = (argc>=7) ? atoi(argv[6]):0;
+  int kMax = (argc>=8) ? atoi(argv[7]):8;
+
+  int p_Np = ((p_N+1)*(p_N+2)*(p_N+3))/6;
+
+  printf("==============================================================\n");
+  printf("===================== BASIC INFO =============================\n");
+  printf("==============================================================\n");
+  printf("Test               : local curved Ax\n");
+  printf("Number of elements : %d\n", E);
+  printf("Polynomial degree  : %d\n", p_N);
+  printf("Nodes per element  : %d\n", p_Np);
+  printf("Elements per block : %d\n", p_Ne);
+  printf("Outputs per thread : %d\n", p_Nb);
+  printf("Running kernels    : %d to %d \n", kMin, kMax);
+  printf("==============================================================\n");
+  printf("==============================================================\n");
+  printf("==============================================================\n");
+  printf("\n\n");
+
+  int BSIZE  = p_Np;
+
+  // number of geometric factors
+  int Nvgeo =  10;
+
+  int Niter = 10, it;
+  double gflops;
+  gflops = p_Np*20*(1+p_Np);
+  gflops *= Niter;
+
+  // build some dummy storage & parameters
+  double results3D[15];
+  double roofline[15];
+  double timeData[15];
+
+  // occa stuff
+  occa::device device;
+  occa::kernel Tet3Dkernel[15];
+  occa::kernel correctRes;
+  occa::kernelInfo kernelInfo;
+
+  // specify device configuration
+
+  // device.setup("mode = Serial");
+  // device.setup("mode = OpenMP  , schedule = compact, chunk = 10");
+  // device.setup("mode = OpenCL  , platformID = 0, deviceID = 0");
+  device.setup("mode = CUDA    , deviceID = 0");
+  // device.setup("mode = Pthreads, threadCount = 4, schedule = compact, pinnedCores = [0, 0, 1, 1]");
+  // device.setup("mode = COI     , deviceID = 0");
+
+  // compiler variables to be passed to backend compiler by OCCA
+  kernelInfo.addDefine("datafloat", datafloatString);
+  kernelInfo.addDefine("dfloat", datafloatString);
+  kernelInfo.addDefine("p_N", p_N);
+  kernelInfo.addDefine("p_Np", p_Np);
+  kernelInfo.addDefine("BSIZE", (int)BSIZE);
+  kernelInfo.addDefine("p_Nvgeo", 11);
+  kernelInfo.addDefine("p_Nggeo", 7);
+  kernelInfo.addDefine("p_Ne", p_Ne);
+  kernelInfo.addDefine("p_Nb", p_Nb);
+  
+  datafloat  *ggeo, *SrrT,  *SrsT,  *SrtT,  *SssT,  *SsrT, *SstT,  *StsT, *StrT,  *SttT,  *MM, *q, *Aq;
+  int  *elementList;
+  occa::memory o_elementList, o_ggeo, o_SrrT, o_SrsT, o_SrtT, o_SsrT,o_SssT,o_SstT, o_StrT, o_StsT, o_SttT, o_MM, o_q, o_Aq; 
+  
+
+  kernelInfo.addDefine("p_G00ID", 0);
+  kernelInfo.addDefine("p_G01ID", 1);
+  kernelInfo.addDefine("p_G02ID", 2);
+  kernelInfo.addDefine("p_G11ID", 3);
+  kernelInfo.addDefine("p_G12ID", 4);
+  kernelInfo.addDefine("p_G22ID", 5);
+  kernelInfo.addDefine("p_GWJID", 6);
+  randCalloc(device, E*7, &ggeo, o_ggeo);
+  randCalloc(device, BSIZE*BSIZE, &SrrT, o_SrrT);
+  randCalloc(device, BSIZE*BSIZE, &SrsT, o_SrsT);
+  randCalloc(device, BSIZE*BSIZE, &SrtT, o_SrtT);
+  randCalloc(device, BSIZE*BSIZE, &SsrT, o_SsrT);
+  randCalloc(device, BSIZE*BSIZE, &SssT, o_SssT);
+  randCalloc(device, BSIZE*BSIZE, &SstT, o_SstT);
+  randCalloc(device, BSIZE*BSIZE, &StrT, o_StrT);
+  randCalloc(device, BSIZE*BSIZE, &StsT, o_StsT);
+  randCalloc(device, BSIZE*BSIZE, &SttT, o_SttT);
+  randCalloc(device, BSIZE*E, &q, o_q);
+  randCalloc(device, BSIZE*E, &Aq, o_Aq);
+  randCalloc(device, BSIZE*BSIZE, &MM, o_MM);
+
+  elementList = (int *) calloc(E, sizeof(int));
+
+  for(int j=0;j<E;++j){
+    elementList[j] = j;
+  }
+  o_elementList = device.malloc(E*sizeof(int), elementList);
+  char buf[200];
+  for (int i =kMin; i<kMax+1; i++){
+    printf("compiling 3D kernel %d ...\n", i);
+    sprintf(buf, "ellipticPartialAxTet3D_Ref%d", i); 
+    Tet3Dkernel[i] = device.buildKernelFromSource("ellipticAxTet3D.okl", buf, kernelInfo);
+  }
+  int elementOffset = 0;
+  occa::initTimer(device);
+
+  // queue Ax kernels
+  for (int i =kMin;i<=kMax; i++){
+    datafloat lambda = 1.;
+    occa::streamTag startTag = device.tagStream();
+    if (i<9){
+      // launch kernel
+      for(it=0;it<Niter;++it){
+	Tet3Dkernel[i](E,
+		       o_elementList,
+		       o_ggeo,
+		       o_SrrT,
+		       o_SrsT,
+		       o_SrtT,
+		       o_SsrT,
+		       o_SssT,
+		       o_SstT,
+		       o_StrT,
+		       o_StsT,
+		       o_SttT,
+		       o_MM,
+		       lambda,
+		       o_q,       
+		       o_Aq);
+      }
+    }
+    else{
+      for(it=0;it<Niter;++it){
+	Tet3Dkernel[i](E,
+		       elementOffset,
+		       o_ggeo,
+		       o_SrrT,
+		       o_SrsT,
+		       o_SrtT,
+		       o_SsrT,
+		       o_SssT,
+		       o_SstT,
+		       o_StrT,
+		       o_StsT,
+		       o_SttT,
+		       o_MM,
+		       lambda,
+		       o_q,       
+		       o_Aq);
+      }
+
+
+    }
+
+    //adjust GFLOPS for Ref2 and Ref3
+    occa::streamTag stopTag = device.tagStream();
+    double elapsed = device.timeBetween(startTag, stopTag);
+    if (i>5){
+      // old: gflops = p_Np*20*(1+p_Np)
+      gflops = p_Np*(p_Np*14 +14);
+      gflops *=Niter;      
+
+
+    } 
+    printf("\n\nKERNEL %d  ================================================== \n\n", i);
+    printf("OCCA elapsed time = %g\n", elapsed);
+    timeData[i] = elapsed/Niter;
+    printf("number of flops = %f time = %f \n", gflops, elapsed);
+    results3D[i] =E*gflops/(elapsed*1.e9); 
+    //elapsed/Niter;
+    //
+    printf("OCCA: estimated time = %17.15f gflops = %17.17f\n", results3D[i], E*gflops/(elapsed*1.e9));
+    printf("GFL %17.17f \n",E*gflops/(elapsed*1.e9) );      
+    // compute l2 of data
+    o_Aq.copyTo(Aq);
+    datafloat normAq = 0;
+
+    for(int n=0;n<E*BSIZE;++n)
+      normAq += Aq[n]*Aq[n];
+    normAq = sqrt(normAq);
+
+    printf("OCCA: normAq = %17.15lf\n", normAq);
+
+    for(int n=0;n<E*BSIZE;++n){
+      Aq[n] = 0.0f;
+    }
+
+    o_Aq.copyFrom(Aq);
+
+  }//for
+
+  //printf("\n\nBWfromCopy%d = [", E);
+  for (int k=kMin; k<=kMax; k++){
+    printf("==== this is kernel %d \n", k);
+    int p_Nq = k+1;
+    int p_gjNq = k+2;
+    int Niter = 10;
+    double gflops;
+    long long int Nbytes;
+    
+    if (k<=5)
+      Nbytes = 10*p_Np*p_Np*sizeof(datafloat) + E*7*sizeof(datafloat)+E*sizeof(int)+E*p_Np*2*sizeof(datafloat);
+    else {
+      Nbytes =  7*p_Np*p_Np*sizeof(datafloat) + E*7*sizeof(datafloat)+E*sizeof(int)+E*p_Np*2*sizeof(datafloat);
+    }
+    
+    Nbytes /= 2;
+    gflops = p_Np*20*(1+p_Np); 
+    if (k>5){
+      // old: gflops = p_Np*20*(1+p_Np)
+      gflops = p_Np*(p_Np*14 +14);
+    }    
+    
+    occa::memory o_foo = device.malloc(Nbytes);
+    occa::memory o_bah = device.malloc(Nbytes);
+    
+    occa::streamTag startCopy = device.tagStream();
+    for(int it=0;it<Niter;++it){
+      o_bah.copyTo(o_foo);
+    }
+    occa::streamTag endCopy = device.tagStream();
+    double copyElapsed = device.timeBetween(startCopy, endCopy);
+    double copyBandwidth = ((Nbytes*Niter*2.)/(1.e9*copyElapsed));
+    printf("copytime = %16.17f \n", copyElapsed);
+
+    printf("pNp = %d Nbytes = %d \n", p_Np, Nbytes);    
+    printf("copy BW = %f gflops = %f bytes = %d \n", copyBandwidth, gflops, Nbytes);
+
+    roofline[k] = (copyBandwidth*gflops*E)/(2.*Nbytes);
+    
+    o_foo.free();
+    o_bah.free();
+  }
+  
+  printf("\n\nROOFLINE(%d,%d,%d,:)  = [", p_N,p_Ne,p_Nb);
+  for (int k=kMin; k<=kMax; k++){
+
+    printf(" %16.17f ", roofline[k]);
+  }
+
+  printf("]\n\n");
+
+
+  printf("\n\nResults(%d,%d,%d,:)  = [", p_N, p_Ne, p_Nb);
+  for (int k=kMin; k<=kMax; k++){
+
+    printf(" %16.17f ", results3D[k]);
+  }
+
+  printf("];\n\n");
+
+  printf("\n\nDOFSvGFLOPSvGDOFS(%d,:)  = [", p_N);
+  for (int k=kMin; k<=kMax; k++){
+    printf(" %d %16.17f %16.17f ", p_Np*E, results3D[k], (p_Np*E/1.e9)/timeData[k]);
+  }
+
+  printf("];\n\n");
+
+  
+  printf("\n\ntimeData(:, %d) = [",p_N);
+  for (int k=kMin; k<=kMax; k++){
+
+    printf(" %16.17f ", timeData[k]);
+  }
+
+  printf("];\n\n");
+
+  printf("\n\n giganodes(%d,%d,%d,:)  = [", p_N,p_Ne,p_Nb);
+  for (int k=kMin; k<=kMax; k++){
+
+    printf(" %16.17f ", (p_Np*E/1.e9)/timeData[k]);
+  }
+
+  printf("];\n\n");
+}
 
 int main(int argc, char **argv){
 
   int option = (argc>=4) ? atoi(argv[3]):1;  
-  
-  if (option == 0){
-    testIpdgAxTet3D(argc, argv);
-  }
-  else{
-    testLocalAxTet3D(argc, argv);
-  }
 
+  // initialize with random numbers on Host and Device
+  srand(12345);
+
+  switch(option){
+  case 0:
+    testIpdgAxTet3D(argc, argv);
+    break;
+  case 1:
+    testLocalAxTet3D(argc, argv);
+    break;
+  default:
+    testLocalAxCurvedTet3D(argc, argv);
+	
+  }
 
   exit(0);
   return 0;
