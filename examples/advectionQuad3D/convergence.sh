16ee9124e768904dbf2b9f32e1f443580af78784
Stephen Timmel <stimmel@princeton.edu>
2018-03-29 16:03:22 -0400

    shared -> exclusive
    Former-commit-id: 3203638757d08d9951736471c9ce1996d0481485

--- examples/advectionQuad3D/convergence.sh
+++ examples/advectionQuad3D/convergence.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /scratch/stimmel/convergence
-for N in `seq 3 7`;
+for N in `seq 5 7`;
 do  
-    for meshnum in `seq 4 4 32`;
+    for meshnum in `seq 20 4 32`;
     do
 	echo mesh=$meshnum N=$N;
 	~/holmes/examples/advectionQuad3D/advectionMainQuad3D ~/holmes/meshes/cubed_grid_${meshnum}.msh $N | grep norm ;
