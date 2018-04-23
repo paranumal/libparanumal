2102f83489b33092a0226553dde3b537e0bf8c5c
Stephen Timmel <stimmel@princeton.edu>
2018-04-23 01:18:43 -0400

    larger mesh files and mostly functional lserk sandbox
    Former-commit-id: c0a1fc90adb6196d18bf9dac92ca0428961acf7c

---
+++ examples/advectionQuad3D/advectionRunLSERKbasicQuad3D.c
@@ -0,0 +1,109 @@
+#include "advectionQuad3D.h"
+
+void advectionRunLSERKbasicQuad3D(solver_t *solver){
+
+  mesh_t *mesh = solver->mesh;
+    
+  // MPI send buffer
+  dfloat * test_q = (dfloat *) calloc(mesh->Nelements*solver->Nfields*mesh->Np,sizeof(dfloat));
+    
+  //kernel arguments
+  dfloat alpha = 1./mesh->N;
+
+  /*  solver->filterKernelH(mesh->Nelements,
+			solver->o_dualProjMatrix,
+			solver->o_cubeFaceNumber,
+			solver->o_EToE,
+			solver->o_q,
+			solver->o_qFilter);
+  
+  solver->filterKernelV(mesh->Nelements,
+			alpha,
+			solver->o_dualProjMatrix,
+			solver->o_cubeFaceNumber,
+			solver->o_EToE,
+			solver->o_x,
+			solver->o_y,
+			solver->o_z,
+			solver->o_qFilter,
+			solver->o_q);
+  */
+  for(iint tstep=0;tstep < solver->NtimeSteps;++tstep){
+	
+      for (iint rk = 0; rk < solver->Nrk; ++rk) {
+	
+	//synthesize actual stage time
+	dfloat t = tstep*solver->dt;
+      
+	// compute volume contribution to DG advection RHS
+	solver->volumeKernel(mesh->Nelements,
+			     solver->o_vgeo,
+			     solver->o_D,
+			     solver->o_x,
+			     solver->o_y,
+			     solver->o_z,
+			     solver->o_q,
+			     solver->o_rhsq);
+	
+	solver->surfaceKernel(mesh->Nelements,
+			       solver->o_sgeo,
+			       solver->o_LIFTT,
+			       solver->o_vmapM,
+			       solver->o_vmapP,
+			       t,
+			       solver->o_x,
+			       solver->o_y,
+			       solver->o_z,
+			       solver->o_q,
+			       solver->o_rhsq);
+	
+	/*solver->filterKernelH(mesh->Nelements,
+			      solver->o_dualProjMatrix,
+			      solver->o_cubeFaceNumber,
+			      solver->o_EToE,
+			      solver->o_rhsq,
+			      solver->o_qFilter);
+	
+	solver->filterKernelV(mesh->Nelements,
+			      alpha,
+			      solver->o_dualProjMatrix,
+			      solver->o_cubeFaceNumber,
+			      solver->o_EToE,
+			      solver->o_x,
+			      solver->o_y,
+			      solver->o_z,
+			      solver->o_qFilter,
+			      solver->o_rhsq);
+	*/
+	solver->volumeCorrectionKernel(mesh->Nelements,
+				       solver->o_q,
+				       solver->o_qCorr);
+	
+	solver->updateKernel(mesh->Nelements,
+			     mesh->dt,
+			     solver->rka[rk],
+			     solver->rkb[rk],
+			     solver->o_rhsq,
+			     solver->o_qCorr,
+			     solver->o_resq,
+			     solver->o_q);
+      }
+      /*      solver->filterKernelH(mesh->Nelements,
+			      solver->o_dualProjMatrix,
+			      solver->o_cubeFaceNumber,
+			      solver->o_EToE,
+			      solver->o_q,
+			      solver->o_qFilter);
+      
+      solver->filterKernelV(mesh->Nelements,
+			      alpha,
+			      solver->o_dualProjMatrix,
+			      solver->o_cubeFaceNumber,
+			      solver->o_EToE,
+			      solver->o_x,
+			      solver->o_y,
+			      solver->o_z,
+			      solver->o_qFilter,
+			      solver->o_q);   */
+  }
+}
--- examples/advectionQuad3D/meshing/generator.sh
+++ examples/advectionQuad3D/meshing/generator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for N in `seq 4 70`;
+for N in `seq 70 128`;
 do  
     ./cubed_grid $N > ../../../meshes/cubed_grid_${N}.msh
+    echo "$N complete"
 done
