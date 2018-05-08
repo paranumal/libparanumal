641a10ad7bc357aac5cf9331341eac5983947663
AliKarakus <alikarakus.me@gmail.com>
2018-05-08 03:00:29 -0400

    Added bns stuff
    Former-commit-id: 580774c249faf05d9cd500f9f7ab3761a63e7645

---
+++ examples/bns/bnsPmlSetup.c
@@ -0,0 +1,430 @@
+#include "bns.h"
+
+void bnsPmlSetup(bns_t *bns, setupAide &options){
+
+  mesh_t *mesh = bns->mesh; 
+
+  
+  dfloat xsigma    = 100., ysigma  = 100.,  zsigma  = 100.;
+  
+  //count the pml elements
+  mesh->pmlNelements=0;
+  for (dlong m=0;m<mesh->Nelements;m++) {
+    dlong e    = mesh->nonPmlElementIds[m];
+    int type   = mesh->elementInfo[e];
+    if ((type==100)||(type==200)||(type==300)|| 
+        (type==400)||(type==500)||(type==600)||
+        (type==700) ) {
+      mesh->pmlNelements++;
+    }
+  }
+
+  //set up the pml
+  if (mesh->pmlNelements) {
+
+    //construct a numbering of the pml elements
+    dlong *pmlIds   = (dlong *) calloc(mesh->Nelements,sizeof(dlong));
+    dlong pmlcnt    = 0;
+    dlong nonpmlcnt = 0;
+    for (dlong e=0;e<mesh->Nelements;e++) {
+      int type = mesh->elementInfo[e];
+      if ((type==100)||(type==200)||(type==300)|| 
+          (type==400)||(type==500)||(type==600)||
+          (type==700) )  //pml element
+        pmlIds[e] = pmlcnt++;
+    }
+
+    mesh->pmlElementIds = (dlong *) calloc(mesh->pmlNelements,sizeof(dlong));
+    mesh->pmlIds        = (dlong *) calloc(mesh->pmlNelements,sizeof(dlong));
+
+    pmlcnt = 0;
+    nonpmlcnt = 0;
+    for (dlong m=0;m<mesh->Nelements;m++){
+      dlong e = mesh->nonPmlElementIds[m];
+      int type = mesh->elementInfo[e];
+
+      if ((type==100)||(type==200)||(type==300)|| 
+          (type==400)||(type==500)||(type==600)||
+          (type==700) ) { //pml element
+        mesh->pmlElementIds[pmlcnt] = e;
+        mesh->pmlIds[pmlcnt] = pmlIds[e];
+        pmlcnt++;
+      } else { //nonpml element
+        mesh->nonPmlElementIds[nonpmlcnt] = e;
+        nonpmlcnt++;
+      }
+    }
+
+
+    //resize nonpml element lists
+    mesh->nonPmlNelements -= mesh->pmlNelements;
+    mesh->nonPmlElementIds = (dlong*) realloc(mesh->nonPmlElementIds,mesh->nonPmlNelements*sizeof(dlong));
+
+
+    int Nnodes = 0;
+    if(options.compareArgs("RELAXATION TYPE","CUBATURE")){ // !!!!!!!!!!!!!!!
+      //
+      printf("Setting PML Coefficient for Cubature Integration\n");
+      //set up damping parameter
+      bns->pmlSigmaX   = (dfloat *) calloc(mesh->pmlNelements*mesh->cubNp,sizeof(dfloat));
+      bns->pmlSigmaY   = (dfloat *) calloc(mesh->pmlNelements*mesh->cubNp,sizeof(dfloat)); 
+      if(bns->dim==3)
+        bns->pmlSigmaZ = (dfloat *) calloc(mesh->pmlNelements*mesh->cubNp,sizeof(dfloat)); 
+
+      Nnodes = mesh->cubNp;    
+    }
+     else{
+      printf("Setting PML Coefficients for Nodal Collocation Integration\n");
+      //set up damping parameter
+      bns->pmlSigmaX    = (dfloat *) calloc(mesh->pmlNelements*mesh->Np,sizeof(dfloat));
+      bns->pmlSigmaY    = (dfloat *) calloc(mesh->pmlNelements*mesh->Np,sizeof(dfloat));
+      if(bns->dim==3)
+        bns->pmlSigmaZ  = (dfloat *) calloc(mesh->pmlNelements*mesh->Np,sizeof(dfloat));
+      Nnodes = mesh->Np; 
+    }
+   
+    //find the bounding box of the whole domain and interior domain
+    dfloat xmin = 1e9, xmax =-1e9;
+    dfloat ymin = 1e9, ymax =-1e9;
+    dfloat zmin = 1e9, zmax =-1e9;
+    dfloat pmlxmin = 1e9, pmlxmax =-1e9;
+    dfloat pmlymin = 1e9, pmlymax =-1e9;
+    dfloat pmlzmin = 1e9, pmlzmax =-1e9;
+
+    for (dlong e=0;e<mesh->Nelements;e++) {
+      for (int n=0;n<mesh->Nverts;n++) {
+        dfloat x =0, y=0, z = 0; 
+        x  = mesh->EX[e*mesh->Nverts+n];
+        y  = mesh->EY[e*mesh->Nverts+n];
+        
+        pmlxmin = (pmlxmin > x) ? x : pmlxmin;
+        pmlymin = (pmlymin > y) ? y : pmlymin;
+        pmlxmax = (pmlxmax < x) ? x : pmlxmax;
+        pmlymax = (pmlymax < y) ? y : pmlymax;
+
+        if(bns->dim==3){
+          z = mesh->EZ[e*mesh->Nverts+n];
+          pmlzmin = (pmlzmin > z) ? z : pmlzmin;
+          pmlzmax = (pmlzmax < z) ? z : pmlzmax;
+        }
+      }
+      //skip pml elements
+      int type = mesh->elementInfo[e];
+      if ((type==100)||(type==200)||(type==300)|| 
+          (type==400)||(type==500)||(type==600)||
+          (type==700) ) continue;
+      for (int n=0;n<mesh->Nverts;n++) {
+        dfloat x = 0., y = 0., z = 0.; 
+        x = mesh->EX[e*mesh->Nverts+n];
+        y = mesh->EY[e*mesh->Nverts+n];
+        xmin = (xmin > x) ? x : xmin;
+        ymin = (ymin > y) ? y : ymin;
+        xmax = (xmax < x) ? x : xmax;
+        ymax = (ymax < y) ? y : ymax;
+        if(bns->dim==3){
+          z = mesh->EZ[e*mesh->Nverts+n];
+          zmin = (zmin > z) ? z : zmin;
+          zmax = (zmax < z) ? z : zmax;          
+        }
+
+      }
+    }
+
+
+
+    int order = 2; 
+    //
+    if(options.compareArgs("PML PROFILE","CONSTANT"))
+      order = 0; 
+    if(options.compareArgs("PML PROFILE","LINEAR"))
+      order = 1; 
+    if(options.compareArgs("PML PROFILE","QUADRATIC"))
+      order = 2;
+    if(options.compareArgs("PML PROFILE","FORTHORDER"))
+      order = 4;
+
+    dfloat xmaxScale =0., xminScale=0.; 
+    dfloat ymaxScale =0., yminScale=0.; 
+    dfloat zmaxScale =0., zminScale=0.; 
+
+    xmaxScale = pow(pmlxmax-xmax,order);
+    xminScale = pow(pmlxmin-xmin,order);
+    ymaxScale = pow(pmlymax-ymax,order);
+    yminScale = pow(pmlymin-ymin,order);
+    if(bns->dim==3){
+      zmaxScale = pow(pmlzmax-zmax,order);
+      zminScale = pow(pmlzmin-zmin,order);      
+    }
+
+    dfloat *xe  = (dfloat *) calloc(mesh->Nverts, sizeof(dfloat));
+    dfloat *ye  = (dfloat *) calloc(mesh->Nverts, sizeof(dfloat));
+    dfloat *ze  = (dfloat *) calloc(mesh->Nverts, sizeof(dfloat));
+
+    //set up the damping factor
+    for (dlong es=0;es<mesh->pmlNelements;es++){
+        dlong e     = mesh->pmlElementIds[es];
+        dlong pmlId = mesh->pmlIds[es];
+        int type    = mesh->elementInfo[e];
+        dlong id    = e*mesh->Nverts;
+
+        for(int n=0; n<mesh->Nverts; n++){
+          xe[n] = mesh->EX[id+n];
+          ye[n] = mesh->EY[id+n];
+          if(bns->dim==3)
+            ze[n] = mesh->EZ[id+n];
+        }
+
+        for(int n=0;n<Nnodes;++n){ /* for each node */
+          dfloat x = 0, y = 0, z = 0; 
+
+          if(Nnodes==mesh->cubNp){
+            dfloat rn =0, sn=0, tn=0;
+            if(bns->elementType==TRIANGLES){
+              rn = mesh->cubr[n];
+              sn = mesh->cubs[n]; 
+              x = -0.5*(rn+sn)*xe[0] + 0.5*(1+rn)*xe[1] + 0.5*(1+sn)*xe[2];
+              y = -0.5*(rn+sn)*ye[0] + 0.5*(1+rn)*ye[1] + 0.5*(1+sn)*ye[2];
+            }
+            else if(bns->elementType==TETRAHEDRA)
+            {
+              rn = mesh->cubr[n];
+              sn = mesh->cubs[n]; 
+              tn = mesh->cubt[n];
+              x = -0.5*(rn+sn+tn+1)*xe[0] + 0.5*(1+rn)*xe[1] - 0.5*(1+sn)*xe[2] + 0.5*(tn+1)*xe[3];
+              y = -0.5*(rn+sn+tn+1)*ye[0] + 0.5*(1+rn)*ye[1] - 0.5*(1+sn)*ye[2] + 0.5*(tn+1)*ye[3];
+              z = -0.5*(rn+sn+tn+1)*ze[0] + 0.5*(1+rn)*ze[1] - 0.5*(1+sn)*ze[2] + 0.5*(tn+1)*ze[3];
+            } 
+            
+          }
+          else{
+            x = mesh->x[n + e*mesh->Np];
+            y = mesh->y[n + e*mesh->Np];
+            z = mesh->z[n + e*mesh->Np];
+          }
+          
+          if (type==100) { //X Pml
+            if(x>xmax)
+              bns->pmlSigmaX[Nnodes*pmlId + n] = xsigma*pow(x-xmax,order)/xmaxScale;
+            if(x<xmin)
+              bns->pmlSigmaX[Nnodes*pmlId + n] = xsigma*pow(x-xmin,order)/xminScale;
+          } else if (type==200) { //Y Pml
+            if(y>ymax)
+              bns->pmlSigmaY[Nnodes*pmlId + n] = ysigma*pow(y-ymax,order)/ymaxScale;
+            if(y<ymin)
+              bns->pmlSigmaY[Nnodes*pmlId + n] = ysigma*pow(y-ymin,order)/yminScale;
+          } else if (type==300) { //XY Pml
+            if(x>xmax)
+              bns->pmlSigmaX[Nnodes*pmlId + n] = xsigma*pow(x-xmax,order)/xmaxScale;
+            if(x<xmin)
+              bns->pmlSigmaX[Nnodes*pmlId + n] = xsigma*pow(x-xmin,order)/xminScale;
+            if(y>ymax)
+              bns->pmlSigmaY[Nnodes*pmlId + n] = ysigma*pow(y-ymax,order)/ymaxScale;
+            if(y<ymin)
+              bns->pmlSigmaY[Nnodes*pmlId + n] = ysigma*pow(y-ymin,order)/yminScale;
+          }
+
+          if(bns->dim==3){
+            if (type==400) { //Z Pml
+              if(z>zmax)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmax,2)/zmaxScale;
+              if(z<zmin)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmin,2)/zminScale;
+            } else if (type==500) {//XZ Pml
+              if(x>xmax)
+                bns->pmlSigmaX[mesh->cubNp*pmlId + n] = xsigma*pow(x-xmax,2)/xmaxScale;
+              if(x<xmin)
+                bns->pmlSigmaX[mesh->cubNp*pmlId + n] = xsigma*pow(x-xmin,2)/xminScale;
+              if(z>zmax)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmax,2)/zmaxScale;
+              if(z<zmin)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmin,2)/zminScale;
+            } else if (type==600){ //YZ Pml
+              if(y>ymax)
+                bns->pmlSigmaY[mesh->cubNp*pmlId + n] = ysigma*pow(y-ymax,2)/ymaxScale;
+              if(y<ymin)
+                bns->pmlSigmaY[mesh->cubNp*pmlId + n] = ysigma*pow(y-ymin,2)/yminScale;
+              if(z>zmax)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmax,2)/zmaxScale;
+              if(z<zmin)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmin,2)/zminScale;
+            } else if (type==700){ //XYZ Pml
+              if(x>xmax)
+                bns->pmlSigmaX[mesh->cubNp*pmlId + n] = xsigma*pow(x-xmax,2)/xmaxScale;
+              if(x<xmin)
+                bns->pmlSigmaX[mesh->cubNp*pmlId + n] = xsigma*pow(x-xmin,2)/xminScale;
+              if(y>ymax)
+                bns->pmlSigmaY[mesh->cubNp*pmlId + n] = ysigma*pow(y-ymax,2)/ymaxScale;
+              if(y<ymin)
+                bns->pmlSigmaY[mesh->cubNp*pmlId + n] = ysigma*pow(y-ymin,2)/yminScale;
+              if(z>zmax)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmax,2)/zmaxScale;
+              if(z<zmin)
+                bns->pmlSigmaZ[mesh->cubNp*pmlId + n] = zsigma*pow(z-zmin,2)/zminScale;
+            }
+          }
+   
+      }
+
+    }
+
+    free(xe); free(ye); free(ze);
+
+    printf("# of PML elements: %d and # of Non-PML elements: %d \n",
+    mesh->pmlNelements, mesh->Nelements-mesh->pmlNelements);
+
+   
+
+    if(options.compareArgs("TIME INTEGRATOR","LSERK")){
+      bns->pmlqx     = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+      bns->pmlrhsqx  = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+      bns->pmlresqx  = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+
+      bns->pmlqy     = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+      bns->pmlrhsqy  = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+      bns->pmlresqy = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+
+
+      // set up PML on DEVICE    
+      bns->o_pmlqx     = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlqx);
+      bns->o_pmlrhsqx  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlrhsqx);
+      bns->o_pmlresqx  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlresqx);
+
+      bns->o_pmlqy     = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlqy);
+      bns->o_pmlrhsqy  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlrhsqy);
+      bns->o_pmlresqy  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlresqy);
+
+      if(bns->dim==3){
+        bns->pmlqz     = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+        bns->pmlrhsqz  = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+        bns->pmlresqz  = (dfloat*) calloc(mesh->pmlNelements*mesh->Np*bns->Nfields, sizeof(dfloat));
+        //
+        // set up PML on DEVICE    
+        bns->o_pmlqz     = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlqz);
+        bns->o_pmlrhsqz  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlrhsqz);
+        bns->o_pmlresqz  = mesh->device.malloc(mesh->pmlNelements*mesh->Np*bns->Nfields*sizeof(dfloat), bns->pmlresqz);
+
+      }
+    }
+
+   
+
+
+
+    if (mesh->pmlNelements){
+      bns->o_pmlSigmaX     = mesh->device.malloc(mesh->pmlNelements*Nnodes*sizeof(dfloat),bns->pmlSigmaX);
+      bns->o_pmlSigmaY     = mesh->device.malloc(mesh->pmlNelements*Nnodes*sizeof(dfloat),bns->pmlSigmaY);
+      if(bns->dim==3)
+        bns->o_pmlSigmaZ   = mesh->device.malloc(mesh->pmlNelements*Nnodes*sizeof(dfloat),bns->pmlSigmaZ);
+
+      mesh->o_pmlElementIds = mesh->device.malloc(mesh->pmlNelements*sizeof(int), mesh->pmlElementIds);
+      mesh->o_pmlIds        = mesh->device.malloc(mesh->pmlNelements*sizeof(int), mesh->pmlIds);
+    }
+
+
+      
+   free(pmlIds);
+
+
+
+  #if 0
+       //
+ //    printf("Wwitting Pml profile\n" );
+ //     int fid = 0; 
+ // for (int es=0;es<mesh->pmlNelements;es++){
+ //       int e     = mesh->pmlElementIds[es];
+ //       int pmlId = mesh->pmlIds[es];   
+ //    for(int n=0;n<mesh->Np;n++){
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 0] = mesh->pmlBetaX[mesh->Np*e+n];
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 1] = mesh->pmlBetaY[mesh->Np*e+n];
+ //    }
+ //  }
+ 
+  char fname[BUFSIZ];
+  sprintf(fname, "pmlProfile1.vtu");
+  boltzmannPlotVTU2D(mesh, fname);
+
+  //    //
+ //    printf("Wwitting Pml profile\n" );
+ //     int fid = 0; 
+ // for (int es=0;es<mesh->pmlNelements;es++){
+ //       int e     = mesh->pmlElementIds[es];
+ //       int pmlId = mesh->pmlIds[es];   
+ //    for(int n=0;n<Nnodes;n++){
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 0] = bns->pmlSigmaX[mesh->Np*e+n];
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 1] = bns->pmlSigmaY[mesh->Np*e+n];
+ //    }
+ //  }
+
+
+ //  char fname[BUFSIZ];
+ //  sprintf(fname, "pmlProfile1.vtu");
+ //  boltzmannPlotVTU2D(mesh, fname);
+
+
+
+ //   dfloat *cubProjectT = (dfloat*) calloc(mesh->cubNp*mesh->Np, sizeof(dfloat));
+ //   mesh->o_cubProjectT.copyTo(cubProjectT);
+ //    // free(pmlIds);
+ //     int fid = 0; 
+ // for (int es=0;es<mesh->pmlNelements;es++){
+ //       int e     = mesh->pmlElementIds[es];
+ //       int pmlId = mesh->pmlIds[es];   
+ //    for(int n=0;n<mesh->Np;n++)
+ //    {
+ //      dfloat q1 = 0; dfloat q2 = 0; 
+ //      for(int m=0; m<mesh->cubNp;++m){
+ //         dfloat prj = cubProjectT[m*mesh->Np+n];
+ //         q1 += prj*bns->pmlSigmaX[Nnodes*pmlId + m];
+ //         q2 += prj*bns->pmlSigmaY[Nnodes*pmlId + m];
+ //      }
+
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 0] = q1;
+ //      mesh->q[bns->Nfields*(n + e*mesh->Np) + 1] = q2;
+ //    }
+ //  }
+  
+
+ //  // char fname[BUFSIZ];
+ //  // sprintf(fname, "pmlProfile1.vtu");
+ //  // boltzmannPlotVTU2D(mesh, fname);
+
+ //   //
+ //  printf("NonPmlElements: %d  PmlElements: %d \n", mesh->nonPmlNelements, mesh->pmlNelements);
+
+  
+ //  for (int es=0;es<mesh->nonPmlNelements;es++){
+ //       int e      = mesh->nonPmlElementIds[es];
+ //       int id = e*mesh->Nverts;  
+ //        dfloat x1 = mesh->EX[id+0]; /* x-coordinates of vertices */
+ //        dfloat x2 = mesh->EX[id+1];
+ //        dfloat x3 = mesh->EX[id+2];
+
+ //        dfloat y1 = mesh->EY[id+0]; /* y-coordinates of vertices */
+ //        dfloat y2 = mesh->EY[id+1];
+ //        dfloat y3 = mesh->EY[id+2];
+ //       printf("Element: %d  x1: %.5f x2: %.5f x3: %.5f y1: %.5f y2: %.5f y3: %.5f \n", e, x1,x2,x3,y1,y2,y3);
+ //  }   
+
+
+ //  for (int es=0;es<mesh->pmlNelements;es++){
+ //       int e     = mesh->pmlElementIds[es];
+ //       int pmlId = mesh->pmlIds[es];
+ //       int id = e*mesh->Nverts;  
+ //       //
+ //       dfloat x1 = mesh->EX[id+0]; /* x-coordinates of vertices */
+ //        dfloat x2 = mesh->EX[id+1];
+ //        dfloat x3 = mesh->EX[id+2];
+
+ //        dfloat y1 = mesh->EY[id+0]; /* y-coordinates of vertices */
+ //        dfloat y2 = mesh->EY[id+1];
+ //        dfloat y3 = mesh->EY[id+2];
+ //       printf("Element: %d   PmlId = %d x1: %.5f x2: %.5f x3: %.5f y1: %.5f y2: %.5f y3: %.5f\n", e, pmlId, x1,x2,x3,y1,y2,y3);
+ //  }   
+
+
+
+  #endif
+  
+  }
+
+}
---
+++ examples/bns/bnsTimeStepperCoefficients.c
@@ -0,0 +1,219 @@
+#include "bns.h"
+
+void bnsTimeStepperCoefficients(bns_t *bns, setupAide &options){
+
+mesh_t *mesh = bns->mesh; 
+
+if(options.compareArgs("TIME INTEGRATOR","MRSAAB")){
+
+	int Nlevels = 0;
+
+	if(mesh->MRABNlevels==0)
+		Nlevels =1;
+	else
+		Nlevels = mesh->MRABNlevels;
+
+	// Create circle on complex plane
+	const int Nr = 32; 
+		dfloat complex R[Nr]; 
+	for(int ind =1; ind <= Nr; ++ind){
+		const dfloat theta = (dfloat) (ind - 0.5) / (dfloat) Nr; 
+		R[ind-1] = cexp(I*M_PI* theta);
+	}
+
+
+	bns->MRSAAB_A = (dfloat *) calloc(3*3*Nlevels,sizeof(dfloat));
+	bns->MRSAAB_B = (dfloat *) calloc(3*3*Nlevels,sizeof(dfloat));
+	bns->MRSAAB_C = (dfloat *) calloc(    Nlevels,sizeof(dfloat));
+	bns->MRAB_A   = (dfloat *) calloc(3*3*Nlevels,sizeof(dfloat));
+	bns->MRAB_B   = (dfloat *) calloc(3*3*Nlevels,sizeof(dfloat));
+	bns->MRAB_C   = (dfloat *) calloc(    Nlevels,sizeof(dfloat));
+
+	int MRABorder = 3; 
+
+  for(int l = 0; l<Nlevels; ++l){
+		// MRSAAB coefficients
+		dfloat alpha = -bns->tauInv*bns->dt*pow(2,l);
+		//dfloat alpha=0.0;
+		dfloat h  = bns->dt * pow(2,l); 
+    //
+		for (int order=0; order<3; ++order){
+      // computation of coefficients based on magnitude
+      const int id = order*Nlevels*3 + l*3;
+      if(order==0){
+
+				double complex a1 = 0. + 0.* I; 
+				double complex b1 = 0. + 0.* I; 
+
+				for(int i = 0; i<Nr; ++i ){
+					double complex lr = alpha  + R[i];
+					a1 +=  h*(cexp(lr) - 1.)/lr;
+					b1 +=  h*(cexp(lr/2.) - 1.)/lr;
+				}
+				// Full dt coeeficients
+				bns->MRSAAB_A[id + 0] = creal(a1)/Nr;
+				bns->MRSAAB_A[id + 1] = 0.f;
+				bns->MRSAAB_A[id + 2] = 0.f;
+				// Half coefficients
+				bns->MRSAAB_B[id + 0] = creal(b1)/Nr;
+				bns->MRSAAB_B[id + 1] = 0.f;
+				bns->MRSAAB_B[id + 2] = 0.f;
+
+				// MRAB coefficients
+				bns->MRAB_A[id + 0]   =  h ;
+				bns->MRAB_A[id + 1]   =  0.f ;
+				bns->MRAB_A[id + 2]   =  0.f ;
+
+				bns->MRAB_B[id+0]     =  h/2. ;
+				bns->MRAB_B[id+1]     =  0.f ;
+				bns->MRAB_B[id+2]     =  0.f ;
+			}
+
+			else if(order==1){
+
+				double complex a1 = 0. + 0.* I; 
+				double complex b1 = 0. + 0.* I; 
+				double complex a2 = 0. + 0.* I; 
+				double complex b2 = 0. + 0.* I; 
+
+				for(int i = 0; i<Nr; ++i ){
+					double complex lr = alpha  + R[i];
+					a1 +=  h*(-2.*lr + (1.+lr)*cexp(lr) - 1.)/cpow(lr,2);
+					a2 +=  h*(lr - cexp(lr) + 1.)/cpow(lr,2);
+					b1 +=  h*(-1.5*lr + (1.+lr)*cexp(lr/2.) - 1.)/cpow(lr,2);
+					b2 +=  h*(0.5*lr - cexp(lr/2.) + 1.)/cpow(lr,2);
+				}
+				// Full dt coeeficients
+				bns->MRSAAB_A[id + 0] = creal(a1)/Nr;
+				bns->MRSAAB_A[id + 1] = creal(a2)/Nr;
+				bns->MRSAAB_A[id + 2] = 0.f;
+				// Half coefficients
+				bns->MRSAAB_B[id + 0] = creal(b1)/Nr;
+				bns->MRSAAB_B[id + 1] = creal(b2)/Nr;
+				bns->MRSAAB_B[id + 2] = 0.f;
+
+
+				// MRAB coefficients
+				bns->MRAB_A[id + 0]   =  3.*h/2. ;
+				bns->MRAB_A[id + 1]   = -1.*h/2. ;
+				bns->MRAB_A[id + 2]   =  0.f ;
+
+				bns->MRAB_B[id + 0]   =  5.*h/8. ;
+				bns->MRAB_B[id + 1]   = -1.*h/8. ;
+				bns->MRAB_B[id + 2]   =   0.f ;
+      }
+
+			else{
+				double complex a1 = 0. + 0.* I; 
+				double complex b1 = 0. + 0.* I; 
+				double complex a2 = 0. + 0.* I; 
+				double complex b2 = 0. + 0.* I; 
+				double complex a3 = 0. + 0.* I; 
+				double complex b3 = 0. + 0.* I; 
+
+				for(int i = 0; i<Nr; ++i ){
+					double complex lr = alpha  + R[i];
+					a1 += h*(-2.5*lr - 3.*cpow(lr,2) + (1.+cpow(lr,2)+1.5*lr)*cexp(lr) - 1.)/cpow(lr,3);
+					a2 += h*(4.*lr + 3.*cpow(lr,2)- (2.*lr + 2.0)*cexp(lr) + 2.)/cpow(lr,3);
+					a3 +=-h*(1.5*lr + cpow(lr,2)- (0.5*lr + 1.)*cexp(lr) + 1.)/cpow(lr,3);
+					b1 += h*(cexp(lr/2.)- 2.*lr - (15.*cpow(lr,2))/8. + (cpow(lr,2) + 1.5*lr)*cexp(lr/2.) - 1.)/cpow(lr,3);
+					b2 += h*(3.*lr - 2.*cexp(lr/2.0) + 1.25*cpow(lr,2) - 2.*lr*cexp(lr/2.) + 2.)/cpow(lr,3);
+					b3 +=-h*(lr - cexp(lr/2.) + 0.375*cpow(lr,2) - 0.5*lr*cexp(lr/2.) + 1.)/cpow(lr,3);
+				}
+
+				// Full dt coeeficients
+				bns->MRSAAB_A[id+0] = creal(a1)/Nr;
+				bns->MRSAAB_A[id+1] = creal(a2)/Nr;
+				bns->MRSAAB_A[id+2] = creal(a3)/Nr;
+				// Half coefficients
+				bns->MRSAAB_B[id+0] = creal(b1)/Nr;
+				bns->MRSAAB_B[id+1] = creal(b2)/Nr;
+				bns->MRSAAB_B[id+2] = creal(b3)/Nr;
+
+				// MRAB coefficients
+				bns->MRAB_A[id+0]   =  23.*h/12. ;
+				bns->MRAB_A[id+1]   = -16.*h/12. ;
+				bns->MRAB_A[id+2]   =  5. *h/12. ;
+
+				bns->MRAB_B[id+0]   =  17.*h/24. ;
+				bns->MRAB_B[id+1]   = - 7.*h/24. ;
+				bns->MRAB_B[id+2]   =   2.*h/24. ;  
+		}
+  }
+
+    // Exponential part
+    bns->MRSAAB_C[l]    = exp(alpha);
+    //printf("Coefficient: %.5f \n", bns->MRSAAB_C[l]);
+    bns->MRAB_C[l]      =   h ;
+ }
+
+}
+
+
+if(options.compareArgs("TIME INTEGRATOR", "LSERK")){
+// 
+printf("Relying the fact that  LSERK set on mesh structure\n");
+
+
+}
+
+
+
+
+if(options.compareArgs("TIME INTEGRATOR","SAADRK")){
+
+	// dfloat rkC[bns->NrkStages], rkA[bns->NrkStages*bns->NrkStages], rkE[bns->NrkStages]; 
+	if(bns->NrkStages==5){ // SAARK43
+
+		// first set non-semianalytic part of the integrator 
+		dfloat rkC[bns->NrkStages]  = {0.0, 0.5, 0.5, 1.0, 1.0};
+		dfloat rkA[bns->NrkStages*bns->NrkStages]  
+		   = {             0.0,             0.0,            0.0,          0.0,             0.0,
+                           0.5,             0.0,            0.0,          0.0,             0.0,
+                           0.0,             0.5,            0.0,          0.0,             0.0,
+                           0.0,             0.0,            1.0,          0.0,             0.0,
+                         1.0/6.0,         1.0/3.0,        1.0/3.0,       1.0/6.0,          0.0}; 
+		dfloat rkE[bns->NrkStages]= {  0.0,             0.0,            0.0,        -1.0/6.0,        1.0/6.0}; 
+
+	bns->rkC    = (dfloat*) calloc(bns->NrkStages, sizeof(dfloat));
+	bns->rkA    = (dfloat*) calloc(bns->NrkStages*bns->NrkStages, sizeof(dfloat));
+	bns->rkE    = (dfloat*) calloc(bns->NrkStages, sizeof(dfloat));
+	
+
+	memcpy(bns->rkC, rkC, bns->NrkStages*sizeof(dfloat)); 
+	memcpy(bns->rkA, rkA, bns->NrkStages*bns->NrkStages*sizeof(dfloat));
+	memcpy(bns->rkE, rkE, bns->NrkStages*sizeof(dfloat));
+	// Compute semi-analytic part of the integrator
+
+	}
+	else if(bns->NrkStages==7){	
+
+      printf("Numbe of stages in SAADRK is 7\n");
+
+		dfloat rkC[bns->NrkStages]   = {0.0, 0.25, 0.25, 0.5, 0.75, 1.0, 1.0};
+		dfloat rkA[bns->NrkStages*bns->NrkStages]  
+		   =  {  0,    0,        0,     0,     0,    0,   0,
+		         1/4,   0,        0,     0,     0,    0,   0,
+                 1/8,  1/8,       0,     0,     0,    0,   0,
+                  0,    0,        1/2,     0,     0,    0,   0,
+               3./16., -3./8.,   3./8.,  9./16.,     0,    0, 0,
+               -3./7.,  8./7.,   6./7., -12./7.,   8./7.,    0, 0,
+               7./90.,    0.,    16./45.,  2./15., 16./45., 7./90., 0}; 
+
+		dfloat rkE[bns->NrkStages]=  {-4./45., 0, 16./45., -8./15., 16./45., -4./45., 0.}; 
+
+    bns->rkC    = (dfloat*) calloc(bns->NrkStages, sizeof(dfloat));
+	bns->rkA    = (dfloat*) calloc(bns->NrkStages*bns->NrkStages, sizeof(dfloat));
+	bns->rkE    = (dfloat*) calloc(bns->NrkStages, sizeof(dfloat));
+	
+
+	memcpy(bns->rkC, rkC, bns->NrkStages*sizeof(dfloat)); 
+	memcpy(bns->rkA, rkA, bns->NrkStages*bns->NrkStages*sizeof(dfloat));
+	memcpy(bns->rkE, rkE, bns->NrkStages*sizeof(dfloat));
+		
+	}
+	bnsSAADRKCoefficients(bns, options);
+}
+
+
+}
