5f9357f1cd499f6261ce6f3ac40467e7cf4ed4df
tcew <spam.warburton@gmail.com>
2018-07-28 00:27:54 +0000

    preliminary header for sine mode test
    Former-commit-id: b065bc1ae548ccd3d99e4065e3d9acd81379509d

---
+++ solvers/elliptic/data/ellipticSineTest3D.h
@@ -0,0 +1,19 @@
+
+/* Homogeneous Dirichlet boundary condition   */
+#define ellipticDirichletCondition3D(t,x,y,z,nx,ny,nz,uM,uxM,uyM,uzM,uB,uxB,uyB,uzB)  \
+  {              \
+    uB  = sin(M_PI*x)*sin(M_PI*y)*sin(M_PI*z);	\
+    uxB = uxM;	\
+    uyB = uyM;   \
+    uzB = uzM;   \
+  }
+
+/* Homogeneous Neumann boundary condition   */
+#define ellipticNeumannCondition3D(t,x,y,z,nx,ny,nz,uM,uxM,uyM,uzM,uB,uxB,uyB,uzB)  \
+  {              \
+    uB  = uM;    \
+    uxB = M_PI*cos(M_PI*x)*sin(M_PI*y)*sin(M_PI*z);	\
+    uyB = M_PI*sin(M_PI*x)*cos(M_PI*y)*sin(M_PI*z);		\
+    uzB = M_PI*sin(M_PI*x)*sin(M_PI*y)*cos(M_PI*z);		\
+  }
+
