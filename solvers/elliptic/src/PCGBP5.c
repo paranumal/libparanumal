c1bf7e7a4ce965687f2e6400d19e4c536705d104
AliKarakus <alikarakus.me@gmail.com>
2018-11-21 10:07:50 -0600

    added a missing function
    Former-commit-id: 8b4381995b57c83f51c0320d64440114a365b0c4

---
+++ solvers/elliptic/src/PCGBP5.c
@@ -0,0 +1,128 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Tim Warburton, Noel Chalmers, Jesse Chan, Ali Karakus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#define TIMER 0
+
+#include "elliptic.h"
+
+void pcgBP5(elliptic_t* elliptic, dfloat lambda, 
+        occa::memory &o_r, occa::memory &o_x, const int MAXIT) {
+
+  mesh_t *mesh = elliptic->mesh;
+  setupAide options = elliptic->options;
+
+  /*aux variables */
+  occa::memory &o_p  = elliptic->o_p;
+  occa::memory &o_z  = elliptic->o_z;
+  occa::memory &o_Ap = elliptic->o_Ap;
+  occa::memory &o_Ax = elliptic->o_Ax;
+
+  // Precon^{-1} (b-A*x)
+  ellipticPreconditioner(elliptic, lambda, o_r, o_z);
+
+  // p = z
+  o_p.copyFrom(o_z); // PCG
+
+  // dot(r,z)
+  rdotz0 = ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_r, o_z);
+  dfloat rdotr1 = 0;
+  dfloat rdotz1 = 0;
+
+  dfloat alpha, beta, pAp = 0;
+
+  for(iter=0; iter<MAXIT, iter++) {
+
+    // [
+    // A*p
+    ellipticOperator(elliptic, lambda, o_p, o_Ap, dfloatString);
+
+    // dot(p,A*p)
+    pAp =  ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_p, o_Ap);
+    // ]
+    
+    // alpha = dot(r,z)/dot(p,A*p)
+    alpha = rdotz0/pAp;
+
+    // x <= x + alpha*p
+    ellipticScaledAdd(elliptic,  alpha, o_p,  1.f, o_x);
+
+#if TIMER
+    occaTimerTic(mesh->device,"Residual update");
+#endif
+    // [
+    // r <= r - alpha*A*p
+    ellipticScaledAdd(elliptic, -alpha, o_Ap, 1.f, o_r);
+
+    // dot(r,r)
+#if 1
+    rdotr1 = ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_r, o_r);
+#else
+    rdotr1 = ellipticWeightedNorm2(elliptic, elliptic->o_invDegree, o_r);
+#endif
+    // ]
+
+#if TIMER
+  occaTimerToc(mesh->device,"Residual update");
+#endif
+
+
+
+#if TIMER
+    occaTimerTic(mesh->device,"Preconditioner");
+#endif
+
+    // [
+    // z = Precon^{-1} r
+    ellipticPreconditioner(elliptic, lambda, o_r, o_z);
+
+    // dot(r,z)
+    rdotz1 = ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_r, o_z);
+    // ]
+    
+    // flexible pcg beta = (z.(-alpha*Ap))/zdotz0
+    // if(options.compareArgs("KRYLOV SOLVER", "PCG+FLEXIBLE") ||
+       // options.compareArgs("KRYLOV SOLVER", "PCG,FLEXIBLE")) {
+      // dfloat zdotAp = ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_z, o_Ap);
+      // beta = -alpha*zdotAp/rdotz0;
+    // } else {
+      beta = rdotz1/rdotz0;
+    // }
+
+    // p = z + beta*p
+    ellipticScaledAdd(elliptic, 1.f, o_z, beta, o_p);
+
+    // switch rdotz0 <= rdotz1
+    rdotz0 = rdotz1;
+
+#if TIMER
+    occaTimerToc(mesh->device,"Preconditioner");
+#endif
+
+    // switch rdotz0,rdotr0 <= rdotz1,rdotr1
+    rdotr0 = rdotr1;
+
+  }
+}
