ad22b2308538ce9cfb9a4f461f2c79153f646a9d
tcew <tim.warburton@gmail.com>
2018-11-21 16:14:00 -0500

    made roofline plotter more robust
    Former-commit-id: 536c0caaf11edce43c56311e920c7e375ff10a78

--- utilities/roofline/rooflinePlot.m
+++ utilities/roofline/rooflinePlot.m
@@ -1,18 +1,24 @@
 
 function rooflinePlot(kernelName)
 
-  per = load(kernelName);
+  ['!' kernelName '!']
+  perf = load(kernelName)
 
-  scatter(per(:,1),per(:,2), 'filled', 'ko');
+  clf
+  
+  scatter(perf(:,1),perf(:,2), 'filled', 'ko');
 
   hold on;
 
   set(gca, 'FontSize', 14);
   xlabel('Arithmetic Intensity (FP64 flops)/deviceMemoryBytes', 'FontSize', 14);
   ylabel('FP64 GFLOPS/s', 'FontSize', 14);
-  title(\"%s\", 'FontSize', 16, 'Interpreter', 'None');\n", kernelName);
+title(kernelName, 'FontSize', 16, 'Interpreter', 'None');
   
   % superimpose roofline
-  hold on;
+
   plot(perf(:,1),perf(:,1).*perf(:,3),'r*', 'LineWidth', 2);
   axis([0,ceil(max(perf(:,1))),0,max(max(perf(:,1).*perf(:,3),perf(:,2)))]);
+  hold off;
+  
+  pause(1);
