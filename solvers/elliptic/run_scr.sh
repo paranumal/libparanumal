34df70833087c59a6a0a34596686191de07ebd59
AliKarakus <alikarakus.me@gmail.com>
2018-11-27 12:55:31 -0500

    load new box mesh
    Former-commit-id: ba5630545c89214ff09a76bdf14c0c9042e163a4

--- solvers/elliptic/run_scr.sh
+++ solvers/elliptic/run_scr.sh
@@ -4,12 +4,12 @@
 #mpiexec -np 8 ./ellipticMain ./setups/setupHex3D.rc
 
 maxiter=100;
-mesh_base=../../meshes/cubeHex;
+mesh_base=../../meshes/box;
 mesh_extension=.msh;
 for size in 1 2 4 8; do
-    for mesh_id in 10 12; do
-	for N in 2 3 4 5 6 7 8 9 11 15 ;do
+    for mesh_id in `seq 6 18`; do
+	for N in 5 6 7 8 9 11 15 ;do
 	    meshfile=${mesh_base}${mesh_id}${mesh_extension}
-            echo Reading ${meshfile} 
+            echo Running ${meshfile} at order ${N} 
 	    mpiexec -np ${size} ./BP ./setups/setupHex3D.rc ${meshfile} ${N} ${maxiter};
 done;done;done;
