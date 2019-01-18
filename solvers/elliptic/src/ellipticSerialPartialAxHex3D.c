2521c8d6358e18ad66c3035207bec166d2214940
tcew <tcew@nrlogin1.cluster>
2019-01-18 16:49:50 -0500

    WIP; version from E5 2680v4 runs
    Former-commit-id: 8aae764cb12c952d85a07b158dc76f4ac0eb20ce

--- solvers/elliptic/src/ellipticSerialPartialAxHex3D.c
+++ solvers/elliptic/src/ellipticSerialPartialAxHex3D.c
@@ -75,6 +75,7 @@ void ellipticSerialPartialAxHexKernel3D (const hlong Nelements,
     
     const dlong element = elementList[e];
 
+#if 1
     for(int k = 0; k < p_Nq; k++) {
       for(int j=0;j<p_Nq;++j){
         for(int i=0;i<p_Nq;++i){
@@ -85,6 +86,74 @@ void ellipticSerialPartialAxHexKernel3D (const hlong Nelements,
       }
     }
 
+    for(int k=0;k<p_Nq;++k){
+      for(int j=0;j<p_Nq;++j){
+        for(int i=0;i<p_Nq;++i){
+
+          const dlong gbase = element*p_Nggeo*c_Np + k*p_Nq*p_Nq + j*p_Nq + i;
+          const dfloat r_G00 = ggeo[gbase+G00ID*p_Np];
+          const dfloat r_G01 = ggeo[gbase+G01ID*p_Np];
+          const dfloat r_G11 = ggeo[gbase+G11ID*p_Np];
+          const dfloat r_G02 = ggeo[gbase+G02ID*p_Np];
+          const dfloat r_G12 = ggeo[gbase+G12ID*p_Np];
+          const dfloat r_G22 = ggeo[gbase+G22ID*p_Np];
+
+          dfloat qr = 0.f;
+          dfloat qs = 0.f;
+          dfloat qt = 0.f;
+
+          for(int m = 0; m < p_Nq; m++) {
+            qr += s_S[m][i]*s_q[k][j][m];  
+            qs += s_S[m][j]*s_q[k][m][i];           
+            qt += s_S[m][k]*s_q[m][j][i]; 
+          }
+
+          dfloat Gqr = r_G00*qr;
+          Gqr += r_G01*qs;
+          Gqr += r_G02*qt;
+          
+          dfloat Gqs = r_G01*qr;
+          Gqs += r_G11*qs;
+          Gqs += r_G12*qt;
+
+          dfloat Gqt = r_G02*qr;
+          Gqt += r_G12*qs;
+          Gqt += r_G22*qt;
+          
+          s_Gqr[k][j][i] = Gqr;
+          s_Gqs[k][j][i] = Gqs;
+          s_Gqt[k][j][i] = Gqt;
+        }
+      }
+    }
+
+    for(int k = 0;k < p_Nq; k++){
+      for(int j=0;j<p_Nq;++j){
+        for(int i=0;i<p_Nq;++i){
+#if 0
+          const dlong gbase = element*p_Nggeo*p_Np + k*p_Nq*p_Nq + j*p_Nq + i;
+          const dfloat r_GwJ = ggeo[gbase+p_GWJID*p_Np];
+
+          dfloat r_Aq = r_GwJ*lambda*s_q[k][j][i];
+#endif
+          dfloat r_Aqr = 0, r_Aqs = 0, r_Aqt = 0;
+
+
+          for(int m = 0; m < p_Nq; m++)
+            r_Aqr += s_D[m][i]*s_Gqr[k][j][m];
+          for(int m = 0; m < p_Nq; m++)
+            r_Aqs += s_D[m][j]*s_Gqs[k][m][i];
+          for(int m = 0; m < p_Nq; m++)
+            r_Aqt += s_D[m][k]*s_Gqt[m][j][i];
+
+          const dlong id = element*p_Np +k*p_Nq*p_Nq+ j*p_Nq + i;
+          Aq[id] = r_Aqr + r_Aqs + r_Aqt; // +r_Aq;
+        }
+      }
+    }
+#endif
+
+    // ok
 #if 0
     for(int k=0;k<p_Nq;++k){
       for(int j=0;j<p_Nq;++j){
@@ -102,86 +171,19 @@ void ellipticSerialPartialAxHexKernel3D (const hlong Nelements,
           dfloat qs = 0.f;
           dfloat qt = 0.f;
 
-          for(int m = 0; m < p_Nq; m++) {
-	    qr += s_S[m][i]*s_q[k][j][m];  
-	    qs += s_S[m][j]*s_q[k][m][i]; 	    
-	    qt += s_S[m][k]*s_q[m][j][i]; 
-          }
+	  const dfloat * __restrict__ qei = q + element*p_Np + k*p_Nq*p_Nq + j*p_Nq;
+	  const dfloat * __restrict__ qej = q + element*p_Np + k*p_Nq*p_Nq + i;
+	  const dfloat * __restrict__ qek = q + element*p_Np + j*p_Nq + i;
 
-	  dfloat Gqr = r_G00*qr;
-	  Gqr += r_G01*qs;
-	  Gqr += r_G02*qt;
-	  
-	  dfloat Gqs = r_G01*qr;
-	  Gqs += r_G11*qs;
-	  Gqs += r_G12*qt;
-
-	  dfloat Gqt = r_G02*qr;
-	  Gqt += r_G12*qs;
-	  Gqt += r_G22*qt;
-	  
-          s_Gqr[k][j][i] = Gqr;
-          s_Gqs[k][i][j] = Gqs;
-          s_Gqt[j][i][k] = Gqt;
-        }
-      }
-    }
-
-    for(int k = 0;k < p_Nq; k++){
-      for(int j=0;j<p_Nq;++j){
-
-	const dfloat * __restrict__ r_Gqr = s_Gqr[k][j];
-	
-	for(int i=0;i<p_Nq;++i){
-          dfloat r_Aqr = 0, r_Aqs = 0, r_Aqt = 0;
-
-	  const dfloat * __restrict__ r_Gqs = s_Gqs[k][i];
-	  const dfloat * __restrict__ r_Gqt = s_Gqt[j][i];
-
-	  r_Gqr = (dfloat*)__builtin_assume_aligned(r_Gqr, USE_OCCA_MEM_BYTE_ALIGN) ;
-	  r_Gqs = (dfloat*)__builtin_assume_aligned(r_Gqs, USE_OCCA_MEM_BYTE_ALIGN) ;
-	  r_Gqt = (dfloat*)__builtin_assume_aligned(r_Gqt, USE_OCCA_MEM_BYTE_ALIGN) ;
-	  
           for(int m = 0; m < p_Nq; m++)
-            r_Aqr += s_D[m][i]*r_Gqr[m];
-	  for(int m = 0; m < p_Nq; m++)
-            r_Aqs += s_D[m][j]*r_Gqs[m];
-	  for(int m = 0; m < p_Nq; m++)
-            r_Aqt += s_D[m][k]*r_Gqt[m];
-
-          const dlong id = element*p_Np +k*p_Nq*p_Nq+ j*p_Nq + i;
-          Aq[id] = r_Aqr + r_Aqs + r_Aqt; // +r_Aq;
-        }
-      }
-    }
-#endif
-    
-    
-#if 1
-    // ok
-    
-    for(int k=0;k<p_Nq;++k){
-      for(int j=0;j<p_Nq;++j){
-        for(int i=0;i<p_Nq;++i){
-
-          const dlong gbase = element*p_Nggeo*c_Np + k*p_Nq*p_Nq + j*p_Nq + i;
-          const dfloat r_G00 = ggeo[gbase+G00ID*p_Np];
-          const dfloat r_G01 = ggeo[gbase+G01ID*p_Np];
-          const dfloat r_G11 = ggeo[gbase+G11ID*p_Np];
-          const dfloat r_G02 = ggeo[gbase+G02ID*p_Np];
-          const dfloat r_G12 = ggeo[gbase+G12ID*p_Np];
-          const dfloat r_G22 = ggeo[gbase+G22ID*p_Np];
-
-          dfloat qr = 0.f;
-          dfloat qs = 0.f;
-          dfloat qt = 0.f;
-
+	    qr += s_S[m][i]*qei[m]; 
+	  for(int m = 0; m < p_Nq; m++) 
+	    //	    qs += s_S[m][j]*s_q[k][m][i]; 	    
+	    qs += s_S[m][j]*qej[m*p_Nq];
           for(int m = 0; m < p_Nq; m++) {
-	    qr += s_S[m][i]*s_q[k][j][m];  
-	    qs += s_S[m][j]*s_q[k][m][i]; 	    
-	    qt += s_S[m][k]*s_q[m][j][i]; 
-          }
-
+	    //	    qt += s_S[m][k]*s_q[m][j][i]; 
+	    qt += s_S[m][k]*qek[m*p_Nq*p_Nq];
+	  }
 	  dfloat Gqr = r_G00*qr;
 	  Gqr += r_G01*qs;
 	  Gqr += r_G02*qt;
@@ -214,20 +216,120 @@ void ellipticSerialPartialAxHexKernel3D (const hlong Nelements,
 
 
           for(int m = 0; m < p_Nq; m++)
-            r_Aqr += s_D[m][i]*s_Gqr[k][j][m];
+            r_Aqr += s_S[i][m]*s_Gqr[k][j][m];
 	  for(int m = 0; m < p_Nq; m++)
-            r_Aqs += s_D[m][j]*s_Gqs[k][m][i];
+            r_Aqs += s_S[j][m]*s_Gqs[k][m][i];
 	  for(int m = 0; m < p_Nq; m++)
-            r_Aqt += s_D[m][k]*s_Gqt[m][j][i];
+            r_Aqt += s_S[k][m]*s_Gqt[m][j][i];
 
           const dlong id = element*p_Np +k*p_Nq*p_Nq+ j*p_Nq + i;
           Aq[id] = r_Aqr + r_Aqs + r_Aqt; // +r_Aq;
         }
       }
     }
-
 #endif
+
+#if 0
+    // sophisticated 9.48e7
+    dfloat s_qr[p_Nq][p_Nq][p_Nq] __attribute__((aligned(USE_OCCA_MEM_BYTE_ALIGN)));
+    dfloat s_qs[p_Nq][p_Nq][p_Nq] __attribute__((aligned(USE_OCCA_MEM_BYTE_ALIGN)));
+    dfloat s_qt[p_Nq][p_Nq][p_Nq] __attribute__((aligned(USE_OCCA_MEM_BYTE_ALIGN)));
     
+    // loop over slices 
+    for(int k=0;k<p_Nq;++k){
+      for(int j=0;j<p_Nq;++j){
+	const dfloat * __restrict__ qei = q + element*p_Np + k*p_Nq*p_Nq + j*p_Nq;
+	
+        for(int i=0;i<p_Nq;++i){
+          dfloat qr = 0.f;
+          for(int m = 0; m < p_Nq; m++)
+	    qr += s_S[m][i]*qei[m]; 
+	  s_qr[k][j][i] = qr;
+	}
+      }
+     
+      for(int i=0;i<p_Nq;++i){
+	const dfloat * __restrict__ qej = q + element*p_Np + k*p_Nq*p_Nq + i;
+	  
+	for(int j=0;j<p_Nq;++j){
+	  
+	  dfloat qs = 0.f;
+	  for(int m = 0; m < p_Nq; m++)
+	    qs += s_S[m][j]*qej[m*p_Nq]; 
+	  
+	  s_qs[k][j][i] = qs;
+	}
+      }
+    }
+    
+    for(int j=0;j<p_Nq;++j){
+      for(int i=0;i<p_Nq;++i){
+	const dfloat * __restrict__ qek = q + element*p_Np + j*p_Nq + i;
+	
+	for(int k=0;k<p_Nq;++k){
+	  dfloat qt = 0;
+          for(int m = 0; m < p_Nq; m++) 
+	    qt += s_S[m][k]*qek[m*p_Nq*p_Nq];
+	  
+	  s_qt[k][j][i] = qt;
+	}
+      }
+    }
+    
+    for(int k=0;k<p_Nq;++k){
+      for(int j=0;j<p_Nq;++j){
+	for(int i=0;i<p_Nq;++i){
+	  const dlong gbase = element*p_Nggeo*c_Np + k*p_Nq*p_Nq + j*p_Nq + i;
+	  const dfloat r_G00 = ggeo[gbase+G00ID*p_Np];
+	  const dfloat r_G01 = ggeo[gbase+G01ID*p_Np];
+	  const dfloat r_G11 = ggeo[gbase+G11ID*p_Np];
+	  const dfloat r_G02 = ggeo[gbase+G02ID*p_Np];
+	  const dfloat r_G12 = ggeo[gbase+G12ID*p_Np];
+	  const dfloat r_G22 = ggeo[gbase+G22ID*p_Np];
+	  
+	  const dfloat qr = s_qr[k][j][i];
+	  const dfloat qs = s_qs[k][j][i];
+	  const dfloat qt = s_qt[k][j][i];
+	  
+	  dfloat Gqr = r_G00*qr;
+	  Gqr += r_G01*qs;
+	  Gqr += r_G02*qt;
+	
+	  dfloat Gqs = r_G01*qr;
+	  Gqs += r_G11*qs;
+	  Gqs += r_G12*qt;
+	  
+	  dfloat Gqt = r_G02*qr;
+	  Gqt += r_G12*qs;
+	  Gqt += r_G22*qt;
+	  
+	  s_Gqr[k][j][i] = Gqr;
+	  s_Gqs[k][j][i] = Gqs;
+	  s_Gqt[k][j][i] = Gqt;
+	}
+      }
+    }
+    
+    for(int k = 0;k < p_Nq; k++){
+      for(int j=0;j<p_Nq;++j){
+        for(int i=0;i<p_Nq;++i){
+
+          dfloat r_Aqr = 0, r_Aqs = 0, r_Aqt = 0;
+
+          for(int m = 0; m < p_Nq; m++)
+            r_Aqr += s_S[i][m]*s_Gqr[k][j][m];
+	  for(int m = 0; m < p_Nq; m++)
+            r_Aqs += s_S[j][m]*s_Gqs[k][m][i];
+	  for(int m = 0; m < p_Nq; m++)
+            r_Aqt += s_S[k][m]*s_Gqt[m][j][i];
+
+          const dlong id = element*p_Np +k*p_Nq*p_Nq+ j*p_Nq + i;
+          Aq[id] = r_Aqr + r_Aqs + r_Aqt; // +r_Aq;
+        }
+      }
+    }
+#endif
+
   }
 }
 
