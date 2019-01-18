e9fd7cbaf537510f8abd0e6a530a8dde78f96f7a
tcew <tim.warburton@gmail.com>
2019-01-17 21:18:14 -0500

    forced vectorization of specialized CPU kernels
    Former-commit-id: 74399f1921258912bc8f1e1426819ccd6417a032

---
+++ solvers/elliptic/src/ellipticUpdatePCG.c
@@ -0,0 +1,165 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Tim Warburton, Noel Chalmers, Jesse Chan, Ali Karakus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include "elliptic.h"
+
+template < int p_Nq >
+dfloat ellipticSerialUpdatePCGKernel(const hlong Nelements,
+				     const dfloat * __restrict__ cpu_invDegree,
+				     const dfloat * __restrict__ cpu_p,
+				     const dfloat * __restrict__ cpu_Ap,
+				     const dfloat alpha,
+				     dfloat * __restrict__ cpu_x,
+				     dfloat * __restrict__ cpu_r){
+
+#define p_Np (p_Nq*p_Nq*p_Nq)
+
+  cpu_p  = (dfloat*)__builtin_assume_aligned(cpu_p,  64) ;
+  cpu_Ap = (dfloat*)__builtin_assume_aligned(cpu_Ap, 64) ;
+  cpu_x  = (dfloat*)__builtin_assume_aligned(cpu_x,  64) ;
+  cpu_r  = (dfloat*)__builtin_assume_aligned(cpu_r,  64) ;
+
+  cpu_invDegree = (dfloat*)__builtin_assume_aligned(cpu_invDegree,  64) ;
+  
+  dfloat rdotr = 0;
+  
+  cpu_p = (dfloat*)__builtin_assume_aligned(cpu_p, 64) ;
+
+  for(hlong e=0;e<Nelements;++e){
+    for(int i=0;i<p_Np;++i){
+      const hlong n = e*p_Np+i;
+      cpu_x[n] += alpha*cpu_p[n];
+
+      const dfloat rn = cpu_r[n] - alpha*cpu_Ap[n];
+      rdotr += rn*rn*cpu_invDegree[n];
+      cpu_r[n] = rn;
+    }
+  }
+
+#undef p_Np
+  
+  return rdotr;
+}
+				     
+dfloat ellipticSerialUpdatePCG(const int Nq, const hlong Nelements,
+			       occa::memory &o_invDegree, occa::memory &o_p, occa::memory &o_Ap, const dfloat alpha,
+			       occa::memory &o_x, occa::memory &o_r){
+
+  const dfloat * __restrict__ cpu_p  = (dfloat*)__builtin_assume_aligned(o_p.ptr(), 64) ;
+  const dfloat * __restrict__ cpu_Ap = (dfloat*)__builtin_assume_aligned(o_Ap.ptr(), 64) ;
+  const dfloat * __restrict__ cpu_invDegree = (dfloat*)__builtin_assume_aligned(o_invDegree.ptr(), 64) ;
+
+  dfloat * __restrict__ cpu_x  = (dfloat*)__builtin_assume_aligned(o_x.ptr(), 64) ;
+  dfloat * __restrict__ cpu_r  = (dfloat*)__builtin_assume_aligned(o_r.ptr(), 64) ;
+
+  dfloat rdotr = 0;
+  
+  switch(Nq){
+  case  2: rdotr = ellipticSerialUpdatePCGKernel <  2 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break; 
+  case  3: rdotr = ellipticSerialUpdatePCGKernel <  3 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  4: rdotr = ellipticSerialUpdatePCGKernel <  4 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  5: rdotr = ellipticSerialUpdatePCGKernel <  5 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  6: rdotr = ellipticSerialUpdatePCGKernel <  6 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  7: rdotr = ellipticSerialUpdatePCGKernel <  7 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  8: rdotr = ellipticSerialUpdatePCGKernel <  8 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case  9: rdotr = ellipticSerialUpdatePCGKernel <  9 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case 10: rdotr = ellipticSerialUpdatePCGKernel < 10 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case 11: rdotr = ellipticSerialUpdatePCGKernel < 11 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  case 12: rdotr = ellipticSerialUpdatePCGKernel < 12 > (Nelements, cpu_invDegree, cpu_p, cpu_Ap, alpha, cpu_x, cpu_r); break;
+  }
+
+  return rdotr;
+}
+
+dfloat ellipticUpdatePCG(elliptic_t *elliptic,
+			 occa::memory &o_p, occa::memory &o_Ap, const dfloat alpha,
+			 occa::memory &o_x, occa::memory &o_r){
+
+  mesh_t *mesh = elliptic->mesh;
+  setupAide options = elliptic->options;
+
+  if(options.compareArgs("THREAD MODEL", "Serial") &&
+     options.compareArgs("DISCRETIZATION", "CONTINUOUS")){
+    
+    dfloat rdotr1 = ellipticSerialUpdatePCG(mesh->Nq, mesh->Nelements, 
+					    elliptic->o_invDegree,
+					    o_p, o_Ap, alpha, o_x, o_r);
+
+    dfloat globalrdotr1 = 0;
+    MPI_Allreduce(&rdotr1, &globalrdotr1, 1, MPI_DFLOAT, MPI_SUM, mesh->comm);
+    
+    return globalrdotr1;
+  }
+  
+  int DEBUG_ENABLE_REDUCTIONS = 1;
+  options.getArgs("DEBUG ENABLE REDUCTIONS", DEBUG_ENABLE_REDUCTIONS);
+  
+  dfloat rdotr1 = 0;
+  
+  if(!options.compareArgs("DISCRETIZATION", "CONTINUOUS")){
+    
+    // x <= x + alpha*p
+    ellipticScaledAdd(elliptic,  alpha, o_p,  1.f, o_x);
+    
+    // [
+    // r <= r - alpha*A*p
+    ellipticScaledAdd(elliptic, -alpha, o_Ap, 1.f, o_r);
+    
+    // dot(r,r)
+    if(DEBUG_ENABLE_REDUCTIONS==1){
+#if 0
+      rdotr1 = ellipticCascadingWeightedInnerProduct(elliptic, elliptic->o_invDegree, o_r, o_r);
+#else
+      rdotr1 = ellipticWeightedNorm2(elliptic, elliptic->o_invDegree, o_r);
+#endif
+    }
+    else
+      rdotr1 = 1;
+  }else{
+    
+    // x <= x + alpha*p
+    // r <= r - alpha*A*p
+    // dot(r,r)
+    elliptic->updatePCGKernel(mesh->Nelements*mesh->Np, elliptic->NblocksUpdatePCG,
+			      elliptic->o_invDegree, o_p, o_Ap, alpha, o_x, o_r, elliptic->o_tmpNormr);
+
+    elliptic->o_tmpNormr.copyTo(elliptic->tmpNormr);
+
+    rdotr1 = 0;
+    for(int n=0;n<elliptic->NblocksUpdatePCG;++n){
+      rdotr1 += elliptic->tmpNormr[n];
+    }
+    
+    dfloat globalrdotr1 = 0;
+    MPI_Allreduce(&rdotr1, &globalrdotr1, 1, MPI_DFLOAT, MPI_SUM, mesh->comm);
+
+    rdotr1 = globalrdotr1;
+    
+  }
+
+  return rdotr1;
+}
+
--- solvers/elliptic/src/ellipticVectors.c
+++ solvers/elliptic/src/ellipticVectors.c
@@ -26,86 +26,7 @@ SOFTWARE.
 
 #include "elliptic.h"
 
-void ellipticScaledAdd(elliptic_t *elliptic, dfloat alpha, occa::memory &o_a, dfloat beta, occa::memory &o_b){
 
-  mesh_t *mesh = elliptic->mesh;
-
-  dlong Ntotal = mesh->Nelements*mesh->Np;
-
-  // b[n] = alpha*a[n] + beta*b[n] n\in [0,Ntotal)
-  elliptic->scaledAddKernel(Ntotal, alpha, o_a, beta, o_b);
-}
-
-dfloat ellipticWeightedInnerProduct(elliptic_t *elliptic, occa::memory &o_w, occa::memory &o_a, occa::memory &o_b){
-
-  mesh_t *mesh = elliptic->mesh;
-  dfloat *tmp = elliptic->tmp;
-  dlong Nblock = elliptic->Nblock;
-  dlong Nblock2 = elliptic->Nblock2;
-  dlong Ntotal = mesh->Nelements*mesh->Np;
-
-  if(elliptic->options.compareArgs("THREAD MODEL", "Serial")){
-    
-    const dfloat *cpu_w = (dfloat*) o_w.ptr();
-    const dfloat *cpu_a = (dfloat*) o_a.ptr();
-    const dfloat *cpu_b = (dfloat*) o_b.ptr();
-    const dfloat *cpu_invDegree = (dfloat*) elliptic->o_invDegree.ptr();
-    
-    // w'*(a.b)
-    dfloat wab = 0;
-
-    const hlong M = mesh->Nelements*mesh->Np;
-    for(hlong i=0;i<M;++i){
-      const dfloat ai = cpu_a[i];
-      const dfloat bi = cpu_b[i];
-      wab += ai*bi*cpu_invDegree[i];
-    }
-
-    dfloat globalwab = 0;
-    MPI_Allreduce(&wab, &globalwab, 1, MPI_DFLOAT, MPI_SUM, mesh->comm);
-    
-    return globalwab;
-  }
-
-
-  
-  occa::memory &o_tmp = elliptic->o_tmp;
-  occa::memory &o_tmp2 = elliptic->o_tmp2;
-
-  if(elliptic->options.compareArgs("DISCRETIZATION","CONTINUOUS"))
-    elliptic->weightedInnerProduct2Kernel(Ntotal, o_w, o_a, o_b, o_tmp);
-  else
-    elliptic->innerProductKernel(Ntotal, o_a, o_b, o_tmp);
-
-  /* add a second sweep if Nblock>Ncutoff */
-  dlong Ncutoff = 100;
-  dlong Nfinal;
-  if(Nblock>Ncutoff){
-
-    mesh->sumKernel(Nblock, o_tmp, o_tmp2);
-
-    o_tmp2.copyTo(tmp);
-
-    Nfinal = Nblock2;
-	
-  }
-  else{
-    o_tmp.copyTo(tmp);
-    
-    Nfinal = Nblock;
-
-  }    
-
-  dfloat wab = 0;
-  for(dlong n=0;n<Nfinal;++n){
-    wab += tmp[n];
-  }
-
-  dfloat globalwab = 0;
-  MPI_Allreduce(&wab, &globalwab, 1, MPI_DFLOAT, MPI_SUM, mesh->comm);
-
-  return globalwab;
-}
 
 typedef union intorfloat {
   int ier;
@@ -160,7 +81,7 @@ dfloat ellipticCascadingWeightedInnerProduct(elliptic_t *elliptic, occa::memory 
   return wab;
 }
 
-
+#if 0
 
 dfloat ellipticWeightedNorm2(elliptic_t *elliptic, occa::memory &o_w, occa::memory &o_a){
 
@@ -171,18 +92,18 @@ dfloat ellipticWeightedNorm2(elliptic_t *elliptic, occa::memory &o_w, occa::memo
   dlong Ntotal = mesh->Nelements*mesh->Np;
 
   if(elliptic->options.compareArgs("THREAD MODEL", "Serial")){
-    
-    const dfloat *cpu_w = (dfloat*) o_w.ptr();
-    const dfloat *cpu_a = (dfloat*) o_a.ptr();
-    const dfloat *cpu_invDegree = (dfloat*) elliptic->o_invDegree.ptr();
-    
+
+    const dfloat * __restrict__ cpu_w = (dfloat*)__builtin_assume_aligned(o_w.ptr(), 64);
+    const dfloat * __restrict__ cpu_a = (dfloat*)__builtin_assume_aligned(o_a.ptr(), 64) ;
+
     // w'*(a.a)
     dfloat wa2 = 0;
 
     const hlong M = mesh->Nelements*mesh->Np;
+
     for(hlong i=0;i<M;++i){
       const dfloat ai = cpu_a[i];
-      wa2 += ai*ai*cpu_invDegree[i];
+      wa2 += ai*ai*cpu_w[i];
     }
 
     dfloat globalwa2 = 0;
@@ -229,6 +150,7 @@ dfloat ellipticWeightedNorm2(elliptic_t *elliptic, occa::memory &o_w, occa::memo
   return globalwab;
 }
 
+#endif
 
 dfloat ellipticInnerProduct(elliptic_t *elliptic, occa::memory &o_a, occa::memory &o_b){
 
