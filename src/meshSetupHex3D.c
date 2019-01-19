1f131e155e259e29b4697d63cf3f2137f0a7da56
tcew <tcew@nrlogin1.cluster>
2019-01-19 10:19:50 -0500

    defaulting to parmetis

--- src/meshSetupHex3D.c
+++ src/meshSetupHex3D.c
@@ -32,8 +32,8 @@ mesh3D *meshSetupHex3D(char *filename, int N){
   mesh3D *mesh = meshParallelReaderHex3D(filename);
 
   // partition elements using Morton ordering & parallel sort
-  meshGeometricPartition3D(mesh);
-  //  meshRecursiveSpectralBisectionPartition(mesh);
+  //  meshGeometricPartition3D(mesh);
+  meshRecursiveSpectralBisectionPartition(mesh);
 
   // connect elements using parallel sort
   meshParallelConnect(mesh);
