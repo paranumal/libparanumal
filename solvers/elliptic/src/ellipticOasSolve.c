78f9687aaed5dfb8dc6afce3da6144e2beec8b97
Tim Warburton <tim.warburton@gmail.com>
2019-01-29 07:58:57 -0500

    WIP: removed infrastructure used to support use of ellipticSolve in OAS precon

--- solvers/elliptic/src/ellipticOasSolve.c
+++ solvers/elliptic/src/ellipticOasSolve.c
@@ -29,27 +29,23 @@ SOFTWARE.
 void ellipticOasSolve(elliptic_t *elliptic, dfloat lambda,
 		      occa::memory &o_r, occa::memory &o_z){
 
-  // TW: do not interleave these tasks yet
-  
-  // 1. restrict to coarse grid
   precon_t *precon = elliptic->precon;
   mesh_t *mesh = elliptic->mesh;
 
   elliptic_t *elliptic1 = (elliptic_t*) precon->ellipticOneRing; // should rename
   mesh_t *mesh1 = elliptic1->mesh;
-
-  // hack to zero initial guess
-  dfloat *h_x = (dfloat*) calloc(mesh1->Np*mesh1->Nelements, sizeof(dfloat));
-  elliptic1->o_x.copyFrom(h_x);
-  free(h_x);
-
+  
+  elliptic_t *ellipticOasCoarse = (elliptic_t*) (precon->ellipticOasCoarse);
+  mesh_t   *meshCoarse   = ellipticOasCoarse->mesh;
+  
   // TW: possibility these device have difference queues
   mesh1->device.finish();
   mesh->device.finish();
 
-  // 3. collect patch rhs    
+  // 1. collect patch residual
   ellipticOneRingExchange(elliptic, elliptic1, mesh1->Np*sizeof(dfloat), o_r, elliptic1->o_r);
 
+  // mask local overlapping patch residual
   if (elliptic1->Nmasked) 
     mesh1->maskKernel(elliptic1->Nmasked, elliptic1->o_maskIds, elliptic1->o_r);
   
@@ -57,17 +53,9 @@ void ellipticOasSolve(elliptic_t *elliptic, dfloat lambda,
   mesh1->device.finish();
   mesh->device.finish();
 
-  // TW: what tolerance to use ?
-  dfloat tol1 = 1e-2;
-  
-  // patch solve
-  if(mesh->rank==0) printf("Starting extended partition iterations:\n");
-
-  //  ellipticSolve(elliptic1, lambda, tol1, elliptic1->o_r, elliptic1->o_x); // may need to zero o_x
-
   ellipticPreconditioner(elliptic1, lambda, elliptic1->o_r, elliptic1->o_x); // may need to zero o_x
 
-  // will gather over all patches - so have to remove local multiplicity
+  // gathering over all patches - so have to remove local multiplicity
   elliptic1->dotMultiplyKernel(mesh1->Nelements*mesh1->Np, elliptic1->ogs->o_invDegree, elliptic1->o_x, elliptic1->o_z);
   
   // sum up overlapping patches
@@ -79,31 +67,13 @@ void ellipticOasSolve(elliptic_t *elliptic, dfloat lambda,
   //   a. call solver
   //   b. prolongate (watch out for +=)
 
-  // TW: QUESTIONABLE FROM HERE ---->
-  if(mesh->rank==0) printf("Starting coarse grid iterations:\n");
-  
-  elliptic_t *ellipticOasCoarse = (elliptic_t*) (precon->ellipticOasCoarse);
-  mesh_t   *meshCoarse   = ellipticOasCoarse->mesh;
-
   mesh1->device.finish();
   mesh->device.finish();
   meshCoarse->device.finish();
   
-  occa::memory o_tmpCoarse = mesh->device.malloc(meshCoarse->Np*meshCoarse->Nelements*sizeof(dfloat));
-  
-#if 1
   precon->oasRestrictionKernel(meshCoarse->Nelements,
 			       precon->o_oasRestrictionMatrix,
 			       o_r, ellipticOasCoarse->o_r);
-#else
-  // do this on mesh device
-  precon->oasRestrictionKernel(meshCoarse->Nelements,
-			       precon->o_oasRestrictionMatrix,
-			       o_r, o_tmpCoarse);
-
-  // copy from mesh device to coarse device
-  o_tmpCoarse.copyTo(ellipticOasCoarse->o_r.ptr());
-#endif
   
   mesh1->device.finish();
   mesh->device.finish();
@@ -121,14 +91,7 @@ void ellipticOasSolve(elliptic_t *elliptic, dfloat lambda,
     meshCoarse->maskKernel(ellipticOasCoarse->Nmasked, ellipticOasCoarse->o_maskIds, ellipticOasCoarse->o_r);
   
   // <----------
-  
-  dfloat *h_xCoarse = (dfloat*) calloc(meshCoarse->Np*meshCoarse->Nelements, sizeof(dfloat));
-  ellipticOasCoarse->o_x.copyFrom(h_xCoarse);
-  free(h_xCoarse);
 
-  dfloat tolCoarse = 1e-2;
-  
-  //  ellipticSolve(ellipticOasCoarse, lambda, tolCoarse, ellipticOasCoarse->o_r, ellipticOasCoarse->o_x);
   ellipticPreconditioner(ellipticOasCoarse, lambda, ellipticOasCoarse->o_r, ellipticOasCoarse->o_x);
   
   mesh1->device.finish();
@@ -137,28 +100,14 @@ void ellipticOasSolve(elliptic_t *elliptic, dfloat lambda,
 
   // prolongate to QN (note kernel expects restriction matrix)
   // do we need to weight the sum against patches?
-#if 1
   precon->oasProlongationKernel(mesh->Nelements, precon->o_oasRestrictionMatrix,
 				ellipticOasCoarse->o_x, o_z);
-#else
-  // copy from coarse device to mesh device
-  o_tmpCoarse.copyFrom(ellipticOasCoarse->o_x.ptr());
-
-  // this happens on the mesh device
-  precon->oasProlongationKernel(mesh->Nelements, precon->o_oasRestrictionMatrix,
-				o_tmpCoarse, o_z);
-#endif
 
   mesh1->device.finish();
   mesh->device.finish();
   meshCoarse->device.finish();
 
-
   if (elliptic->Nmasked) 
     mesh->maskKernel(elliptic->Nmasked, elliptic->o_maskIds, o_z);
-  
-  o_tmpCoarse.free();
-  
-  if(mesh->rank==0) printf("Ending coarse grid iterations:\n Outer ");  
   
 }
