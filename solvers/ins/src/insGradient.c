524efc5f9b32f3e9aec09e1b433707073222b512
tcew <tim.warburton@gmail.com>
2019-02-09 12:01:43 -0500

    WIP: fix use of hlong versus dlong in gradient, subcycle, divergence. Introduced nonblocking halo exchange for divergence and gradient

--- solvers/ins/src/insGradient.c
+++ solvers/ins/src/insGradient.c
@@ -26,57 +26,126 @@ SOFTWARE.
 
 #include "ins.h"
 
+#define USE_THIN_HALO 1
+
 // complete a time step using LSERK4
 void insGradient(ins_t *ins, dfloat time, occa::memory o_P, occa::memory o_GP){
 
   mesh_t *mesh = ins->mesh;
 
   //  if (ins->pOptions.compareArgs("DISCRETIZATION","IPDG")) {
-  {
-    if(mesh->totalHaloPairs>0){
-      ins->pressureHaloExtractKernel(mesh->Nelements,
-                                 mesh->totalHaloPairs,
-                                 mesh->o_haloElementList,
-                                 o_P,
-                                 ins->o_pHaloBuffer);
+  
+  if(mesh->totalHaloPairs>0){
 
-      // copy extracted halo to HOST
-      ins->o_pHaloBuffer.copyTo(ins->pSendBuffer);
+    // make sure compute device is ready to perform halo extract
+    mesh->device.finish();
+    
+    // switch to data stream
+    mesh->device.setStream(mesh->dataStream);
 
-      // start halo exchange
-      meshHaloExchangeStart(mesh,
-			    mesh->Np*sizeof(dfloat),
-                           ins->pSendBuffer,
-                           ins->pRecvBuffer);
-    }
+#if USE_THIN_HALO==0
+    
+    ins->pressureHaloExtractKernel(mesh->Nelements,
+				   mesh->totalHaloPairs,
+				   mesh->o_haloElementList,
+				   o_P,
+				   ins->o_pHaloBuffer);
+    
+    // copy extracted halo to HOST
+    ins->o_pHaloBuffer.copyTo(ins->pSendBuffer, "async: true");
+    
+#else
+
+    int one = 1;
+    dlong Ndata = one*mesh->Nfp*mesh->totalHaloPairs;
+
+    ins->haloGetKernel(mesh->totalHaloPairs,
+		       one,
+		       ins->fieldOffset,
+		       mesh->o_haloElementList,
+		       mesh->o_haloGetNodeIds,
+		       o_P,
+		       ins->o_pHaloBuffer);   
+    
+    // copy extracted halo to HOST 
+    ins->o_pHaloBuffer.copyTo(ins->pSendBuffer,
+			      Ndata*sizeof(dfloat), 0, "async: true");
+    
+#endif
+
+    mesh->device.setStream(mesh->defaultStream);
+    
   }
-
+  
+  
   occaTimerTic(mesh->device,"GradientVolume");
+
   // Compute Volume Contribution
   ins->gradientVolumeKernel(mesh->Nelements,
-                            mesh->o_vgeo,
-                            mesh->o_Dmatrices,
+			    mesh->o_vgeo,
+			    mesh->o_Dmatrices,
                             ins->fieldOffset,
                             o_P,
                             o_GP);
-  occaTimerToc(mesh->device,"GradientVolume");
 
+  occaTimerToc(mesh->device,"GradientVolume");
+  
   // COMPLETE HALO EXCHANGE
   //  if (ins->pOptions.compareArgs("DISCRETIZATION","IPDG")) {
-  {
-    if(mesh->totalHaloPairs>0){
-      meshHaloExchangeFinish(mesh);
+  if(mesh->totalHaloPairs>0){
 
-      ins->o_pHaloBuffer.copyFrom(ins->pRecvBuffer);
+    // make sure compute device is ready to perform halo extract
+    mesh->device.setStream(mesh->dataStream);
+    mesh->device.finish();
 
-      ins->pressureHaloScatterKernel(mesh->Nelements,
-                                      mesh->totalHaloPairs,
-                                      o_P,
-                                      ins->o_pHaloBuffer);
-    }
+#if USE_THIN_HALO==0
+
+    // start halo exchange
+    meshHaloExchangeStart(mesh,
+			  mesh->Np*sizeof(dfloat),
+			  ins->pSendBuffer,
+			  ins->pRecvBuffer);
+
+    meshHaloExchangeFinish(mesh);
+
+    ins->o_pHaloBuffer.copyFrom(ins->pRecvBuffer);
+    
+    ins->pressureHaloScatterKernel(mesh->Nelements,
+				   mesh->totalHaloPairs,
+				   o_P,
+				   ins->o_pHaloBuffer);
+#else
+    
+    // start halo exchange
+    meshHaloExchangeStart(mesh,
+			  mesh->Nfp*sizeof(dfloat),
+			  ins->pSendBuffer,
+			  ins->pRecvBuffer);
+    
+    meshHaloExchangeFinish(mesh);
+    
+    int one = 1;
+    dlong Ndata = one*mesh->Nfp*mesh->totalHaloPairs;
+    
+    ins->o_pHaloBuffer.copyFrom(ins->pRecvBuffer, Ndata*sizeof(dfloat), 0);  // zero offset
+    
+    ins->haloPutKernel(mesh->totalHaloPairs,
+		       one,
+		       ins->fieldOffset,
+		       mesh->o_haloElementList,
+		       mesh->o_haloPutNodeIds,
+		       ins->o_pHaloBuffer,
+		       o_P);
+#endif
+
+    mesh->device.finish();
+    
+    mesh->device.setStream(mesh->defaultStream);
+    mesh->device.finish();
   }
   
   occaTimerTic(mesh->device,"GradientSurface");
+
   // Compute Surface Conribution
   ins->gradientSurfaceKernel(mesh->Nelements,
 			     mesh->o_sgeo,
