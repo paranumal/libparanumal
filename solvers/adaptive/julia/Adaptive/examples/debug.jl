3476f9a03aae63f6b0b59cd735ba3d1de0ad8692
Lucas C Wilcox <lucas@swirlee.com>
2019-02-28 14:50:24 -0800

    [julia] move p4est plotting to a function
    Former-commit-id: 05ac891c1a348417443e9019ffdc863b4e22a9cd

--- solvers/adaptive/julia/Adaptive/examples/debug.jl
+++ solvers/adaptive/julia/Adaptive/examples/debug.jl
@@ -1,17 +1,31 @@
 using MPI
+using Pxest
 using Pxest.p8est
+
+function dump_vtk(pxest;
+                  mpicomm = MPI.COMM_WORLD,
+                  mpirank = MPI.Comm_rank(mpicomm),
+                  vtk_dir = "vtk_files",
+                  vtk_base = "mesh_p8est",
+                 )
+  # dump VTK
+  mpirank == 0 ? mkpath(vtk_dir) : nothing
+  p8est.vtk_write_file(pxest, string(vtk_dir, "/", vtk_base))
+  if mpirank == 0
+    mv(string(vtk_dir, "/", vtk_base, ".pvtu"), string(vtk_base, ".pvtu"),
+      force=true)
+    mv(string(vtk_dir, "/", vtk_base, ".visit"), string(vtk_base, ".visit"),
+      force=true)
+  end
+end
 
 !MPI.Initialized() && MPI.Init()
 
 let
-  mpicomm = MPI.COMM_WORLD
-  mpirank = MPI.Comm_rank(mpicomm)
-
   N = 3 # polynomial degree
 
   conn = p8est.Connectivity(1,1,2)
   pxest = p8est.PXEST(conn; min_lvl=0)
-
 
   refine_level = 1
   p8est.refine!(pxest) do which_tree, quadrant
@@ -30,18 +44,7 @@ let
   p8est.lnodes!(pxest, N)
   mesh = p8est.Mesh(pxest)
 
-  # dump VTK
-  vtk_dir = "vtk_files"
-  vtk_base = "mesh_p8est"
-  mpirank == 0 ? mkpath(vtk_dir) : nothing
-  MPI.Barrier(mpicomm)
-  p8est.vtk_write_file(pxest, string(vtk_dir, "/", vtk_base))
-  if mpirank == 0
-    mv(string(vtk_dir, "/", vtk_base, ".pvtu"), string(vtk_base, ".pvtu"),
-      force=true)
-    mv(string(vtk_dir, "/", vtk_base, ".visit"), string(vtk_base, ".visit"),
-      force=true)
-  end
+  dump_vtk(pxest)
 end
 
 if !isinteractive()
