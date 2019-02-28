84aa05c9752e77449bb78edd9551dd7ee0ec2c02
Lucas C Wilcox <lucas@swirlee.com>
2019-02-28 15:34:12 -0800

    Add face code decode from p4est
    Former-commit-id: 1eb7f5653f399a50b593ca8382eb479a1485012c

--- solvers/adaptive/julia/Adaptive/examples/debug.jl
+++ solvers/adaptive/julia/Adaptive/examples/debug.jl
@@ -3,6 +3,37 @@ using Pxest
 using Pxest.p8est
 using SparseArrays
 
+p8est_face_edges = [4 6  8 10
+                    5 7  9 11
+                    0 2  8  9
+                    1 3 10 11
+                    0 1  4  5
+                    2 3  6  7]
+p8est_corner_faces = [0 2 4
+                      1 2 4
+                      0 3 4
+                      1 3 4
+                      0 2 5
+                      1 2 5
+                      0 3 5
+                      1 3 5]
+p8est_corner_edges = [0 4  8
+                      0 5  9
+                      1 4 10
+                      1 5 11
+                      2 6  8
+                      2 7  9
+                      3 6 10
+                      3 7 11]
+p8est_corner_face_corners = [ 0 -1  0 -1  0 -1
+                             -1  0  1 -1  1 -1
+                              1 -1 -1  0  2 -1
+                             -1  1 -1  1  3 -1
+                              2 -1  2 -1 -1  0
+                             -1  2  3 -1 -1  1
+                              3 -1 -1  2 -1  2
+                             -1  3 -1  3 -1  3]
+
 function dump_vtk(pxest;
                   mpicomm = MPI.COMM_WORLD,
                   mpirank = MPI.Comm_rank(mpicomm),
@@ -48,6 +79,78 @@ let
   # WARNING assume single MPI Rank
   S = sparse(1:length(mesh.DToC), mesh.DToC[:], ones(Int, length(mesh.DToC)))
   G = S'
+
+
+  #   hanging_face = -1 if the face is not hanging,
+  #                = the corner of the full face that it touches:
+  #                  e.g. if face = i and hanging_face[i] =
+  #                  j, then the interpolation operator corresponding
+  #                  to corner j should be used for that face.
+  #  hanging_edge = -1 if the edge is not hanging,
+  #               =  0 if the edge is the first half of a full edge,
+  #                    but neither of the two faces touching the
+  #                    edge is hanging,
+  #               =  1 if the edge is the second half of a full edge,
+  #                    but neither of the two faces touching the
+  #                    edge is hanging,
+  #               =  2 if the edge is the first half of a full edge
+  #                    and is on the boundary of a full face,
+  #               =  3 if the edge is the second half of a full edge
+  #                    and is on the boundary of a full face,
+  #               =  4 if the edge is in the middle of a full face.
+  #                    See the diagram below for clarification.
+  #
+  # o...............o  o...............o  +---2---+.......o  o.......+---3---+
+  # :               :  :               :  |       |       :  :       |       |
+  # :               :  :               :  3   2   4       :  :       4   3   3
+  # :               :  :               :  |       |       :  :       |       |
+  # +---4---+       :  :       +---4---+  +---4---+       :  :       +---4---+
+  # |       |       :  :       |       |  :               :  :               :
+  # 2   0   4       :  :       4   1   2  :               :  :               :
+  # |       |       :  :       |       |  :               :  :               :
+  # +---2---+.......o  o.......+---3---+  o...............o  o...............o
+  #
+  #                    o                  +-------+
+  #                    :                  |\       \
+  #                    :                  1 \       \
+  #                    :                  |  +-------+
+  #                    +-------+          +  |       |
+  #                    |\       \         :\ |       |
+  #                    0 \       \        : \|       |
+  #                    |  +-------+       :  +-------+
+  #                    +  |       |       o
+  #                     \ |       |
+  #                      \|       |
+  #                       +-------+
+  #
+  for fc in mesh.EToFC
+    hf = zeros(Int, 6)
+    he = zeros(Int, 12)
+    c = fc & 0x0007
+    work = fc >> 3
+    cwork = c
+    for i = 0:2
+      if (work & 0x0001) != 0
+        f = p8est_corner_faces[c+1,i+1]
+        hf[f+1] = p8est_corner_face_corners[c+1,f+1]
+        for j = 0:3
+          e = p8est_face_edges[f+1,j+1]
+          he[e+1] = 4
+        end
+      end
+      work >>= 1;
+    end
+    for i = 0:2
+      if (work & 0x0001) != 0
+        e = p8est_corner_edges[c+1,i+1]
+        he[e+1] = (he[e+1] == -1) ? 0 : 2
+        he[e+1] += (cwork & 0x0001)
+      end
+      cwork >>= 1
+      work >>= 1
+    end
+    @show hf, he
+  end
 
 
   dump_vtk(pxest)
