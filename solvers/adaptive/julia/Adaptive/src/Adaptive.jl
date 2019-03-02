39b4ee0a1d23997f5db7cdbec677c2b1af5eea25
Lucas C Wilcox <lucas@swirlee.com>
2019-03-01 16:06:43 -0800

    Move noncon to src
    Former-commit-id: 47f35ac98528914e139e1fcb105499af69674e6f

--- solvers/adaptive/julia/Adaptive/src/Adaptive.jl
+++ solvers/adaptive/julia/Adaptive/src/Adaptive.jl
@@ -6,10 +6,12 @@ export lglpoints, baryweights, spectralderivative, interpolationmatrix
 export creategrid!, computemetric!
 export creategrid1d, creategrid2d, creategrid3d, computemetric, creategrid
 export writemesh
+export nonconinterpolation
 
 include("quadrature.jl")
 include("operators.jl")
 include("metric.jl")
+include("noncon.jl")
 include("vtk.jl")
 
 
