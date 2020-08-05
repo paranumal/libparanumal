d188f465f6701835da0402783177890e682a4498
tcew <tim.warburton@gmail.com>
2020-08-05 12:52:43 -0400

    WIP: enabling tests for advection and gradient

--- solvers/gradient/src/gradientRun.cpp
+++ solvers/gradient/src/gradientRun.cpp
@@ -41,4 +41,17 @@ void gradient_t::Run(){
                o_gradq);
 
   Report();
+
+  // output norm of final solution
+  {
+    //compute q.M*dqdx
+    MassMatrixKernel(mesh.Nelements, mesh.o_ggeo, mesh.o_MM, o_gradq, o_Mgradq);
+
+    dlong Nentries = mesh.Nelements*mesh.Np;
+    dfloat norm2 = sqrt(linAlg.innerProd(Nentries, o_q, o_Mgradq, comm));
+
+    // actually probably only doing x-component
+    printf("Testing norm of gradient of solution = %17.15lg\n", norm2);
+  }
+
 }
