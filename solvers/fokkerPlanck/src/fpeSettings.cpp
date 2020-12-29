dad2221c0554c2ff3c56fb31fb96252a44ebb4c0
Noel Chalmers <noel.chalmers@gmail.com>
2020-12-28 19:57:38 -0600

    [FPE] Adding CFL number calculation

--- solvers/fokkerPlanck/src/fpeSettings.cpp
+++ solvers/fokkerPlanck/src/fpeSettings.cpp
@@ -48,6 +48,10 @@ fpeSettings_t::fpeSettings_t(MPI_Comm& _comm):
              "Time integration method",
              {"AB3", "DOPRI5", "LSERK4", "EXTBDF3", "SSBDF3"});
 
+  newSetting("CFL NUMBER",
+             "0.5",
+             "Multiplier for timestep stability bound");
+
   newSetting("NUMBER OF SUBCYCLES",
              "1",
              "Ratio of full timestep size to subcycling step size");
