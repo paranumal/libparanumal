c954b67892a1ae48d63932934ef50c64a0138064
tcew <tim.warburton@gmail.com>
2021-01-29 12:30:39 -0500

    WIP: example how to reduce expansion in SpMM

--- libs/parAlmond/parAlmondSpMM.cpp
+++ libs/parAlmond/parAlmondSpMM.cpp
@@ -29,6 +29,8 @@ SOFTWARE.
 
 namespace parAlmond {
 
+  dfloat TOL = 1e-8;
+  
 parCSR *SpMM(parCSR *A, parCSR *B){
 
   // MPI info
@@ -42,15 +44,15 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   // have the row of B on this rank, so we just need to gather
   // the offd colIds
 
-  hlong *recvRows = (hlong *) calloc(A->Ncols-A->NlocalCols, sizeof(hlong));
+  int *recvRows = (int *) calloc(A->Ncols-A->NlocalCols, sizeof(int));
   int *sendCounts = (int*) calloc(size, sizeof(int));
   int *recvCounts = (int*) calloc(size, sizeof(int));
   int *sendOffsets = (int*) calloc(size+1, sizeof(int));
   int *recvOffsets = (int*) calloc(size+1, sizeof(int));
 
   //use the colMap of A to list the needed rows of B
-  int r=0;
-  for (dlong n=A->NlocalCols;n<A->Ncols;n++) {
+  hlong r=0;
+  for (hlong n=A->NlocalCols;n<A->Ncols;n++) {
     const hlong id = A->colMap[n];
     while (id>=B->globalRowStarts[r+1]) r++; //assumes the halo is sorted
     recvCounts[r]++;
@@ -66,20 +68,20 @@ parCSR *SpMM(parCSR *A, parCSR *B){
     recvOffsets[r+1] = recvOffsets[r]+recvCounts[r];
   }
 
-  int sendTotal = sendOffsets[size];
+  hlong sendTotal = sendOffsets[size];
   hlong *sendRows = (hlong *) calloc(sendTotal, sizeof(hlong));
 
   //share the rowIds
-  MPI_Alltoallv(recvRows, recvCounts, recvOffsets, MPI_HLONG,
-                sendRows, sendCounts, sendOffsets, MPI_HLONG,
+  MPI_Alltoallv(recvRows, recvCounts, recvOffsets, MPI_INT,
+                sendRows, sendCounts, sendOffsets, MPI_INT,
                 B->comm);
 
   //we now have a list of rows to send, count the nnz to send
-  dlong nnzTotal=0;
+  hlong nnzTotal=0;
   for (r=0;r<size;r++) {
     sendCounts[r] =0; //reset
-    for (int n=sendOffsets[r];n<sendOffsets[r+1];n++) {
-      dlong i = (dlong) (sendRows[n]-B->globalRowStarts[rank]); //local row id
+    for (hlong n=sendOffsets[r];n<sendOffsets[r+1];n++) {
+      hlong i = (hlong) (sendRows[n]-B->globalRowStarts[rank]); //local row id
       sendCounts[r]+= B->diag.rowStarts[i+1]-B->diag.rowStarts[i]; //count entries in this row
       sendCounts[r]+= B->offd.rowStarts[i+1]-B->offd.rowStarts[i]; //count entries in this row
     }
@@ -90,15 +92,15 @@ parCSR *SpMM(parCSR *A, parCSR *B){
 
   nnzTotal=0; //reset
   for (r=0;r<size;r++) {
-    for (int n=sendOffsets[r];n<sendOffsets[r+1];n++) {
-      dlong i = (dlong) (sendRows[n] - B->globalRowStarts[rank]); //local row id
-      for (dlong jj=B->diag.rowStarts[i]; jj<B->diag.rowStarts[i+1];jj++){
+    for (hlong n=sendOffsets[r];n<sendOffsets[r+1];n++) {
+      hlong i = (hlong) (sendRows[n] - B->globalRowStarts[rank]); //local row id
+      for (hlong jj=B->diag.rowStarts[i]; jj<B->diag.rowStarts[i+1];jj++){
         sendNonZeros[nnzTotal].row = sendRows[n];
         sendNonZeros[nnzTotal].col = B->diag.cols[jj] + B->globalColStarts[rank];
         sendNonZeros[nnzTotal].val = B->diag.vals[jj];
         nnzTotal++;
       }
-      for (dlong jj=B->offd.rowStarts[i]; jj<B->offd.rowStarts[i+1];jj++){
+      for (hlong jj=B->offd.rowStarts[i]; jj<B->offd.rowStarts[i+1];jj++){
         sendNonZeros[nnzTotal].row = sendRows[n];
         sendNonZeros[nnzTotal].col = B->colMap[B->offd.cols[jj]];
         sendNonZeros[nnzTotal].val = B->offd.vals[jj];
@@ -107,8 +109,8 @@ parCSR *SpMM(parCSR *A, parCSR *B){
     }
   }
 
-  MPI_Alltoall(sendCounts, 1, MPI_INT,
-               recvCounts, 1, MPI_INT, A->comm);
+  MPI_Alltoall(sendCounts, 1, MPI_HLONG,
+               recvCounts, 1, MPI_HLONG, A->comm);
 
   for (r=0;r<size;r++) {
     sendOffsets[r+1] = sendOffsets[r]+sendCounts[r];
@@ -116,7 +118,7 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   }
 
 
-  dlong Boffdnnz = recvOffsets[size]; //total nonzeros
+  hlong Boffdnnz = recvOffsets[size]; //total nonzeros
   parCOO::nonZero_t *BoffdRows = (parCOO::nonZero_t *)
                                  calloc(Boffdnnz, sizeof(parCOO::nonZero_t));
 
@@ -135,10 +137,10 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   //we now have all the needed nonlocal rows (should also be sorted by row then col)
 
   //make an array of row offsets so we know how large each row is
-  dlong *BoffdRowOffsets = (dlong *) calloc(A->Ncols-A->NlocalCols+1, sizeof(dlong));
+  hlong *BoffdRowOffsets = (hlong *) calloc(A->Ncols-A->NlocalCols+1, sizeof(hlong));
 
-  dlong id=0;
-  for (dlong n=0;n<Boffdnnz;n++) {
+  hlong id=0;
+  for (hlong n=0;n<Boffdnnz;n++) {
     hlong row = BoffdRows[n].row;
 
     while(A->colMap[id+A->NlocalCols]!=row) id++;
@@ -147,7 +149,7 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   }
 
   //cumulative sum
-  for (dlong n=0;n<A->Ncols-A->NlocalCols;n++)
+  for (hlong n=0;n<A->Ncols-A->NlocalCols;n++)
     BoffdRowOffsets[n+1] += BoffdRowOffsets[n];
 
 
@@ -156,97 +158,143 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   // the entries
 
   // Find how big the intermediate form is
+  hlong maxrowCount  = 0;
   nnzTotal = 0;
-  for (dlong i=0;i<A->Nrows;i++) {
+  for (hlong i=0;i<A->Nrows;i++) {
+    hlong rowcnt = 0;
     //local entries
-    dlong start = A->diag.rowStarts[i];
-    dlong end   = A->diag.rowStarts[i+1];
-    for (dlong j=start;j<end;j++) {
-      const dlong col = A->diag.cols[j];
-      const int nnzBj =  B->diag.rowStarts[col+1]-B->diag.rowStarts[col]
+    hlong start = A->diag.rowStarts[i];
+    hlong end   = A->diag.rowStarts[i+1];
+    for (hlong j=start;j<end;j++) {
+      const hlong col = A->diag.cols[j];
+      const hlong nnzBj =  B->diag.rowStarts[col+1]-B->diag.rowStarts[col]
                         +B->offd.rowStarts[col+1]-B->offd.rowStarts[col];
       nnzTotal += nnzBj;
+      rowcnt += nnzBj;
     }
     //non-local entries
     start = A->offd.rowStarts[i];
     end   = A->offd.rowStarts[i+1];
-    for (dlong j=start;j<end;j++) {
-      const dlong col = A->offd.cols[j]-A->NlocalCols;
-      const int nnzBj = BoffdRowOffsets[col+1] - BoffdRowOffsets[col];
+    for (hlong j=start;j<end;j++) {
+      const hlong col = A->offd.cols[j]-A->NlocalCols;
+      const hlong nnzBj = BoffdRowOffsets[col+1] - BoffdRowOffsets[col];
       nnzTotal += nnzBj;
+      rowcnt += nnzBj;
     }
+    maxrowCount = mymax(maxrowCount, rowcnt);
   }
+  printf("maxrowCount = %lld\n", maxrowCount);
+  
+  if(nnzTotal<0) printf("OVERFLOW in parAlmondSpMM: A->nnz=%d, B->nnz=%d\n",
+			A->diag.nnz, B->diag.nnz);
 
   parCOO::nonZero_t *Ctmp = (parCOO::nonZero_t *)
                             calloc(nnzTotal, sizeof(parCOO::nonZero_t));
 
+  //  dlong maxrowCount = 100000;
+  parCOO::nonZero_t *rowCtmp = (parCOO::nonZero_t *)
+    calloc(maxrowCount, sizeof(parCOO::nonZero_t));
+  
+  printf("sizeof(hlong)=%d\n", sizeof(hlong));
+  
   // Fill the intermediate form of C
-  dlong cnt = 0;
-  for (dlong i=0;i<A->Nrows;i++) {
-    //local A entries
-    dlong start = A->diag.rowStarts[i];
-    dlong end   = A->diag.rowStarts[i+1];
-    for (dlong j=start;j<end;j++) {
-      const dlong col = A->diag.cols[j];
-      const dfloat Aval = A->diag.vals[j];
+  hlong cnt = 0;
+  for (hlong i=0;i<A->Nrows;i++) {
 
+    dlong rowcnt = 0;
+
+    //local A entries
+    hlong start = A->diag.rowStarts[i];
+    hlong end   = A->diag.rowStarts[i+1];
+    for (hlong j=start;j<end;j++) {
+      const hlong col = A->diag.cols[j];
+      const dfloat Aval = A->diag.vals[j];
+      
+      if(col<0) printf("FOUND negative col %lld (1)\n", col);
+      
       //local B entries
-      dlong Bstart = B->diag.rowStarts[col];
-      dlong Bend   = B->diag.rowStarts[col+1];
-      for (dlong jj=Bstart;jj<Bend;jj++) {
-        Ctmp[cnt].row = i + A->globalRowStarts[rank];
-        Ctmp[cnt].col = B->diag.cols[jj]+B->globalColStarts[rank]; //global id
-        Ctmp[cnt].val = Aval*B->diag.vals[jj];
-        cnt++;
+      hlong Bstart = B->diag.rowStarts[col];
+      hlong Bend   = B->diag.rowStarts[col+1];
+      for (hlong jj=Bstart;jj<Bend;jj++) {
+	dfloat val = Aval*B->diag.vals[jj];
+	if(fabs(val)>TOL){
+	  rowCtmp[rowcnt].row = i + A->globalRowStarts[rank];
+	  rowCtmp[rowcnt].col = B->diag.cols[jj]+B->globalColStarts[rank]; //global id
+	  rowCtmp[rowcnt].val = val;
+	  rowcnt++;
+	}
       }
       //non-local B entries
       Bstart = B->offd.rowStarts[col];
       Bend   = B->offd.rowStarts[col+1];
-      for (dlong jj=Bstart;jj<Bend;jj++) {
-        Ctmp[cnt].row = i + A->globalRowStarts[rank];
-        Ctmp[cnt].col = B->colMap[B->offd.cols[jj]]; //global id
-        Ctmp[cnt].val = Aval*B->offd.vals[jj];
-        cnt++;
+      for (hlong jj=Bstart;jj<Bend;jj++) {
+	dfloat val = Aval*B->offd.vals[jj];
+	if(fabs(val)>TOL){
+	  rowCtmp[rowcnt].row = i + A->globalRowStarts[rank];
+	  rowCtmp[rowcnt].col = B->colMap[B->offd.cols[jj]]; //global id
+	  rowCtmp[rowcnt].val = val;
+	  rowcnt++;
+	}
       }
     }
+
+    if(rowcnt>maxrowCount) printf("WARNING rowcnt=%d, maxrowcnt=%d\n", rowcnt, maxrowCount);
+    
     //non-local A entries
     start = A->offd.rowStarts[i];
     end   = A->offd.rowStarts[i+1];
-    for (dlong j=start;j<end;j++) {
-      const dlong col = A->offd.cols[j]-A->NlocalCols;
+    for (hlong j=start;j<end;j++) {
+      const hlong col = A->offd.cols[j]-A->NlocalCols;
       const dfloat Aval = A->offd.vals[j];
 
+      if(col<0) printf("FOUND negative col %lld (2)\n", col);
+
       // entries from recived rows of B
-      dlong Bstart = BoffdRowOffsets[col];
-      dlong Bend   = BoffdRowOffsets[col+1];
-      for (dlong jj=Bstart;jj<Bend;jj++) {
-        Ctmp[cnt].row = i + A->globalRowStarts[rank];
-        Ctmp[cnt].col = BoffdRows[jj].col; //global id
-        Ctmp[cnt].val = Aval*BoffdRows[jj].val;
-        cnt++;
+      hlong Bstart = BoffdRowOffsets[col];
+      hlong Bend   = BoffdRowOffsets[col+1];
+      for (hlong jj=Bstart;jj<Bend;jj++) {
+	dfloat val = Aval*BoffdRows[jj].val;
+	if(fabs(val)>TOL){
+	  rowCtmp[rowcnt].row = i + A->globalRowStarts[rank];
+	  rowCtmp[rowcnt].col = BoffdRows[jj].col; //global id
+	  rowCtmp[rowcnt].val = val;
+	  rowcnt++;
+	}
+      }
+
+      if(rowcnt>maxrowCount) printf("WARNING rowcnt=%d, maxrowcnt=%d\n", rowcnt, maxrowCount);
+      
+    }
+
+    //sort entries by the row and col
+    std::sort(rowCtmp, rowCtmp+rowcnt,
+	      [](const parCOO::nonZero_t& a, const parCOO::nonZero_t& b) {
+		if (a.row < b.row) return true;
+		if (a.row > b.row) return false;
+		
+		return a.col < b.col;
+	      });
+
+    //count total number of nonzeros;
+    if (rowcnt) Ctmp[cnt++] = rowCtmp[0];
+    for (hlong j=1;j<rowcnt;j++) {
+      if ((rowCtmp[j].row!=rowCtmp[j-1].row)||
+	  (rowCtmp[j].col!=rowCtmp[j-1].col)) {
+	Ctmp[cnt++] = rowCtmp[j];
+      } else {
+	Ctmp[cnt-1].val += rowCtmp[j].val;
       }
     }
   }
+
   free(BoffdRowOffsets);
   free(BoffdRows);
-
-
-  //sort entries by the row and col
-  std::sort(Ctmp, Ctmp+nnzTotal,
-            [](const parCOO::nonZero_t& a, const parCOO::nonZero_t& b) {
-              if (a.row < b.row) return true;
-              if (a.row > b.row) return false;
-
-              return a.col < b.col;
-            });
-
-  //count total number of nonzeros;
-  dlong nnz =0;
-  if (nnzTotal) nnz++;
-  for (dlong i=1;i<nnzTotal;i++)
-    if ((Ctmp[i].row!=Ctmp[i-1].row)||
-        (Ctmp[i].col!=Ctmp[i-1].col)) nnz++;
-
+  free(rowCtmp);
+  
+  printf("Est nnzTotal=%lld, cnt=%lld\n", nnzTotal, cnt);
+  
+  nnzTotal = cnt;
+  dlong nnz = cnt;
   parCOO cooC(A->platform, A->comm);
 
   //copy global partition
@@ -261,16 +309,25 @@ parCSR *SpMM(parCSR *A, parCSR *B){
   //compress nonzeros
   nnz = 0;
   if (nnzTotal) cooC.entries[nnz++] = Ctmp[0];
-  for (dlong i=1;i<nnzTotal;i++) {
+  for (hlong i=1;i<nnzTotal;i++) {
     if ((Ctmp[i].row!=Ctmp[i-1].row)||
         (Ctmp[i].col!=Ctmp[i-1].col)) {
-      cooC.entries[nnz++] = Ctmp[i];
+      //increment non-zero count if current entry is above tolerance
+      if (fabs(cooC.entries[nnz-1].val)>parAlmond::dropTolerance) nnz++;
+
+      cooC.entries[nnz-1] = Ctmp[i];
     } else {
       cooC.entries[nnz-1].val += Ctmp[i].val;
     }
   }
   //clean up
   free(Ctmp);
+
+  //update nnz count
+  cooC.nnz = nnz;
+  cooC.entries = (parCOO::nonZero_t *)
+                    realloc(cooC.entries,
+                            cooC.nnz*sizeof(parCOO::nonZero_t));
 
   //build C from coo matrix
   return new parCSR(cooC);
